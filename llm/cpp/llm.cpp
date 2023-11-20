dca54f1cc67ec05c5473368dcfbd67a76f7992cc
Wovchena <vladimir.zlobin@intel.com>
2023-11-20 21:16:30 +0400

    int32_t->int64_t

--- llm/cpp/llm.cpp
+++ llm/cpp/llm.cpp
@@ -13,11 +13,11 @@ std::pair<ov::Tensor, ov::Tensor> tokenize(ov::InferRequest&& tokenizer, std::st
     return {tokenizer.get_tensor("input_ids"), tokenizer.get_tensor("attention_mask")};
 }
 
-void print_token(ov::InferRequest& detokenizer, int32_t out_token) {
+void print_token(ov::InferRequest& detokenizer, int64_t out_token) {
     constexpr size_t BATCH_SIZE = 1;
     ov::Tensor inp = detokenizer.get_input_tensor();
     inp.set_shape({BATCH_SIZE, 1});
-    inp.data<int32_t>()[0] = out_token;
+    inp.data<int64_t>()[0] = out_token;
     detokenizer.infer();
     std::cout << openvino_extensions::unpack_strings(detokenizer.get_output_tensor()).front() << std::flush;
 }
@@ -56,19 +56,19 @@ int main(int argc, char* argv[]) try {
         ireq.get_input_tensor(idx).set_shape(inputs.at(idx).get_partial_shape().get_min_shape());
     }
     ireq.get_tensor("input_ids").set_shape(input_ids.get_shape());  // TODO: replace with ireq.set_tensor("input_ids", input_ids); after it's fixed
-    ireq.get_tensor("attention_mask").set_shape(input_ids.get_shape());
+    ireq.get_tensor("attention_mask").set_shape(attention_mask.get_shape());
     std::copy_n(input_ids.data<const int64_t>(), input_ids.get_size(), ireq.get_tensor("input_ids").data<int64_t>());
-    std::fill_n(ireq.get_tensor("attention_mask").data<int64_t>(), input_ids.get_size(), 1);
+    std::fill_n(ireq.get_tensor("attention_mask").data<int64_t>(), attention_mask.get_size(), 1);
     ireq.get_tensor("position_ids").set_shape(input_ids.get_shape());
     std::iota(ireq.get_tensor("position_ids").data<int64_t>(), ireq.get_tensor("position_ids").data<int64_t>() + ireq.get_tensor("position_ids").get_size(), 0);
     ireq.infer();
     size_t vocab_size = ireq.get_tensor("logits").get_shape().back();
     float* logits = ireq.get_tensor("logits").data<float>() + (input_ids.get_size() - 1) * vocab_size;
-    int32_t out_token = int32_t(std::max_element(logits, logits + vocab_size) - logits);
+    int64_t out_token = std::max_element(logits, logits + vocab_size) - logits;
 
     ireq.get_tensor("input_ids").set_shape({BATCH_SIZE, 1});
     ireq.get_tensor("position_ids").set_shape({BATCH_SIZE, 1});
-    constexpr int32_t SPECIAL_EOS_TOKEN = 2;  // There's no way to extract the value from the detokenizer for now
+    constexpr int64_t SPECIAL_EOS_TOKEN = 2;  // There's no way to extract the value from the detokenizer for now
     while (out_token != SPECIAL_EOS_TOKEN) {
         ireq.get_tensor("input_ids").data<int64_t>()[0] = out_token;
         ireq.get_tensor("attention_mask").set_shape({BATCH_SIZE, ireq.get_tensor("attention_mask").get_shape()[1] + 1});
@@ -81,7 +81,7 @@ int main(int argc, char* argv[]) try {
         print_token(detokenizer, out_token);
         ireq.wait();
         logits = ireq.get_tensor("logits").data<float>();
-        out_token = int32_t(std::max_element(logits, logits + vocab_size) - logits);
+        out_token = std::max_element(logits, logits + vocab_size) - logits;
     }
     std::cout << '\n';
 } catch (const std::exception& error) {
