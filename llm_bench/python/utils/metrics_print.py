adb9b342edd18f3e07ad25e1c248be2bc4aeedc8
wgzintel <guozhong.wang@intel.com>
2023-11-30 17:33:14 +0800

    format code

--- llm_bench/python/utils/metrics_print.py
+++ llm_bench/python/utils/metrics_print.py
@@ -62,7 +62,8 @@ def print_stable_diffusion_infer_latency(iter_str, iter_data, ovForward):
     )
     log.info(f"[{iter_str}] text encoder latency: {ovForward.get_text_encoder_time() * 1000:.2f} ms, "
                 f"unet latency: {ovForward.get_unet_time() * 1000:.2f} ms, "
-                f"vae decoder latency: {ovForward.get_vae_decoder_time() * 1000:.2f} ms")
+                f"vae decoder latency: {ovForward.get_vae_decoder_time() * 1000:.2f} ms",
+                )
 
 
 def print_ldm_unet_vqvae_infer_latency(iter_num, iter_data, tms=None, warm_up=False):
@@ -83,7 +84,8 @@ def print_ldm_unet_vqvae_infer_latency(iter_num, iter_data, tms=None, warm_up=Fa
         log.info(f"[{iter_str}] unet latency: {sum(tms[0:(len_tms - 1)]) * 1000:.2f} ms, "
                 f"vqvae decoder latency: {tms[len_tms - 1] * 1000:.2f} ms, "
                 f"unet infer numbers: {len_tms - 1}, "
-                f"vqvae decoder infer numbers: 1",)
+                f"vqvae decoder infer numbers: 1",
+                )
 
 
 def print_average(iter_data_list):
