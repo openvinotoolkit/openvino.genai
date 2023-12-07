e5d786146633eae6abfdf5d63e32703018e5628f
eaidova <ekaterina.aidova@intel.com>
2023-12-07 10:06:51 +0400

    add support stablelm into bettertransformer

--- llm_bench/python/convert.py
+++ llm_bench/python/convert.py
@@ -9,7 +9,6 @@ from argparse import ArgumentParser
 from enum import Enum
 from functools import wraps
 from pathlib import Path
-import types
 from typing import Tuple, Dict, Optional
 import torch
 from diffusers import StableDiffusionPipeline, StableDiffusionXLImg2ImgPipeline, LDMSuperResolutionPipeline, DiffusionPipeline
@@ -45,9 +44,10 @@ try:
 except ImportError:
     from optimum.exporters.onnx.__main__ import _get_submodels_and_onnx_configs as _get_submodels_and_export_configs
 
-from transformers import AutoTokenizer, AutoConfig, AutoModelForCausalLM, AutoModelForSeq2SeqLM, AutoModel, PreTrainedModel
-from transformers.modeling_outputs import BaseModelOutputWithPast
+from transformers import AutoTokenizer, AutoConfig, AutoModelForCausalLM, AutoModelForSeq2SeqLM, AutoModel
 from utils.nncf_utils import COMPRESSION_OPTIONS, INT4_MODEL_CONFIGURATION, get_compressed_path
+from utils.conversion_utils.convert_patch import patch_model_for_optimum_export
+from utils.conversion_utils.better_transformer_patch import patch_model_with_bettertransformer
 
 
 class BackendType(Enum):
@@ -135,153 +135,6 @@ def unpatch_gptq(orig_cuda_check, orig_post_init_model):
     GPTQQuantizer.post_init_model = orig_post_init_model
 
 
-@torch.jit.script_if_tracing
-def _chatglm2_get_context_layer(query_layer: torch.Tensor, key_layer: torch.Tensor, value_layer: torch.Tensor):
-    mask = torch.zeros((query_layer.shape[-2], key_layer.shape[-2]), dtype=query_layer.dtype)
-    if query_layer.shape[2] == key_layer.shape[2]:
-        tmp_mask = torch.ones((query_layer.shape[-2], key_layer.shape[-2]), dtype=torch.bool).triu(diagonal=1)
-        mask.masked_fill_(tmp_mask, float("-inf"))
-
-    context_layer = torch.nn.functional.scaled_dot_product_attention(query_layer, key_layer, value_layer, attn_mask=mask)
-    return context_layer
-
-
-def _core_attention_forward(self, query_layer, key_layer, value_layer, attention_mask):
-    query_layer, key_layer, value_layer = [k.permute(1, 2, 0, 3) for k in [query_layer, key_layer, value_layer]]
-    if attention_mask is None:
-        context_layer = _chatglm2_get_context_layer(query_layer, key_layer, value_layer)
-    else:
-        context_layer = torch.nn.functional.scaled_dot_product_attention(
-            query_layer, key_layer, value_layer, attention_mask
-        )
-    context_layer = context_layer.permute(2, 0, 1, 3)
-    new_context_layer_shape = context_layer.size()[:-2] + (self.hidden_size_per_partition,)
-    context_layer = context_layer.reshape(*new_context_layer_shape)
-
-    return context_layer
-
-
-@torch.jit.script_if_tracing
-def _get_chatglm_attention_mask(input_ids, past_key):
-    mask = torch.zeros((input_ids.shape[1], past_key.shape[0] + input_ids.shape[1]), dtype=past_key.dtype)
-    if past_key.shape[0] == 0:
-        tmp_mask = torch.ones((input_ids.shape[1], past_key.shape[0] + input_ids.shape[1]), dtype=torch.bool).triu(diagonal=1)
-        mask.masked_fill_(tmp_mask, float("-inf"))
-    return mask
-
-
-def _chatglm_transformer_forward(
-        self,
-        input_ids,
-        position_ids: Optional[torch.Tensor] = None,
-        attention_mask: Optional[torch.BoolTensor] = None,
-        full_attention_mask: Optional[torch.BoolTensor] = None,
-        past_key_values: Optional[Tuple[Tuple[torch.Tensor, torch.Tensor], ...]] = None,
-        inputs_embeds: Optional[torch.Tensor] = None,
-        use_cache: Optional[bool] = None,
-        output_hidden_states: Optional[bool] = None,
-        return_dict: Optional[bool] = None
-):
-    output_hidden_states = (
-        output_hidden_states if output_hidden_states is not None else self.config.output_hidden_states
-    )
-    use_cache = use_cache if use_cache is not None else self.config.use_cache
-    return_dict = return_dict if return_dict is not None else self.config.use_return_dict
-
-    batch_size, seq_length = input_ids.shape
-
-    if inputs_embeds is None:
-        inputs_embeds = self.embedding(input_ids)
-
-    if self.pre_seq_len is not None:
-        if past_key_values is None:
-            past_key_values = self.get_prompt(batch_size=batch_size, device=input_ids.device,
-                                              dtype=inputs_embeds.dtype)
-        if attention_mask is not None:
-            attention_mask = torch.cat([attention_mask.new_ones((batch_size, self.pre_seq_len)), attention_mask], dim=-1)
-
-    if full_attention_mask is None:
-        if (attention_mask is not None and not attention_mask.all()) or (past_key_values and seq_length != 1):
-            full_attention_mask = self.get_masks(input_ids, past_key_values, padding_mask=attention_mask)
-        elif past_key_values is not None:
-            full_attention_mask = _get_chatglm_attention_mask(input_ids, past_key_values[0][0])
-
-    # Rotary positional embeddings
-    rotary_pos_emb = self.rotary_pos_emb(self.seq_length)
-    if position_ids is not None:
-        rotary_pos_emb = rotary_pos_emb[position_ids]
-    else:
-        rotary_pos_emb = rotary_pos_emb[None, :seq_length]
-    rotary_pos_emb = rotary_pos_emb.transpose(0, 1).contiguous()
-
-    # Run encoder.
-    hidden_states, presents, all_hidden_states, all_self_attentions = self.encoder(
-        inputs_embeds, full_attention_mask, rotary_pos_emb=rotary_pos_emb,
-        kv_caches=past_key_values, use_cache=use_cache, output_hidden_states=output_hidden_states
-    )
-
-    if not return_dict:
-        return tuple(v for v in [hidden_states, presents, all_hidden_states, all_self_attentions] if v is not None)
-
-    return BaseModelOutputWithPast(
-        last_hidden_state=hidden_states,
-        past_key_values=presents,
-        hidden_states=all_hidden_states,
-        attentions=all_self_attentions,
-    )
-
-
-def _patch_chatglm_core_attention_forward(model: "PreTrainedModel"):
-    model.transformer.forward = types.MethodType(_chatglm_transformer_forward, model.transformer)
-    for block in model.transformer.encoder.layers:
-        block.self_attention.core_attention.forward = types.MethodType(
-            _core_attention_forward, block.self_attention.core_attention
-        )
-
-
-def _update_qwen_rotary_embedding_cache(model):
-    model.transformer.rotary_emb(2048)
-
-
-def _yi_prepare_decoder_attention_mask(attention_mask, input_ids, inputs_embeds, past_key_values_length):
-    input_shape = input_ids.shape if input_ids is not None else inputs_embeds.shape[:-1]
-    return _prepare_decoder_attention_mask(attention_mask, input_shape, inputs_embeds, past_key_values_length)
-
-
-# Modified from transformers.models.mistral.modeling_mistral._prepare_decoder_sliding_window_attention_mask
-def _prepare_decoder_sliding_window_attention_mask(
-    attention_mask: torch.Tensor,
-    input_shape: Tuple[int, int],
-    inputs_embeds: torch.Tensor,
-    past_key_values_length: int,
-    sliding_window: int,
-):
-    from transformers.models.mistral.modeling_mistral import _expand_mask, _make_sliding_window_causal_mask
-
-    # create causal mask
-    # [bsz, seq_len] -> [bsz, 1, tgt_seq_len, src_seq_len]
-    combined_attention_mask = None
-
-    combined_attention_mask = _make_sliding_window_causal_mask(
-        input_shape,
-        device=inputs_embeds.device,
-        dtype=inputs_embeds.dtype,
-        past_key_values_length=past_key_values_length,
-        sliding_window=sliding_window,
-    )
-
-    if attention_mask is not None:
-        # [bsz, seq_len] -> [bsz, 1, tgt_seq_len, src_seq_len]
-        expanded_attn_mask = _expand_mask(attention_mask, inputs_embeds.dtype, tgt_len=input_shape[-1]).to(
-            inputs_embeds.device
-        )
-        combined_attention_mask = (
-            expanded_attn_mask if combined_attention_mask is None else expanded_attn_mask + combined_attention_mask
-        )
-
-    return combined_attention_mask
-
-
 class TextDecoderWithPositionIdsOnnxConfig(TextDecoderOnnxConfig):
     @property
     def inputs(self) -> Dict[str, Dict[int, str]]:
@@ -294,105 +147,6 @@ class TextDecoderWithPositionIdsOnnxConfig(TextDecoderOnnxConfig):
             common_inputs["position_ids"] = {0: "batch_size", 1: "sequence_length"}
 
         return common_inputs
-
-
-# Copied from transformers.models.bart.modeling_bart._expand_mask
-def _expand_mask(mask: torch.Tensor, dtype: torch.dtype, tgt_len: Optional[int] = None):
-    """
-    Expands attention_mask from `[bsz, seq_len]` to `[bsz, 1, tgt_seq_len, src_seq_len]`.
-    """
-    bsz, src_len = mask.size()
-    tgt_len = tgt_len if tgt_len is not None else src_len
-
-    expanded_mask = mask[:, None, None, :].expand(bsz, 1, tgt_len, src_len).to(dtype)
-
-    inverted_mask = 1.0 - expanded_mask
-
-    return inverted_mask.masked_fill(inverted_mask.to(torch.bool), torch.finfo(dtype).min)
-
-
-# Modified from transformers.models.bloom.modeling_bloom._make_causal_mask
-def _make_causal_mask(
-    input_ids_shape: torch.Size,
-    device: torch.device,
-    past_key_values_length: int,
-    dtype: torch.dtype = torch.bool,
-) -> torch.BoolTensor:
-    """
-    Make causal mask used for bi-directional self-attention.
-    """
-    batch_size, target_length = input_ids_shape
-    mask = torch.zeros((target_length, target_length + past_key_values_length), dtype=dtype, device=device)
-    seq_ids = torch.arange(target_length, device=device)
-
-    mask[:, past_key_values_length:] = (
-        (seq_ids[:, None] < seq_ids[None, :]) * torch.finfo(dtype).min
-        if torch.is_floating_point(mask)
-        else seq_ids[:, None] < seq_ids[None, :]
-    )
-
-    return mask[None, None, :, :].expand(batch_size, 1, target_length, target_length + past_key_values_length)
-
-
-# Modified from transformers.models.llama.modeling_llama._prepare_decoder_attention_mask
-def _prepare_decoder_attention_mask(attention_mask, input_shape, inputs_embeds, past_key_values_length):
-
-    # create causal mask
-    # [bsz, seq_len] -> [bsz, 1, tgt_seq_len, src_seq_len]
-    combined_attention_mask = None
-
-    combined_attention_mask = _make_causal_mask(
-        input_shape,
-        device=inputs_embeds.device,
-        past_key_values_length=past_key_values_length,
-        dtype=inputs_embeds.dtype,
-    )
-
-    if attention_mask is not None:
-        # [bsz, seq_len] -> [bsz, 1, tgt_seq_len, src_seq_len]
-        expanded_attn_mask = _expand_mask(attention_mask, inputs_embeds.dtype, tgt_len=input_shape[-1]).to(
-            inputs_embeds.device
-        )
-        combined_attention_mask = (
-            expanded_attn_mask if combined_attention_mask is None else expanded_attn_mask + combined_attention_mask
-        )
-
-    return combined_attention_mask
-
-
-def patch_model_for_optimum_export(model):
-    if model.config.model_type in ["stablelm_epoch", "baichuan"]:
-        model.model._prepare_decoder_attention_mask = _prepare_decoder_attention_mask
-    elif model.config.model_type == "chatglm":
-        _patch_chatglm_core_attention_forward(model)
-    elif model.config.model_type == "qwen":
-        _update_qwen_rotary_embedding_cache(model)
-    elif model.config.model_type == "mistral":
-        model.model._prepare_decoder_attention_mask = _prepare_decoder_sliding_window_attention_mask
-    elif model.config.model_type == "Yi":
-        model.model._prepare_decoder_attention_mask = _yi_prepare_decoder_attention_mask
-    return model
-
-
-def patch_model_with_bettertransformer(model, model_config):
-    model = model.to_bettertransformer()
-
-    # for better transformers we need sequence lenght to be not 1 to make a correct trace
-    # patch generate_dummy_inputs in the config
-
-    def pathed_generate_dummy_inputs(self, *args, **kwargs):
-        dummy_inputs = self._original_generate_dummy_inputs(*args, **kwargs)
-        if 'input_ids' in dummy_inputs and dummy_inputs['input_ids'].shape[1] == 1:
-            dummy_inputs['input_ids'] = torch.cat([dummy_inputs['input_ids'], dummy_inputs['input_ids']], dim=-1)
-            attention_mask = dummy_inputs['attention_mask']
-            dummy_inputs['attention_mask'] = torch.cat([attention_mask, attention_mask.new_ones((attention_mask.shape[0], 1))], dim=-1)
-        return dummy_inputs
-
-    import types
-    model_config._original_generate_dummy_inputs = model_config.generate_dummy_inputs
-    model_config.generate_dummy_inputs = types.MethodType(pathed_generate_dummy_inputs, model_config)
-
-    return model
 
 
 def convert_optimum_causallm_base(model, args):
