28d8a679caf409ca92bfe454c7d2f5b12e278b19
wgzintel <guozhong.wang@intel.com>
2023-12-12 15:10:54 +0800

    Correct the conversion output path of the model

--- llm_bench/python/llm_run_on_linux.sh
+++ llm_bench/python/llm_run_on_linux.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 # Copyright (C) 2023 Intel Corporation
 # SPDX-License-Identifier: Apache-2.0
-
+ 
 git lfs install
 export GIT_LFS_SKIP_SMUDGE=0
-git clone https://huggingface.co/bigscience/bloomz-560m
-python ./llm_bench/python/convert.py --model_id bloomz-560m/pytorch  --output_dir ./ --precision FP16
-python ./llm_bench/python/benchmark.py -m ./bloomz-560m/pytorch/dldt/FP16/ 
+git_clone_bloomz_560m="git clone https://huggingface.co/bigscience/bloomz-560m"
+echo ${git_clone_bloomz_560m}
+eval ${git_clone_bloomz_560m}
+wait
+
+convert_model="python ./llm_bench/python/convert.py --model_id bloomz-560m/  --output_dir ./ov_models/bloomz-560m --precision FP16"
+echo ${convert_model}
+eval ${convert_model}
+wait
+
+bemchmaking="python ./llm_bench/python/benchmark.py -m ./ov_models/bloomz-560m/pytorch/dldt/FP16/"
+echo ${bemchmaking}
+eval ${bemchmaking}
