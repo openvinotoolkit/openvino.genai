16717f5cdd2ae05428a8addccf9169f8e96ce7c1
Su Yang <su.yang@intel.com>
2023-12-14 14:17:18 -0500

    change path of set_up_and_run.sh

--- image_generation/stable_diffusion_1_5/cpp/set_up_and_run.sh
+++ image_generation/stable_diffusion_1_5/cpp/set_up_and_run.sh
@@ -15,11 +15,10 @@ cd "`abs_path`"
 mkdir ./ov/
 curl https://storage.openvinotoolkit.org/repositories/openvino/packages/2023.2/linux/l_openvino_toolkit_ubuntu20_2023.2.0.13089.cfd42bd2cb0_x86_64.tgz | tar --directory ./ov/ --strip-components 1 -xz
 sudo ./ov/install_dependencies/install_openvino_dependencies.sh
-
 source ./ov/setupvars.sh
-python -m pip install -r requirements.txt && python -m convert_model.py -sd runwayml/stable-diffusion-v1-5 -b 1 -t FP16 -dyn True &
+python -m pip install -r ./scripts/requirements.txt && python -m convert_model.py -sd runwayml/stable-diffusion-v1-5 -b 1 -t FP16 -dyn True &
 cmake -DCMAKE_BUILD_TYPE=Release -S ./ -B ./build/
 cmake --build ./build/ --config Release -j
 wait
 
-../build/SD-generate -m runwayml/stable-diffusion-v1-5 -t FP16_dyn
+./build/SD-generate -m runwayml/stable-diffusion-v1-5 -t FP16_dyn
