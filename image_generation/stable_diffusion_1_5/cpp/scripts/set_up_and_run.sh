414e3a59ac7781ec7186634e856a4b9d69072494
Su Yang <su.yang@intel.com>
2023-12-14 11:33:46 -0500

    add test script

---
+++ image_generation/stable_diffusion_1_5/cpp/scripts/set_up_and_run.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# Copyright (C) 2023 Intel Corporation
+# SPDX-License-Identifier: Apache-2.0
+
+set -e  # Exit immediately if a command exits with a non-zero status
+
+abs_path() {
+    script_path=$(eval echo "${BASH_SOURCE[0]}")
+    directory=$(dirname "$script_path")
+    builtin cd "$directory" || exit
+    pwd -P
+}
+cd "`abs_path`"
+
+mkdir ./ov/
+curl https://storage.openvinotoolkit.org/repositories/openvino/packages/2023.1/linux/l_openvino_toolkit_ubuntu20_2023.1.0.12185.47b736f63ed_x86_64.tgz | tar --directory ./ov/ --strip-components 1 -xz
+sudo ./ov/install_dependencies/install_openvino_dependencies.sh
+
+source ./ov/setupvars.sh
+python -m pip install -r requirements.txt && python -m convert_model.py -sd runwayml/stable-diffusion-v1-5 -b 1 -t FP16 -dyn True &
+cmake -DCMAKE_BUILD_TYPE=Release -S ./ -B ./build/
+cmake --build ./build/ --config Release -j
+wait
+
+../build/SD-generate -m runwayml/stable-diffusion-v1-5 -t FP16_dyn
