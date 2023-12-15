047a75e3d0e31c2f0c8571ee92798a17ae29bf55
Ilya Lavrenov <ilya.lavrenov@intel.com>
2023-12-15 21:10:01 +0800

    Refactored main.cpp

--- image_generation/stable_diffusion_1_5/cpp/src/main.cpp
+++ image_generation/stable_diffusion_1_5/cpp/src/main.cpp
@@ -14,28 +14,37 @@ int32_t main(int32_t argc, char* argv[]) {
             "cyberpunk cityscape like Tokyo New York  with tall buildings at dusk golden hour cinematic lighting"))(
         "n,negPrompt",
         "Defaut is empty with space",
-        cxxopts::value<std::string>()->default_value(
-            " "))("d,device", "AUTO, CPU, or GPU", cxxopts::value<std::string>()->default_value("CPU"))(
+        cxxopts::value<std::string>()->default_value(" "))(
+        "d,device",
+        "AUTO, CPU, or GPU",
+        cxxopts::value<std::string>()->default_value("CPU"))(
         "step",
         "Number of diffusion step",
         cxxopts::value<size_t>()->default_value("20"))(
         "s,seed",
         "Number of random seed to generate latent for one image output",
-        cxxopts::value<size_t>()->default_value(
-            "42"))("num", "Number of image output", cxxopts::value<size_t>()->default_value("1"))(
+        cxxopts::value<size_t>()->default_value("42"))(
+        "num",
+        "Number of image output",
+        cxxopts::value<size_t>()->default_value("1"))(
         "height",
         "height",
-        cxxopts::value<size_t>()->default_value(
-            "512"))("width", "width", cxxopts::value<size_t>()->default_value("512"))(
+        cxxopts::value<size_t>()->default_value("512"))(
+        "width",
+        "width",
+        cxxopts::value<size_t>()->default_value("512"))(
         "log",
         "generate logging into log.txt for debug",
-        cxxopts::value<bool>()->default_value(
-            "false"))("c,useCache", "use model caching", cxxopts::value<bool>()->default_value("false"))(
+        cxxopts::value<bool>()->default_value("false"))(
+        "c,useCache",
+        "use model caching",
+        cxxopts::value<bool>()->default_value("false"))(
         "e,useOVExtension",
         "use OpenVINO extension for tokenizer",
-        cxxopts::value<bool>()->default_value("false"))("r,readNPLatent",
-                                                        "read numpy generated latents from file",
-                                                        cxxopts::value<bool>()->default_value("false"))(
+        cxxopts::value<bool>()->default_value("false"))(
+        "r,readNPLatent",
+        "read numpy generated latents from file",
+        cxxopts::value<bool>()->default_value("false"))(
         "m,modelPath",
         "Specify path of SD model IR",
         cxxopts::value<std::string>()->default_value("../models/dreamlike-anime-1.0"))(
@@ -44,8 +53,10 @@ int32_t main(int32_t argc, char* argv[]) {
         cxxopts::value<std::string>()->default_value("FP16_static"))(
         "l,loraPath",
         "Specify path of lora file. (*.safetensors).",
-        cxxopts::value<std::string>()->default_value(
-            ""))("a,alpha", "alpha for lora", cxxopts::value<float>()->default_value("0.75"))("h,help", "Print usage");
+        cxxopts::value<std::string>()->default_value(""))(
+        "a,alpha",
+        "alpha for lora",
+        cxxopts::value<float>()->default_value("0.75"))("h,help", "Print usage");
     cxxopts::ParseResult result;
 
     try {
@@ -66,7 +77,7 @@ int32_t main(int32_t argc, char* argv[]) {
     const std::string device = result["device"].as<std::string>();
     uint32_t step = result["step"].as<size_t>();
     uint32_t seed = result["seed"].as<size_t>();
-    uint32_t num = result["num"].as<size_t>();
+    uint32_t num_images = result["num"].as<size_t>();
     uint32_t height = result["height"].as<size_t>();
     uint32_t width = result["width"].as<size_t>();
     const bool use_logger = result["log"].as<bool>();
@@ -74,59 +85,60 @@ int32_t main(int32_t argc, char* argv[]) {
     const bool use_OV_extension = result["useOVExtension"].as<bool>();
     const bool read_NP_latent = result["readNPLatent"].as<bool>();
     const std::string model_path = result["modelPath"].as<std::string>();
-    const std::string type = result["type"].as<std::string>();
+    const std::string precision = result["type"].as<std::string>();
     const std::string lora_path = result["loraPath"].as<std::string>();
     float alpha = result["alpha"].as<float>();
+
     if ((negative_prompt == " ") && (use_OV_extension == true)) {
-        std::cout << "Please utilize the OpenVINO extension tokenizer without an empty negative prompt.\n";
-        exit(0);
+        std::cout << "Please, use the OpenVINO extension tokenizer with non-empty negative prompt.\n";
+        return EXIT_FAILURE;
     }
-    if (!std::filesystem::exists(model_path + "/" + type)) {
-        std::cout << "Model path: " << model_path + "/" + type << "not exist\n";
-        exit(0);
+    if (!std::filesystem::exists(model_path + "/" + precision)) {
+        std::cout << "Model path: " << model_path + "/" + precision << " does not exist\n";
+        return EXIT_FAILURE;
     }
 
-    std::vector<std::string> output_vec;
+    std::vector<std::string> output_images_paths;
     std::vector<uint32_t> seed_vec;
-    std::string folderName = "images";
+    std::string folder_name = "images";
     try {
-        std::filesystem::create_directory(folderName);
-
+        std::filesystem::create_directory(folder_name);
     } catch (const std::exception& e) {
         std::cerr << "fail to create dir" << e.what() << std::endl;
     }
 
-    if (num == 1) {
+    if (num_images == 1) {
         seed_vec.push_back(seed);
-        std::string output_png_path = std::string{"./images/seed_"} + std::to_string(seed) + std::string{".bmp"};
-        output_vec.push_back(output_png_path);
+        std::string output_bmp_path = std::string{"./images/seed_"} + std::to_string(seed) + std::string{".bmp"};
+        output_images_paths.push_back(output_bmp_path);
     } else {
-        for (uint32_t n = 0; n < num; n++) {
+        for (uint32_t n = 0; n < num_images; n++) {
             seed_vec.push_back(n);
-            std::string output_png_path = std::string{"./images/seed_"} + std::to_string(n) + std::string{".bmp"};
-            output_vec.push_back(output_png_path);
+            std::string output_bmp_path = std::string{"./images/seed_"} + std::to_string(n) + std::string{".bmp"};
+            output_images_paths.push_back(output_bmp_path);
         }
     }
 
     auto start_total = std::chrono::steady_clock::now();
     stable_diffusion(positive_prompt,
-                     output_vec,
+                     output_images_paths,
                      device,
                      step,
                      seed_vec,
-                     num,
+                     num_images,
                      height,
                      width,
                      negative_prompt,
                      use_logger,
                      use_cache,
                      model_path,
-                     type,
+                     precision,
                      lora_path,
                      alpha,
                      use_OV_extension,
                      read_NP_latent);
     auto end_total = std::chrono::steady_clock::now();
     auto duration_total = std::chrono::duration_cast<std::chrono::duration<float>>(end_total - start_total);
-    return 0;
+
+    return EXIT_SUCCESS;
 }
