6b4d56420a2ad6f97a5b044d482d665767497fed
wgzintel <guozhong.wang@intel.com>
2023-12-19 11:16:48 +0800

    use stable-diffusion-v1-5 to test

--- llm_bench/python/llm_run_on_linux.sh
+++ llm_bench/python/llm_run_on_linux.sh
@@ -10,3 +10,12 @@ wait
 bemchmarking="python ./llm_bench/python/benchmark.py -m ./ov_models/bloomz-560m/pytorch/dldt/FP16/ -d cpu -n 1"
 echo ${bemchmarking}
 eval ${bemchmarking}
+
+convert_model="python ./llm_bench/python/convert.py --model_id stable-diffusion-v1-5/ --output_dir ./ov_models/stable-diffusion-v1-5 --precision FP16"
+echo ${convert_model}
+eval ${convert_model}
+wait
+
+bemchmarking="python ./llm_bench/python/benchmark.py -m ./ov_models/stable-diffusion-v1-5/pytorch/dldt/FP16/ -pf ./llm_bench/python/prompts/stable-diffusion-v2-1.jsonl -d cpu -n 1"
+echo ${bemchmarking}
+eval ${bemchmarking}
