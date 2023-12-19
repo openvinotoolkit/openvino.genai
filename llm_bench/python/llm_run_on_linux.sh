fd5c3d716190530c22081f09541913bdb2b14fea
wgzintel <guozhong.wang@intel.com>
2023-12-19 15:00:20 +0800

    test stable-diffusion-nano-2-1 on linux

--- llm_bench/python/llm_run_on_linux.sh
+++ llm_bench/python/llm_run_on_linux.sh
@@ -2,11 +2,11 @@
 # Copyright (C) 2023 Intel Corporation
 # SPDX-License-Identifier: Apache-2.0
 
-convert_model="python ./llm_bench/python/convert.py --model_id juggernaut-xl/ --output_dir ./ov_models/juggernaut-xl --precision FP16"
+convert_model="python ./llm_bench/python/convert.py --model_id stable-diffusion-nano-2-1/ --output_dir ./ov_models/stable-diffusion-nano-2-1 --precision FP16"
 echo ${convert_model}
 eval ${convert_model}
 wait
 
-bemchmarking="python ./llm_bench/python/benchmark.py -m ./ov_models/juggernaut-xl/pytorch/dldt/FP16/ -pf ./llm_bench/python/prompts/stable-diffusion-v2-1.jsonl -d cpu -n 1"
+bemchmarking="python ./llm_bench/python/benchmark.py -m ./ov_models/stable-diffusion-nano-2-1/pytorch/dldt/FP16/ -pf ./llm_bench/python/prompts/stable-diffusion-v2-1.jsonl -d cpu -n 1"
 echo ${bemchmarking}
 eval ${bemchmarking}
