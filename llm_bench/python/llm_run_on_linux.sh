68d7452825ec3ca87d9d37f6a9ec97dc9cfa11fb
wgzintel <guozhong.wang@intel.com>
2023-12-19 11:26:06 +0800

    only test statble-diffusion-v1-5

--- llm_bench/python/llm_run_on_linux.sh
+++ llm_bench/python/llm_run_on_linux.sh
@@ -2,15 +2,6 @@
 # Copyright (C) 2023 Intel Corporation
 # SPDX-License-Identifier: Apache-2.0
 
-convert_model="python ./llm_bench/python/convert.py --model_id bloomz-560m/ --output_dir ./ov_models/bloomz-560m --precision FP16"
-echo ${convert_model}
-eval ${convert_model}
-wait
-
-bemchmarking="python ./llm_bench/python/benchmark.py -m ./ov_models/bloomz-560m/pytorch/dldt/FP16/ -d cpu -n 1"
-echo ${bemchmarking}
-eval ${bemchmarking}
-
 convert_model="python ./llm_bench/python/convert.py --model_id stable-diffusion-v1-5/ --output_dir ./ov_models/stable-diffusion-v1-5 --precision FP16"
 echo ${convert_model}
 eval ${convert_model}
