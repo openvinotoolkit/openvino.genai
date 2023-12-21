0742009ddc6d98d2dafefb7c6fddfde51cb6f60e
Wovchena <vladimir.zlobin@intel.com>
2023-12-21 16:55:34 +0400

    ..

--- image_generation/stable_diffusion_1_5/cpp/set_up_and_run.sh
+++ image_generation/stable_diffusion_1_5/cpp/set_up_and_run.sh
@@ -25,7 +25,7 @@ sudo -E apt install libeigen3-dev -y
 cd scripts
 python -m pip install -U pip
 python -m pip install -r ./requirements.txt
-python -m pip install ../../../../thirdparty/openvino_contrib/modules/custom_operations/
+python -m pip install ../../../thirdparty/openvino_contrib/modules/custom_operations/
 python convert_model.py -sd runwayml/stable-diffusion-v1-5 -b 1 -t FP16 -dyn True
 convert_tokenizer ./runwayml/stable-diffusion-v1-5/FP16_dyn/tokenizer --output ../models/tokenizer/
 cd ..
