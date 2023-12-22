b5b1e7f1ea6906c7ec2ce33c89b2472f122953a6
Ekaterina Aidova <ekaterina.aidova@intel.com>
2023-12-22 12:21:23 +0400

    fix qwen model compression if model already exists: (#108)

--- llm_bench/python/convert.py
+++ llm_bench/python/convert.py
@@ -1054,14 +1054,9 @@ def convert_qwen(args):
     )
     if post_init is not None:
         model_kwargs = {"torch_dtype": torch.float32, "revision": "c02ede58c0ab0045f5e4788c35842bec6a7baa0a"}
+    model = None
     if not compression_only:
-        model = None
-        if not compression_only:
-            model = AutoModelForCausalLM.from_pretrained(args.model_id, trust_remote_code=True, **model_kwargs)
-            try:
-                model.to(torch.float32)
-            except Exception:
-                pass
+        model = AutoModelForCausalLM.from_pretrained(args.model_id, trust_remote_code=True, **model_kwargs)
         try:
             model.to(torch.float32)
         except Exception:
