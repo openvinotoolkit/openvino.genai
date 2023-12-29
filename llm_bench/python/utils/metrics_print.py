ac193fde598a7d23164a103ee6a2b9952df3be6f
wgzintel <guozhong.wang@intel.com>
2023-12-29 11:13:51 +0800

    simply log output

--- llm_bench/python/utils/metrics_print.py
+++ llm_bench/python/utils/metrics_print.py
@@ -5,7 +5,7 @@ import logging as log
 
 
 def print_metrics(
-        iter_num, iter_data, tms=None, tms_infer=None, generated=None, warm_up=False, max_rss_mem=-1, max_shared_mem=-1,
+        iter_num, iter_data, tms=None, tms_infer=None, warm_up=False, max_rss_mem=-1, max_shared_mem=-1,
         stable_diffusion=None, tokenization_time=None
 ):
     if tms is None:
@@ -15,26 +15,23 @@ def print_metrics(
     iter_str = str(iter_num)
     if warm_up:
         iter_str = 'warm-up'
+    output_str = '[{}] '.format(iter_str)
     if iter_data['input_size'] != '':
-        log.info(f"[{iter_str}] Input token size: {iter_data['input_size']}")
+        output_str += 'Input token size: {}, '.format(iter_data['input_size'])
     if iter_data['output_size'] != '':
-        log.info(f"[{iter_str}] Output size: {iter_data['output_size']}")
+        output_str += 'Output size: {}, '.format(iter_data['output_size'])
     if iter_data['infer_count'] != '':
-        log.info(f"[{iter_str}] Infer count: {iter_data['infer_count']}")
+        output_str += 'Infer count: {}, '.format(iter_data['infer_count'])
     if tokenization_time:
-        encode_time = tokenization_time[0]
-        log.info(f"[{iter_str}] Tokenization Time: {encode_time:.2f}ms")
+        output_str += 'Tokenization Time: {:.2f}ms, '.format(tokenization_time[0])
         if len(tokenization_time) > 1:
-            decode_time = tokenization_time[1]
-            log.info(f"[{iter_str}] Detokenization Time: {decode_time:.2f}ms")
+            output_str += 'Detokenization Time: {:.2f}ms, '.format(tokenization_time[1])
     if iter_data['generation_time'] != '':
-        log.info(f"[{iter_str}] Generation Time: {iter_data['generation_time']:.2f}s")
+        output_str += 'Generation Time: {:.2f}s, '.format(iter_data['generation_time'])
     if iter_data['latency'] != '':
-        log.info(f"[{iter_str}] Latency: {iter_data['latency']:.2f} ms/token")
-    if generated is not None:
-        log.info(f'[{iter_str}] Generated:\n{generated}')
-    if iter_data['result_md5'] != '':
-        log.info(f"[{iter_str}] Result MD5:{iter_data['result_md5']}")
+        output_str += 'Latency: {:.2f} ms/token'.format(iter_data['latency'])
+    if output_str != '':
+        log.info(output_str)
     if len(tms) > 0:
         iter_data['first_token_latency'] = tms[0] * 1000 if len(tms) > 0 else -1
         iter_data['other_tokens_avg_latency'] = sum(tms[1:]) / (len(tms) - 1) * 1000 if len(tms) > 1 else -1
@@ -51,10 +48,24 @@ def print_metrics(
         )
     if stable_diffusion is not None:
         print_stable_diffusion_infer_latency(iter_str, iter_data, stable_diffusion)
+    output_str = ''
     if max_rss_mem != '' and max_rss_mem > -1:
-        log.info(f'[{iter_str}] max rss memory cost:\n{max_rss_mem}')
+        output_str += '[{}] Max rss memory cost: {:.2f}, '.format(iter_str, max_rss_mem)
     if max_shared_mem != '' and max_shared_mem > -1:
-        log.info(f'[{iter_str}] max shared memory cost:\n{max_shared_mem}')
+        output_str += 'max shared memory cost: {:.2f}'.format(max_shared_mem)
+    if output_str != '':
+        log.info(output_str)
+
+
+def print_generated_and_md5(iter_num, warm_up=False, generated=None, result_md5=''):
+    iter_str = str(iter_num)    
+    if warm_up:
+        iter_str = 'warm-up'
+    if generated is not None:
+        log.info(f'[{iter_str}] Generated: {generated}')
+    if result_md5 != '':
+        log.info(f"[{iter_str}] Result MD5: {result_md5}")
+
 
 
 def print_stable_diffusion_infer_latency(iter_str, iter_data, stable_diffusion):
@@ -64,7 +75,7 @@ def print_stable_diffusion_infer_latency(iter_str, iter_data, stable_diffusion):
     iter_data['other_tokens_infer_avg_latency'] = iter_data['other_tokens_avg_latency']
     log.info(f"[{iter_str}] First step of unet latency: {iter_data['first_token_latency']:.2f} ms/step, "
              f"other steps of unet latency: {iter_data['other_tokens_avg_latency']:.2f} ms/step",)
-    log.info(f"[{iter_str}] text encoder latency: {stable_diffusion.get_text_encoder_latency():.2f} ms/step, "
+    log.info(f"[{iter_str}] Text encoder latency: {stable_diffusion.get_text_encoder_latency():.2f} ms/step, "
              f"unet latency: {stable_diffusion.get_unet_latency():.2f} ms/step, "
              f"vae decoder latency: {stable_diffusion.get_vae_decoder_latency():.2f} ms/step, "
              f"text encoder step count: {stable_diffusion.get_text_encoder_step_count()}, "
@@ -85,7 +96,7 @@ def print_ldm_unet_vqvae_infer_latency(iter_num, iter_data, tms=None, warm_up=Fa
     log.info(f"[{iter_str}] First step of unet latency: {iter_data['first_token_latency']:.2f} ms/step, "
              f"other steps of unet latency: {iter_data['other_tokens_avg_latency']:.2f} ms/step",)
     if len_tms > 1:
-        log.info(f"[{iter_str}] unet latency: {(sum(tms[0:(len_tms - 1)]) / (len_tms - 1)) * 1000:.2f} ms/step, "
+        log.info(f"[{iter_str}] Unet latency: {(sum(tms[0:(len_tms - 1)]) / (len_tms - 1)) * 1000:.2f} ms/step, "
                  f"vqvae decoder latency: {tms[len_tms - 1] * 1000:.2f} ms/step, "
                  f"unet step count: {len_tms - 1}, "
                  f"vqvae decoder step count: 1",)
@@ -112,12 +123,13 @@ def print_average(iter_data_list):
 
     if total_iters > 0:
         log.info('<<< Warm-up iteration is excluded. >>>')
-        log.info(f'[Total] Iterations: {total_iters}')
+        out_str = '[Total] Iterations: {}'.format(total_iters)
         if total_num_tokens > 0:
-            log.info(f'[Total] Output size: {total_num_tokens} tokens')
+            out_str += ', Output size: {} tokens'.format(total_num_tokens)
         if total_generation_time > 0:
             avg_per_iter_time = total_generation_time / total_iters
-            log.info(f'[Average] Iteration time: {avg_per_iter_time:.2f}s')
+            out_str += '\n[ INFO ] [Average] Iteration time: {:.2f}s'.format(avg_per_iter_time)
             if total_num_tokens > 0:
                 avg_per_token_time = total_generation_time * 1000 / total_num_tokens
-                log.info(f'[Average] Latency: {avg_per_token_time:.2f} ms/token')
+                out_str += ', Latency: {:.2f} ms/token'.format(avg_per_token_time)
+        log.info(out_str)
