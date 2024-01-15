58b865d7dd6767759bfcd6450cb1b5d8e8aeef29
eaidova <ekaterina.aidova@intel.com>
2024-01-15 17:31:49 +0400

    add mixtral support in llm_bench

--- llm_bench/python/utils/conversion_utils/convert_patch.py
+++ llm_bench/python/utils/conversion_utils/convert_patch.py
@@ -3,6 +3,7 @@
 # SPDX-License-Identifier: Apache-2.0
 
 import torch
+import torch.nn.functional as F
 from typing import Tuple, Optional
 import types
 from transformers.modeling_outputs import BaseModelOutputWithPast
@@ -230,6 +231,41 @@ def _prepare_decoder_attention_mask(attention_mask, input_shape, inputs_embeds, 
     return combined_attention_mask
 
 
+def mixtral_sparse_moe_block_forward(self, hidden_states: torch.Tensor) -> torch.Tensor:
+    """ """
+    batch_size, sequence_length, hidden_dim = hidden_states.shape
+    hidden_states = hidden_states.view(-1, hidden_dim)
+    # router_logits: (batch * sequence_length, n_experts)
+    router_logits = self.gate(hidden_states)
+
+    routing_weights = F.softmax(router_logits, dim=1, dtype=torch.float)
+    routing_weights, selected_experts = torch.topk(routing_weights, self.top_k, dim=-1)
+    routing_weights /= routing_weights.sum(dim=-1, keepdim=True)
+    # we cast back to the input dtype
+    routing_weights = routing_weights.to(hidden_states.dtype)
+
+    final_hidden_states = torch.zeros((batch_size * sequence_length, hidden_dim), dtype=hidden_states.dtype, device=hidden_states.device)
+
+    # One hot encode the selected experts to create an expert mask
+    # this will be used to easily index which expert is going to be sollicitated
+    expert_mask = torch.nn.functional.one_hot(selected_experts, num_classes=self.num_experts).permute(2, 1, 0)
+
+    # Loop over all available experts in the model and perform the computation on each expert
+    for expert_idx in range(self.num_experts):
+        expert_layer = self.experts[expert_idx]
+        idx, top_x = torch.where(expert_mask[expert_idx])
+
+        # Index the correct hidden states and compute the expert hidden state for
+        # the current expert. We need to make sure to multiply the output hidden
+        # states by `routing_weights` on the corresponding tokens (top-1 and top-2)
+        current_state = hidden_states[None, top_x].reshape(-1, hidden_dim)
+        current_hidden_states = expert_layer(current_state) * routing_weights[top_x, idx, None]
+
+        final_hidden_states.index_add_(0, top_x, current_hidden_states.to(hidden_states.dtype))
+    final_hidden_states = final_hidden_states.reshape(batch_size, sequence_length, hidden_dim)
+    return final_hidden_states, router_logits
+
+
 def patch_model_for_optimum_export(model):
     if model.config.model_type in ["stablelm_epoch", "baichuan"]:
         model.model._prepare_decoder_attention_mask = _prepare_decoder_attention_mask
@@ -241,4 +277,7 @@ def patch_model_for_optimum_export(model):
         model.model._prepare_decoder_attention_mask = _prepare_decoder_sliding_window_attention_mask
     elif model.config.model_type == "Yi":
         model.model._prepare_decoder_attention_mask = _yi_prepare_decoder_attention_mask
+    elif model.config.model_type == "mixtral":
+        for layer in model.model.layers:
+            layer.block_sparse_moe.forward = types.MethodType(mixtral_sparse_moe_block_forward, layer.block_sparse_moe)
     return model
