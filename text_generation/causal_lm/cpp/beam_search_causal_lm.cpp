c0b1397debe7c4914402a51f468acc67a988281a
Wovchena <vladimir.zlobin@intel.com>
2024-01-19 20:01:50 +0400

    Error code->EXIT_FAILURE

--- text_generation/causal_lm/cpp/beam_search_causal_lm.cpp
+++ text_generation/causal_lm/cpp/beam_search_causal_lm.cpp
@@ -86,8 +86,8 @@ int main(int argc, char* argv[]) try {
     lm.reset_state();
 } catch (const std::exception& error) {
     std::cerr << error.what() << '\n';
-    return 1;
+    return EXIT_FAILURE;
 } catch (...) {
     std::cerr << "Non-exception object thrown\n";
-    return 2;
+    return EXIT_FAILURE;
 }
