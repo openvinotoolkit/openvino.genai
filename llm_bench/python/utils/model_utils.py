150b6c2a62fd2a62bb7d43cb02a91fe7c397aa7c
Chen Peter <peter.chen@intel.com>
2024-02-23 11:13:09 +0800

    Merge master 4722fa4 (#241)
    Co-authored-by: guozhong wang <guozhong.wang@intel.com>

--- llm_bench/python/utils/model_utils.py
+++ llm_bench/python/utils/model_utils.py
@@ -133,6 +133,7 @@ def analyze_args(args):
     model_args['num_beams'] = args.num_beams
     model_args['torch_compile_backend'] = args.torch_compile_backend
     model_args['convert_tokenizer'] = args.convert_tokenizer
+    model_args['interleave'] = args.interleave
 
     model_framework = args.framework
     model_path = Path(args.model)
