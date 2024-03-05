0f534a167d8d5f76b4ab3783a9da540cdf99fedd
eaidova <ekaterina.aidova@intel.com>
2024-03-05 13:21:27 +0400

    update according stablelm model integration into transformers

--- llm_bench/python/utils/conversion_utils/export_configs.py
+++ llm_bench/python/utils/conversion_utils/export_configs.py
@@ -444,6 +444,7 @@ class ChatGLM2OpenVINOConfig(TextDecoderOnnxConfig):
 
 TasksManager._SUPPORTED_MODEL_TYPE['stablelm_epoch'] = TasksManager._SUPPORTED_MODEL_TYPE['llama']
 TasksManager._SUPPORTED_MODEL_TYPE['stablelm-epoch'] = TasksManager._SUPPORTED_MODEL_TYPE['llama']
+TasksManager._SUPPORTED_MODEL_TYPE['stablelm'] = TasksManager._SUPPORTED_MODEL_TYPE['llama']
 TasksManager._SUPPORTED_MODEL_TYPE["aquila"] = TasksManager._SUPPORTED_MODEL_TYPE["llama"]
 TasksManager._SUPPORTED_MODEL_TYPE["codegen2"] = TasksManager._SUPPORTED_MODEL_TYPE["codegen"]
 TasksManager._SUPPORTED_MODEL_TYPE["mixtral"] = TasksManager._SUPPORTED_MODEL_TYPE['mistral']
