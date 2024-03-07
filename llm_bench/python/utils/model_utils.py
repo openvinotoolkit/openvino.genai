8afb24a34682c4c2045e999790fe8fe7dae14121
wgzintel <guozhong.wang@intel.com>
2024-03-07 11:30:15 +0800

    add option --end_token_stopping

--- llm_bench/python/utils/model_utils.py
+++ llm_bench/python/utils/model_utils.py
@@ -135,7 +135,7 @@ def analyze_args(args):
     model_args['convert_tokenizer'] = args.convert_tokenizer
     model_args['subsequent'] = args.subsequent
     model_args['output_dir'] = args.output_dir
-    model_args['ignore_end'] = args.ignore_end
+    model_args['end_token_stopping'] = args.end_token_stopping
 
     model_framework = args.framework
     model_path = Path(args.model)
