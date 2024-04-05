c933ca0267e5e59d436a04b6fc3b19627d67b087
Pavel Esir <pavel.esir@intel.com>
2024-04-05 12:18:22 +0200

    cosmetic changes in main

--- text_generation/causal_lm/cpp/generate_pipeline/main.cpp
+++ text_generation/causal_lm/cpp/generate_pipeline/main.cpp
@@ -12,46 +12,37 @@ constexpr size_t BATCH_SIZE = 1;
 }  // namespace
 
 int main(int argc, char* argv[]) try {
-    // PIPELINE ex.1
-    std::string model_path = "/home/epavel/devel/openvino.genai/text_generation/causal_lm/TinyLlama-1.1B-Chat-v1.0/pytorch/dldt/FP16/";
-    // LLMPipeline pipe(model_path);
-    // std::cout << pipe.call("Alan Turing was a");
-
-    // {   
-    //     // PIPELINE ex.2
-    //     LLMPipeline pipe(model_path);
-    //     GenerationConfig config = pipe.generation_config();
-        
-    //     std::cout << pipe("Alan Turing was a", config.temperature(0.2).top_k(4).do_sample(true).repetition_penalty(1.2));
-        
-    //     // batched inputs
-    //     auto results = pipe({"table is made of ", 
-    //                         "Alan Turing was a", 
-    //                         "1 + 1 = ",
-    //                         "Why is the Sun yellow?"
-    //                         }, config.temperature(0.2).top_k(4).do_sample(true).repetition_penalty(1.2));
-        
-    //     for (const auto& res: results) {
-    //         std::cout << res << std::endl;
-    //     }
-    // }
     {
-
-GenerationConfig config = GenerationConfig().group_size(3).num_groups(3).diversity_penalty(1.2);
-config.max_length(100);
-
+        // PIPELINE Ex.1
+        std::string model_path = "/home/epavel/devel/openvino.genai/text_generation/causal_lm/TinyLlama-1.1B-Chat-v1.0/pytorch/dldt/FP16/";
+        LLMPipeline pipe(model_path);
+        std::cout << pipe.call("Alan Turing was a");
     }
 
-    {
-GenerationConfig config;
-config.m_bos_token_id = 0;
-config.m_num_groups = 3;
-config.m_group_size = 5;
-config.m_max_new_tokens = 100;
+    {   
+        // PIPELINE Ex.2
+        std::string model_path = "/home/epavel/devel/openvino.genai/text_generation/causal_lm/TinyLlama-1.1B-Chat-v1.0/pytorch/dldt/FP16/";
+        LLMPipeline pipe(model_path);
+        GenerationConfig config = pipe.generation_config();
+        
+        std::cout << pipe("Alan Turing was a", config.temperature(0.2).top_k(4).do_sample(true).repetition_penalty(1.2));
+        
+        // batched inputs 
+        // todo: tokenizer fails for batched input strings
+        auto results = pipe({"table is made of ", 
+                            "Alan Turing was a", 
+                            "1 + 1 = ",
+                            "Why is the Sun yellow?"
+                            }, config.temperature(0.2).top_k(4).do_sample(true).repetition_penalty(1.2));
+        
+        for (const auto& res: results) {
+            std::cout << res << std::endl;
+        }
     }
 
     // GENERATE
     ov::Core core;
+    std::string model_path = "/home/epavel/devel/openvino.genai/text_generation/causal_lm/TinyLlama-1.1B-Chat-v1.0/pytorch/dldt/FP16/";
     core.add_extension(OPENVINO_TOKENIZERS_PATH);  // OPENVINO_TOKENIZERS_PATH is defined in CMakeLists.txt
     ov::InferRequest tokenizer = core.compile_model(model_path + "/openvino_tokenizer.xml", "CPU").create_infer_request();
     ov::InferRequest detokenizer = core.compile_model(model_path + "/openvino_detokenizer.xml", "CPU").create_infer_request();
@@ -61,16 +52,14 @@ config.m_max_new_tokens = 100;
     ov::InferRequest request = core.compile_model(model, "CPU").create_infer_request();
     
     auto [input_ids, attention_mask] = tokenize(tokenizer, "Alan Turing was a");
-    GenerationConfig sampling_params = GenerationConfig::beam_search();
+    GenerationConfig config = GenerationConfig::beam_search();
     LLMModel engine(request);
-    GenerationResult generation_results = engine.generate(input_ids, sampling_params);
+    
+    GenerationResult generation_results = engine.generate(input_ids, config);
     std::cout << detokenize(detokenizer, generation_results[0]);
     
-    
-    // LLMModel engine(request);
-    // GenerationConfig params.temperature(0.2).top_k(4).do_sample(true).repetition_penalty(1.2);
-    // GenerationResult generation_results = engine.generate(input_ids, params);
-    // std::cout << detokenize(detokenizer, generation_results[0]);
+    generation_results = engine.generate(input_ids, config.temperature(0.2).top_k(4).do_sample(true).repetition_penalty(1.2));
+    std::cout << detokenize(detokenizer, generation_results[0]);
 
 } catch (const std::exception& error) {
     std::cerr << error.what() << '\n';
