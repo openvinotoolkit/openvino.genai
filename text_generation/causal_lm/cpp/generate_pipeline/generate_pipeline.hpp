c43e901440b3cf27bcad48625da35c033dd775d0
Pavel Esir <pavel.esir@intel.com>
2024-04-10 08:56:05 +0200

    greedy search with batches and left padding works

--- text_generation/causal_lm/cpp/generate_pipeline/generate_pipeline.hpp
+++ text_generation/causal_lm/cpp/generate_pipeline/generate_pipeline.hpp
@@ -10,30 +10,117 @@
 
 // using GenerationResult = ov::Tensor;
 using GenerationResult = std::vector<std::vector<int64_t>>;
+using namespace std;
 
-class LLMModel {
+std::pair<ov::Tensor, ov::Tensor> pad_left(ov::Tensor&& input_ids, ov::Tensor&& attention_mask, int64_t pad_token=2) {
+    const size_t batch_size = input_ids.get_shape().at(0);
+    const size_t sequence_length = input_ids.get_shape().at(1);
+    int64_t* inputs_data = input_ids.data<int64_t>();
+    int64_t* attention_mask_data = attention_mask.data<int64_t>();
+
+    for (size_t batch = 0; batch < batch_size; batch++) {
+        const size_t batch_offset = batch * sequence_length;
+
+        // last token in the sequence is not a PAD_TOKEN, skipping
+        if (inputs_data[batch_offset + sequence_length - 1] != pad_token) {
+            continue;
+        }
+
+        size_t pad_tokens_number = 0;
+        for (int i = sequence_length - 1; i >= 0; i--) {
+            const size_t token_offset = batch_offset + i;
+
+            if (inputs_data[token_offset] == pad_token) {
+                continue;
+            }
+
+            if (pad_tokens_number == 0) {
+                pad_tokens_number = sequence_length - i - 1;
+            }
+
+            std::swap(inputs_data[token_offset], inputs_data[token_offset + pad_tokens_number]);
+            std::swap(attention_mask_data[token_offset], attention_mask_data[token_offset + pad_tokens_number]);
+        }
+    }
+
+    return {input_ids, attention_mask};
+}
+
+void update_position_ids(ov::Tensor& position_ids, const ov::Tensor& attention_mask) {
+    const size_t batch_size = attention_mask.get_shape().at(0);
+    const size_t seq_length = attention_mask.get_shape().at(1);
+    position_ids.set_shape({batch_size, 1});
+
+    for (size_t batch = 0; batch < batch_size; batch++) {
+        int64_t* start = attention_mask.data<int64_t>() + batch * seq_length;
+        position_ids.data<int64_t>()[batch] = std::accumulate(start, start + seq_length - 1, 0);
+    }
+}
+
+void initialize_position_ids(ov::Tensor& position_ids, const ov::Tensor& attention_mask) {
+    const size_t batch_size = attention_mask.get_shape()[0];
+    const size_t seq_length = attention_mask.get_shape()[1];
+
+    const int64_t* attention_mask_data = attention_mask.data<int64_t>();
+    int64_t* position_ids_data = position_ids.data<int64_t>();
+
+    for (size_t batch = 0; batch < batch_size; batch++) {
+        size_t sum = 0;
+        for (size_t i = 0; i < seq_length; i++) {
+            const size_t element_offset = batch * seq_length + i;
+            position_ids_data[element_offset] = sum;
+            if (attention_mask_data[element_offset] == 1) {
+                sum += 1;
+            }
+        }
+    }
+}
+
+class LLModel {
+    ov::Tensor extend_attention(ov::Tensor attention_mask) {
+        auto shape = attention_mask.get_shape();
+        auto batch_size = shape[0];
+        auto seq_len = shape[1];
+
+        ov::Tensor new_atten_mask = ov::Tensor{attention_mask.get_element_type(), {batch_size, seq_len + 1}};
+        auto old_data = attention_mask.data<int64_t>();
+        auto new_data = new_atten_mask.data<int64_t>();
+        for (size_t batch = 0; batch < batch_size; ++batch) {
+            std::memcpy(new_data + batch * (seq_len + 1), old_data + batch * seq_len, seq_len * sizeof(int64_t));
+            new_data[batch * (seq_len + 1) + seq_len] = 1;
+        }
+        return new_atten_mask;
+    }
+
     ov::InferRequest m_model_runner;
+    
+    GenerationResult greedy_search(ov::Tensor input_ids, GenerationConfig sampling_params) {
+        auto attention_mask = ov::Tensor{ov::element::i64, input_ids.get_shape()};
+        std::fill_n(attention_mask.data<int64_t>(), attention_mask.get_size(), 1);
+        return greedy_search(input_ids, attention_mask, sampling_params);
+    }
 
-    GenerationResult greedy_search(ov::Tensor prompts, GenerationConfig sampling_params) {
-        ov::Shape prompts_shape = prompts.get_shape();
+    GenerationResult greedy_search(ov::Tensor input_ids, 
+                                   ov::Tensor attention_mask, 
+                                   GenerationConfig sampling_params) {
+        ov::Shape prompts_shape = input_ids.get_shape();
         size_t batch_size = prompts_shape[0];
-        OPENVINO_ASSERT(batch_size == 1);
         
         GenerationResult results(batch_size);
 
-        auto attention_mask = ov::Tensor{ov::element::i64, prompts.get_shape()};
-        std::fill_n(attention_mask.data<int64_t>(), attention_mask.get_size(), 1);
-        auto position_ids = ov::Tensor{ov::element::i64, prompts.get_shape()};
-        std::iota(position_ids.data<int64_t>(), position_ids.data<int64_t>() + position_ids.get_size(), 0);
-        auto initial_seq_len = prompts.get_shape()[1];
+        auto position_ids = ov::Tensor{ov::element::i64, input_ids.get_shape()};
+        // std::iota(position_ids.data<int64_t>(), position_ids.data<int64_t>() + position_ids.get_size(), 0);
+        initialize_position_ids(position_ids, attention_mask);
 
-        m_model_runner.set_tensor("input_ids", prompts);
+        size_t initial_seq_len = input_ids.get_shape()[1];
+
+        m_model_runner.set_tensor("input_ids", input_ids);
         m_model_runner.set_tensor("attention_mask", attention_mask);
         m_model_runner.set_tensor("position_ids", position_ids);
     
-        // set beam_idx for stateful model: no beam search is used and BATCH_SIZE = 1
         m_model_runner.get_tensor("beam_idx").set_shape({batch_size});
-        m_model_runner.get_tensor("beam_idx").data<int32_t>()[0] = 0;
+        auto beam_data = m_model_runner.get_tensor("beam_idx").data<int32_t>();
+        std::iota(beam_data, beam_data + batch_size, 0);
 
         for (size_t i = 0; i < sampling_params.m_max_new_tokens; ++i) {
             m_model_runner.infer();
@@ -42,11 +129,10 @@ class LLMModel {
             size_t seq_len = logits_shape[1], vocab_size = logits_shape[2];
 
             m_model_runner.get_tensor("input_ids").set_shape({batch_size, 1});
-            m_model_runner.get_tensor("attention_mask").set_shape({batch_size, m_model_runner.get_tensor("attention_mask").get_shape()[1] + 1});
-            std::fill_n(m_model_runner.get_tensor("attention_mask").data<int64_t>(), m_model_runner.get_tensor("attention_mask").get_size(), 1);
-            
-            m_model_runner.get_tensor("position_ids").set_shape({batch_size, 1});
-    
+            m_model_runner.set_tensor("attention_mask", extend_attention(m_model_runner.get_tensor("attention_mask")));
+            // m_model_runner.get_tensor("position_ids").set_shape({batch_size, 1});
+            update_position_ids(position_ids, attention_mask);
+
             for (size_t batch = 0; batch < batch_size; ++batch) {
                 const float * logits_data = logits.data<const float>() + seq_len * vocab_size * batch + (seq_len - 1) * vocab_size;
                 int64_t out_token = std::max_element(logits_data, logits_data + vocab_size) - logits_data;
@@ -137,16 +223,16 @@ class LLMModel {
     }
 
 public:
-    LLMModel(ov::InferRequest& request) :
+    LLModel(ov::InferRequest& request) :
           m_model_runner(request) {
     }
     
-    LLMModel() = default;
+    LLModel() = default;
 
     // more high level interface
-    GenerationResult generate(ov::Tensor prompts, GenerationConfig sampling_params) {
+    GenerationResult generate(ov::Tensor prompts, ov::Tensor attention_mask, GenerationConfig sampling_params) {
         if (sampling_params.is_gready_sampling()) {
-            return greedy_search(prompts, sampling_params);
+            return greedy_search(prompts, attention_mask, sampling_params);
         } else if (sampling_params.is_beam_search()) {
             return beam_search(prompts, sampling_params);
         } else {  // if (sampling_params.is_multimomial()) {
@@ -159,13 +245,48 @@ std::pair<ov::Tensor, ov::Tensor> tokenize(ov::InferRequest& tokenizer, std::str
     size_t batch_size = 1;
     tokenizer.set_input_tensor(ov::Tensor{ov::element::string, {batch_size}, &prompt});
     tokenizer.infer();
+
+    vector<vector<int64_t>> input_ids_vec;
+    input_ids_vec.reserve(1);
+    auto res_tensor = tokenizer.get_tensor("input_ids");
+    auto res_shape = res_tensor.get_shape();
+    
+    for (int i = 0; i < res_shape[0]; ++i) {
+        int64_t* start = res_tensor.data<int64_t>() + i * res_shape[1];
+        input_ids_vec.emplace_back(std::vector<int64_t>(start, start + res_shape[1]));
+    }
+
     return {tokenizer.get_tensor("input_ids"), tokenizer.get_tensor("attention_mask")};
 }
 
 std::pair<ov::Tensor, ov::Tensor> tokenize(ov::InferRequest& tokenizer, std::vector<std::string> prompts) {
-    tokenizer.set_input_tensor(ov::Tensor{ov::element::string, {prompts.size()}, &prompts});
+    tokenizer.set_input_tensor(ov::Tensor{ov::element::string, {prompts.size()}, prompts.data()});
     auto size_ = tokenizer.get_input_tensor().get_shape();
     tokenizer.infer();
+
+    pad_left(tokenizer.get_tensor("input_ids"), tokenizer.get_tensor("attention_mask"));
+    // fix mask filled with '2' instead of '0'
+    ov::Tensor attention_mask = tokenizer.get_tensor("attention_mask");
+    int64_t* attention_mask_data = attention_mask.data<int64_t>();
+    std::replace(attention_mask_data, attention_mask_data + attention_mask.get_size(), 2, 0);
+    
+    vector<vector<int64_t>> input_ids_vec;
+    vector<vector<int64_t>> atten_mask_vec;
+    
+    input_ids_vec.reserve(prompts.size());
+    atten_mask_vec.reserve(prompts.size());
+    auto res_tensor = tokenizer.get_tensor("input_ids");
+    auto atten_tensor = tokenizer.get_tensor("attention_mask");
+    auto res_shape = res_tensor.get_shape();
+    
+    for (int i = 0; i < res_shape[0]; ++i) {
+        int64_t* start = res_tensor.data<int64_t>() + i * res_shape[1];
+        input_ids_vec.emplace_back(std::vector<int64_t>(start, start + res_shape[1]));
+        
+        int64_t* atten_start = atten_tensor.data<int64_t>() + i * res_shape[1];
+        atten_mask_vec.emplace_back(std::vector<int64_t>(atten_start, atten_start + res_shape[1]));
+    }
+
     return {tokenizer.get_tensor("input_ids"), tokenizer.get_tensor("attention_mask")};
 }
 
@@ -201,7 +322,8 @@ std::vector<std::string> detokenize(ov::InferRequest& detokenizer,
         detokenizer.set_input_tensor(tokens);
         detokenizer.infer();
         auto res = detokenizer.get_output_tensor();
-        strings.emplace_back(res.data<std::string>()[0]);
+        auto res_str = res.data<std::string>()[0];
+        strings.emplace_back(res_str);
     }
     
     return strings;
@@ -243,7 +365,7 @@ struct TextStreamer {
 };
 
 class LLMPipeline {
-    LLMModel m_model_runner;
+    LLModel m_model_runner;
     ov::InferRequest m_tokenizer;
     ov::InferRequest m_detokenizer;
     std::string m_path;
@@ -254,12 +376,12 @@ public:
         if (std::experimental::filesystem::exists(m_path + "/generation_config.json")) {
             m_sampling_parameters = GenerationConfig(m_path + "/generation_config.json");
         }
-        m_sampling_parameters = GenerationConfig(m_path + "/generation_config_beam.json");
+        // m_sampling_parameters = GenerationConfig(m_path + "/generation_config_beam.json");
 
         ov::Core core;
         // The model can be compiled for GPU as well
         auto model_request = core.compile_model(m_path + "/openvino_model.xml", "CPU").create_infer_request();
-        m_model_runner = LLMModel(model_request);
+        m_model_runner = LLModel(model_request);
 
         // tokenizer and detokenizer work on CPU only
         core.add_extension(OPENVINO_TOKENIZERS_PATH);  // OPENVINO_TOKENIZERS_PATH is defined in CMakeLists.txt
@@ -274,7 +396,7 @@ public:
     std::string call(std::string text) {
         auto [input_ids, attention_mask] = tokenize(m_tokenizer, text);
 
-        auto generate_results = m_model_runner.generate(input_ids, m_sampling_parameters);
+        auto generate_results = m_model_runner.generate(input_ids, attention_mask, m_sampling_parameters);
 
         return detokenize(m_detokenizer, generate_results, 0)[0];
     }
@@ -282,7 +404,7 @@ public:
     std::string call(std::string text, GenerationConfig sampling_parameters) {
         auto [input_ids, attention_mask] = tokenize(m_tokenizer, text);
 
-        auto generate_results = m_model_runner.generate(input_ids, sampling_parameters);
+        auto generate_results = m_model_runner.generate(input_ids, attention_mask, sampling_parameters);
 
         return detokenize(m_detokenizer, generate_results, 0)[0];
     }
@@ -290,7 +412,7 @@ public:
     std::vector<std::string> call(std::vector<std::string> text, GenerationConfig sampling_parameters) {
         auto [input_ids, attention_mask] = tokenize(m_tokenizer, text);
 
-        auto generate_results = m_model_runner.generate(input_ids, sampling_parameters);
+        auto generate_results = m_model_runner.generate(input_ids, attention_mask, sampling_parameters);
 
         return detokenize(m_detokenizer, generate_results, 0);
     }
--- text_generation/causal_lm/cpp/generate_pipeline/main.cpp
+++ text_generation/causal_lm/cpp/generate_pipeline/main.cpp
@@ -11,55 +11,55 @@ constexpr size_t BATCH_SIZE = 1;
 
 }  // namespace
 
+using namespace std;
+
 int main(int argc, char* argv[]) try {
     {
-        // PIPELINE Ex.1
-        std::string model_path = "/home/epavel/devel/openvino.genai/text_generation/causal_lm/TinyLlama-1.1B-Chat-v1.0/pytorch/dldt/FP16/";
-        LLMPipeline pipe(model_path);
-        std::cout << pipe.call("Alan Turing was a");
+        // // PIPELINE Ex.1
+        // std::string model_path = "text_generation/causal_lm/TinyLlama-1.1B-Chat-v1.0/pytorch/dldt/FP16/";
+        // LLMPipeline pipe(model_path);
+        // std::cout << pipe.call("table is made of");
     }
 
-    {   
+    {
         // PIPELINE Ex.2
-        std::string model_path = "/home/epavel/devel/openvino.genai/text_generation/causal_lm/TinyLlama-1.1B-Chat-v1.0/pytorch/dldt/FP16/";
+        std::string model_path = "text_generation/causal_lm/TinyLlama-1.1B-Chat-v1.0/pytorch/dldt/FP16/";
         LLMPipeline pipe(model_path);
         GenerationConfig config = pipe.generation_config();
         
-        std::cout << pipe("Alan Turing was a", config.temperature(0.2).top_k(4).do_sample(true).repetition_penalty(1.2));
-        
-        // batched inputs 
-        // todo: tokenizer fails for batched input strings
-        auto results = pipe({"table is made of ", 
-                            "Alan Turing was a", 
+        // batched inputs
+        auto results = pipe({"table is made of", 
+                            "Alan Turing was a",
                             "1 + 1 = ",
                             "Why is the Sun yellow?"
-                            }, config.temperature(0.2).top_k(4).do_sample(true).repetition_penalty(1.2));
+                            }, config.do_sample(false));
         
         for (const auto& res: results) {
-            std::cout << res << std::endl;
+            cout << res << endl;
+            cout << "-------------------" << endl;
         }
     }
 
-    // GENERATE
-    ov::Core core;
-    std::string model_path = "/home/epavel/devel/openvino.genai/text_generation/causal_lm/TinyLlama-1.1B-Chat-v1.0/pytorch/dldt/FP16/";
-    core.add_extension(OPENVINO_TOKENIZERS_PATH);  // OPENVINO_TOKENIZERS_PATH is defined in CMakeLists.txt
-    ov::InferRequest tokenizer = core.compile_model(model_path + "/openvino_tokenizer.xml", "CPU").create_infer_request();
-    ov::InferRequest detokenizer = core.compile_model(model_path + "/openvino_detokenizer.xml", "CPU").create_infer_request();
+    // // GENERATE
+    // ov::Core core;
+    // std::string model_path = "text_generation/causal_lm/TinyLlama-1.1B-Chat-v1.0/pytorch/dldt/FP16/";
+    // core.add_extension(OPENVINO_TOKENIZERS_PATH);  // OPENVINO_TOKENIZERS_PATH is defined in CMakeLists.txt
+    // ov::InferRequest tokenizer = core.compile_model(model_path + "/openvino_tokenizer.xml", "CPU").create_infer_request();
+    // ov::InferRequest detokenizer = core.compile_model(model_path + "/openvino_detokenizer.xml", "CPU").create_infer_request();
 
-    // todo: beam search does not work properly on GPU, when reshaped from batch=1 to batch=num_beams not broadcasted
-    std::shared_ptr<ov::Model> model = core.read_model(model_path + "/openvino_model.xml");
-    ov::InferRequest request = core.compile_model(model, "CPU").create_infer_request();
+    // // todo: beam search does not work properly on GPU, when reshaped from batch=1 to batch=num_beams not broadcasted
+    // std::shared_ptr<ov::Model> model = core.read_model(model_path + "/openvino_model.xml");
+    // ov::InferRequest request = core.compile_model(model, "CPU").create_infer_request();
     
-    auto [input_ids, attention_mask] = tokenize(tokenizer, "Alan Turing was a");
-    GenerationConfig config = GenerationConfig::beam_search();
-    LLMModel engine(request);
+    // auto [input_ids, attention_mask] = tokenize(tokenizer, "Alan Turing was a");
+    // GenerationConfig config = GenerationConfig::beam_search();
+    // LLMModel engine(request);
     
-    GenerationResult generation_results = engine.generate(input_ids, config);
-    std::cout << detokenize(detokenizer, generation_results[0]);
+    // GenerationResult generation_results = engine.generate(input_ids, config);
+    // std::cout << detokenize(detokenizer, generation_results[0]);
     
-    generation_results = engine.generate(input_ids, config.temperature(0.2).top_k(4).do_sample(true).repetition_penalty(1.2));
-    std::cout << detokenize(detokenizer, generation_results[0]);
+    // generation_results = engine.generate(input_ids, config.temperature(0.2).top_k(4).do_sample(true).repetition_penalty(1.2));
+    // std::cout << detokenize(detokenizer, generation_results[0]);
 
 } catch (const std::exception& error) {
     std::cerr << error.what() << '\n';
