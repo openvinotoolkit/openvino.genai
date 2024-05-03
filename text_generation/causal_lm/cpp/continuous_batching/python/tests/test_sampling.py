4adddd1077869457fc79a7da602d85e081d67885
Ilya Lavrenov <ilya.lavrenov@intel.com>
2024-05-04 01:54:29 +0400

    LLama2 works in beam search

--- text_generation/causal_lm/cpp/continuous_batching/python/tests/test_sampling.py
+++ text_generation/causal_lm/cpp/continuous_batching/python/tests/test_sampling.py
@@ -3,6 +3,7 @@ from unittest import TestCase
 
 from transformers import AutoTokenizer, AutoModelForCausalLM
 from transformers import GenerationConfig as HFGenerationConfig
+from optimum.intel import OVModelForCausalLM
 
 from py_continuous_batching import ContinuousBatchingPipeline, GenerationConfig, SchedulerConfig, GenerationResult
 
@@ -21,16 +22,16 @@ def get_beam_search() -> GenerationConfig:
 
 def get_test_dataset() -> Tuple[List[str], List[GenerationConfig]]:
     prompts = [
-        # "What is OpenVINO?",
-        # "How are you?",
-        # "What is your name?",
+        "What is OpenVINO?",
+        "How are you?",
+        "What is your name?",
         "Tell me something about Canada"
     ]
     generation_configs = [
         get_beam_search(),
-        # get_beam_search(),
-        # get_beam_search(),
-        # get_beam_search()
+        get_beam_search(),
+        get_beam_search(),
+        get_beam_search()
     ]
     return (prompts, generation_configs)
 
@@ -38,6 +39,8 @@ def get_scheduler_config() -> SchedulerConfig:
     scheduler_config = SchedulerConfig()
     scheduler_config.dynamic_split_fuse = True
     scheduler_config.num_kv_blocks = 300
+    # scheduler_config.max_num_batched_tokens = 256
+    # scheduler_config.max_num_seqs = 256
 
     return scheduler_config
 
@@ -76,19 +79,22 @@ def convert_to_hf(
 def run_hugging_face(
     model_id : str,
     prompts: List[str],
-    generation_configs: List[GenerationConfig]
+    generation_configs: List[GenerationConfig],
+    use_optimum: bool
 ) -> List[GenerationResult]:
     tokenizer = AutoTokenizer.from_pretrained(model_id)
-    model = AutoModelForCausalLM.from_pretrained(model_id)
+    model = OVModelForCausalLM.from_pretrained(model_id, export=True) if use_optimum else \
+            AutoModelForCausalLM.from_pretrained(model_id)
     generation_results: List[GenerationResult] = []
 
     for prompt, generation_config in zip(prompts, generation_configs):
         inputs = tokenizer(prompt, return_tensors="pt")
+        prompt_len = len(inputs['input_ids'][0])
         generate_outputs = model.generate(**inputs, generation_config=convert_to_hf(generation_config), return_dict_in_generate=True)
-        all_text_batch = tokenizer.batch_decode(generate_outputs.sequences, skip_special_tokens=True)
+        all_text_batch = tokenizer.batch_decode([generated_ids[prompt_len:] for generated_ids in generate_outputs.sequences], skip_special_tokens=True)
 
         generation_result = GenerationResult()
-        generation_result.m_generation_ids = [text[len(prompt):] for text in all_text_batch]
+        generation_result.m_generation_ids = all_text_batch
         generation_result.m_scores = [score for score in generate_outputs.sequences_scores]
         generation_results.append(generation_result)
 
@@ -103,10 +109,18 @@ def run_continuous_batching(
     pipe = ContinuousBatchingPipeline(model_path, scheduler_config)
     return pipe.generate(prompts, generation_configs)
 
+# export models via
+# optimum-cli export openvino -m meta-llama/Llama-2-7b-chat-hf llama2 --convert-tokenizer --trust-remote-code
+# optimum-cli export openvino -m meta-llama/Llama-2-7b-chat-hf llama2-fp16 --convert-tokenizer --trust-remote-code --fp16
+
+# tested models:
+# - facebook/opt-125m (opt125)
+# - meta-llama/Llama-2-7b-chat-hf (llama2 or llama2-fp16)
+
 def test_check_greedy_search():
     prompts, generation_configs = get_test_dataset()
-    hf_results : List[GenerationResult] = run_hugging_face("facebook/opt-125m", prompts, generation_configs)
-    my_results : List[GenerationResult] = run_continuous_batching("/home/sandye51/Documents/Programming/git_repo/openvino.genai/build/opt125", get_scheduler_config(), prompts, generation_configs)
+    hf_results : List[GenerationResult] = run_hugging_face(model_id="meta-llama/Llama-2-7b-chat-hf", prompts=prompts, generation_configs=generation_configs, use_optimum=True)
+    my_results : List[GenerationResult] = run_continuous_batching("/home/sandye51/Documents/Programming/git_repo/openvino.genai/build/llama2", get_scheduler_config(), prompts, generation_configs)
 
     assert len(prompts) == len(hf_results)
     assert len(prompts) == len(my_results)
@@ -118,4 +132,4 @@ def test_check_greedy_search():
         # for each num_return_sequences
         for hf_text, hf_score, my_text, my_score in zip(hf_result.m_generation_ids, hf_result.m_scores, my_result.m_generation_ids, my_result.m_scores):
             assert hf_text == my_text
-            # assert hf_score == my_score
+            assert abs(hf_score - my_score) < 0.02
