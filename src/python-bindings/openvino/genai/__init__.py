02d0eae989008d14b60480ca6c935f4fa5ca5d19
Wovchena <vladimir.zlobin@intel.com>
2024-05-10 18:08:03 +0400

    import openvino for win and lin

--- src/python-bindings/openvino/genai/__init__.py
+++ src/python-bindings/openvino/genai/__init__.py
@@ -1,8 +1,8 @@
 # Copyright (C) 2024 Intel Corporation
 # SPDX-License-Identifier: Apache-2.0
 
+import openvino  # add_dll_directory for openvino lib
 import os
 
 if hasattr(os, "add_dll_directory"):
     os.add_dll_directory(os.path.dirname(__file__))
-    import openvino  # add_dll_directory for openvino lib
