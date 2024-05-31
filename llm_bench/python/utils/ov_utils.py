8a9c7117de5b63e750dbf29bfd2c606cb83bfca6
SlawomirStrehlke <slawomir.strehlke@intel.com>
2024-05-31 11:46:52 +0200

    Eliminate utils from import section

--- llm_bench/python/utils/ov_utils.py
+++ llm_bench/python/utils/ov_utils.py
@@ -9,8 +9,8 @@ import logging as log
 import torch
 import time
 import types
-import utils.hook_common as hook_common
-from utils.config_class import OV_MODEL_CLASSES_MAPPING, TOKENIZE_CLASSES_MAPPING, DEFAULT_MODEL_CLASSES
+from .hook_common import get_bench_hook
+from .config_class import OV_MODEL_CLASSES_MAPPING, TOKENIZE_CLASSES_MAPPING, DEFAULT_MODEL_CLASSES
 import openvino.runtime.opset13 as opset
 
 
@@ -168,7 +168,7 @@ def create_text_gen_model(model_path, device, **kwargs):
         if not isinstance(ov_model, OV_MODEL_CLASSES_MAPPING['t5']):
             patch_inter_processing_and_compile(ov_model, **kwargs)
         end = time.perf_counter()
-    bench_hook = hook_common.get_bench_hook(kwargs['num_beams'], ov_model)
+    bench_hook = get_bench_hook(kwargs['num_beams'], ov_model)
     from_pretrained_time = end - start
     log.info(f'From pretrained time: {from_pretrained_time:.2f}s')
     # load token
