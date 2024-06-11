8738b29b8d0fbac2c7336a22ab0d849600dffc34
Dariusz Trawinski <Dariusz.Trawinski@intel.com>
2024-06-11 11:12:17 +0200

    change default max_tokens

--- text_generation/causal_lm/cpp/continuous_batching/library/include/generation_config.hpp
+++ text_generation/causal_lm/cpp/continuous_batching/library/include/generation_config.hpp
@@ -20,7 +20,7 @@ class Sequence;
 
 struct GenerationConfig {
     // Generic
-    size_t max_new_tokens = 30;
+    size_t max_new_tokens = std::numeric_limits<std::size_t>::max();
     size_t max_length = std::numeric_limits<std::size_t>::max(); // m_max_new_tokens should have priority over m_max_length
     bool ignore_eos = false;
 
