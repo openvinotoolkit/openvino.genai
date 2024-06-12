3a47386197bc96585bb127b7f37f8497a69aa0c7
mzegla <milosz.zeglarski@intel.com>
2024-06-12 14:06:52 +0200

    remove redundant commit

--- text_generation/causal_lm/cpp/continuous_batching/library/src/continuous_batching_pipeline.cpp
+++ text_generation/causal_lm/cpp/continuous_batching/library/src/continuous_batching_pipeline.cpp
@@ -52,7 +52,6 @@ class ContinuousBatchingPipeline::Impl {
         while (requests_iterator != m_requests.end()) {
             const auto& request = *requests_iterator;
             if(request->has_finished() || request->out_of_memory() || request->handle_dropped()) {
-                // Assuming there are no running sequences at this point
                 for (const auto& sequence: request->get_sequences()) {
                     m_scheduler->free_sequence(sequence->get_id());
                 }
