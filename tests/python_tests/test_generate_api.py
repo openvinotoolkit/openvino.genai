c5d5781eabcff5caa9dcd3a6803f4d723c8a679d
Alexander Suvorov <alexander.suvorov@intel.com>
2024-06-28 09:42:07 +0200

    Use 2019 studio

--- tests/python_tests/test_generate_api.py
+++ tests/python_tests/test_generate_api.py
@@ -211,12 +211,12 @@ input_tensors_list = [
 ]
 @pytest.mark.parametrize("inputs", input_tensors_list)
 @pytest.mark.parametrize("model_descr", models_list())
-@pytest.mark.xfail(
-    raises=TypeError, 
-    reason="pybind was unable to find overloads with tensor inputs on Linux",
-    strict=False,
-    condition=sys.platform == "linux"
-)
+# @pytest.mark.xfail(
+#     raises=TypeError, 
+#     reason="pybind was unable to find overloads with tensor inputs on Linux",
+#     strict=False,
+#     condition=sys.platform == "linux"
+# )
 @pytest.mark.precommit
 def test_ov_tensors(model_descr, inputs):
     hf_ov_genai_tensors_comparison(read_model(model_descr), dict(max_new_tokens=20), *inputs)
@@ -232,12 +232,12 @@ prompts = [
 @pytest.mark.parametrize("model_descr", models_list())
 @pytest.mark.parametrize("prompt", prompts)
 @pytest.mark.precommit
-@pytest.mark.xfail(
-    raises=TypeError, 
-    reason="pybind was unable to find ov::Tensor from openvino yet",
-    strict=False,
-    condition=sys.platform in ["linux", "win32"]
-)
+# @pytest.mark.xfail(
+#     raises=TypeError, 
+#     reason="pybind was unable to find ov::Tensor from openvino yet",
+#     strict=False,
+#     condition=sys.platform in ["linux", "win32"]
+# )
 def test_genai_tokenizer_encode(model_descr, prompt):
     model_id, path, tokenizer, model, pipe = read_model(model_descr)
     tok = pipe.get_tokenizer()
@@ -267,12 +267,12 @@ encoded_prompts = [
 @pytest.mark.parametrize("model_descr", models_list())
 @pytest.mark.parametrize("encoded_prompt", encoded_prompts)
 @pytest.mark.precommit
-@pytest.mark.xfail(
-    raises=TypeError, 
-    reason="pybind was unable to find ov::Tensor from openvino yet",
-    strict=False,
-    condition=sys.platform in ["linux", "win32"]
-)
+# @pytest.mark.xfail(
+#     raises=TypeError, 
+#     reason="pybind was unable to find ov::Tensor from openvino yet",
+#     strict=False,
+#     condition=sys.platform in ["linux", "win32"]
+# )
 def test_genai_tokenizer_decode(model_descr, encoded_prompt):
     model_id, path, tokenizer, model, pipe = read_model(model_descr)
     tok = pipe.get_tokenizer()
@@ -618,11 +618,11 @@ def test_load_special_tokens_3(model_tmp_path):
 
 
 @pytest.mark.precommit
-@pytest.mark.xfail(
-    raises=AssertionError, 
-    reason="CVS-143410 ov tokenizer should be aligned with hf",
-    strict=False,
-)
+# @pytest.mark.xfail(
+#     raises=AssertionError, 
+#     reason="CVS-143410 ov tokenizer should be aligned with hf",
+#     strict=False,
+# )
 def test_load_special_tokens_4(model_tmp_path):
     # only string representation is provided, find token integers by inference
     model_id, temp_path = model_tmp_path
