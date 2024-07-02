12fd8dcff8ef7b9e05fd129ba57105cb9c7200e8
Vasily Shamporov <vasily.shamporov@intel.com>
2024-07-02 13:24:41 +0200

    Extract cache tensor ID calculation

--- src/cpp/src/paged_attention_transformations.cpp
+++ src/cpp/src/paged_attention_transformations.cpp
@@ -5,6 +5,7 @@
 #include "openvino/pass/sdpa_to_paged_attention.hpp"
 
 #include "paged_attention_transformations.hpp"
+#include "cache_manager.hpp"
 
 namespace ov::genai {
 inline ov::PartialShape to_partial_with_dyn_0_dim(const ov::Shape& static_shape) {
@@ -26,19 +27,20 @@ void apply_paged_attention_transformations(std::shared_ptr<ov::Model> model, Dev
     });
 
     // extract num_kv_heads and head_size
-    size_t kv_caches_inputs_offset = 2;
-    ov::PartialShape k_shape = parameters[kv_caches_inputs_offset]->get_partial_shape();
+    ov::PartialShape k_shape = parameters[KV_CACHES_INPUTS_OFFSET]->get_partial_shape();
     OPENVINO_ASSERT(k_shape.rank().get_length() == 3, "KV cache shape is expected to have rank 3, while shape is ", k_shape);
     size_t num_kv_heads = k_shape[1].get_length(), head_size = k_shape[2].get_length();
 
     device_config.set_model_params(num_kv_heads, head_size, num_layers);
 
     for (size_t decoder_layer_id = 0; decoder_layer_id < num_layers; ++decoder_layer_id) {
-        parameters[kv_caches_inputs_offset + 2 * decoder_layer_id]->set_element_type(device_config.get_cache_precision());
-        parameters[kv_caches_inputs_offset + 2 * decoder_layer_id + 1]->set_element_type(device_config.get_cache_precision());
+        auto key_cache_id = get_key_cache_tensor_id_for_decoder_layer(decoder_layer_id);
+        auto value_cache_id = get_value_cache_tensor_id_for_decoder_layer(decoder_layer_id);
+        parameters[key_cache_id]->set_element_type(device_config.get_cache_precision());
+        parameters[value_cache_id]->set_element_type(device_config.get_cache_precision());
         // TODO: CVS-145270
-        parameters[kv_caches_inputs_offset + 2 * decoder_layer_id]->set_partial_shape(to_partial_with_dyn_0_dim(device_config.get_key_cache_shape()));
-        parameters[kv_caches_inputs_offset + 2 * decoder_layer_id + 1]->set_partial_shape(to_partial_with_dyn_0_dim(device_config.get_value_cache_shape()));
+        parameters[key_cache_id]->set_partial_shape(to_partial_with_dyn_0_dim(device_config.get_key_cache_shape()));
+        parameters[value_cache_id]->set_partial_shape(to_partial_with_dyn_0_dim(device_config.get_value_cache_shape()));
     }
     model->validate_nodes_and_infer_types();
 }
