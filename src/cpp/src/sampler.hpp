efea34db679db0ba6a5bc04aeb8f9e32b0cb2083
Alexander Suvorov <alexander.suvorov@intel.com>
2024-07-11 17:12:31 +0200

    remove debug printing

--- src/cpp/src/sampler.hpp
+++ src/cpp/src/sampler.hpp
@@ -227,25 +227,12 @@ class Sampler {
         std::vector<float> multinomial_weights(logit_vector.size());
         for (size_t i = 0; i < logit_vector.size(); i++) multinomial_weights[i] = logit_vector[i].m_log_prob;
 
-        for (auto weight : multinomial_weights) {
-            std::cout << weight << ",";
-        }
-        std::cout << std::endl;
-
-        auto rng_n = rng_engine2();
-
         auto dist = std::discrete_distribution<size_t>(multinomial_weights.begin(), multinomial_weights.end()); // equivalent to multinomial with number of trials == 1
         std::vector<Token> out_tokens;
         for (size_t token_idx = 0; token_idx < num_tokens_per_sequence; ++token_idx) {
             size_t element_to_pick = dist(rng_engine);
-            
-            std::cout << rng_n << ":" << element_to_pick << ",";
-
             out_tokens.push_back(logit_vector[element_to_pick]);
         }
-
-        std::cout << std::endl;
-        std::cout << std::endl;
 
         return out_tokens;
     }
@@ -254,17 +241,13 @@ class Sampler {
     std::map<uint64_t, GroupBeamSearcher> m_beam_search_info;
 
     std::mt19937 rng_engine;
-    std::mt19937 rng_engine2;
     // { request_id, logit_processor }
     std::map<uint64_t, LogitProcessor> m_logit_processors;
 
 public:
     SamplerOutput sample(std::vector<SequenceGroup::Ptr> & sequence_groups, ov::Tensor logits);
 
-    void set_seed(size_t seed) { 
-        rng_engine.seed(seed);
-        rng_engine2.seed(seed);
-    }
+    void set_seed(size_t seed) { rng_engine.seed(seed); }
 };
 
 SamplerOutput Sampler::sample(std::vector<SequenceGroup::Ptr> & sequence_groups, ov::Tensor logits) {
