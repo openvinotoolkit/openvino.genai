d223d68f81742bd035958ff168ba36641dc0a071
Wovchena <vladimir.zlobin@intel.com>
2024-07-15 09:04:11 +0400

    -am cache

--- tests/python_tests/test_generate_api.py
+++ tests/python_tests/test_generate_api.py
@@ -11,6 +11,7 @@ import openvino as ov
 import sys
 from pathlib import Path
 import torch
+import functools
 import math
 from ov_genai_test_utils import (
     get_models_list, 
@@ -674,6 +675,7 @@ def test_left_pad():
     run_hf_ov_genai_comparison_batched(models, config, prompts)
 
 
+@functools.lru_cache(1)
 def get_continuous_batching(path):
     return ov_genai.LLMPipeline(str(path), ov_genai.Tokenizer(str(path)), 'CB')
 
