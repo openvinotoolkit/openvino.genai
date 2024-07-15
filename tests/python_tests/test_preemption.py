eb5aa0067f00a2d387872fee437ca238aa27018b
Alexander Suvorov <alexander.suvorov@intel.com>
2024-07-15 14:27:40 +0200

    Unskip tests

--- tests/python_tests/test_preemption.py
+++ tests/python_tests/test_preemption.py
@@ -9,7 +9,7 @@ from common import get_model_and_tokenizer, save_ov_model_from_optimum, generate
     DEFAULT_SCHEDULER_CONFIG, get_scheduler_config, run_test_pipeline, get_models_list, get_beam_search, get_greedy, \
     get_multinomial_all_parameters, get_multinomial_temperature_and_num_return_sequence, \
     get_multinomial_temperature_and_top_k, get_multinomial_temperature, get_multinomial_temperature_and_top_p
-from test_sampling import RandomSamplingTestStruct
+from test_sampling import RandomSamplingTestStruct, RandomSamplingRefTexts
 
 def get_greedy_seq_len_300() -> GenerationConfig:
     generation_config = GenerationConfig()
@@ -38,23 +38,48 @@ scheduler_params_list = [({"num_kv_blocks": 2, "block_size": 32, "dynamic_split_
 def test_preemption(tmp_path, params):
     run_test_pipeline(tmp_path, "facebook/opt-125m", params[0], params[1])
 
-multinomial_params = RandomSamplingTestStruct(generation_config=[get_multinomial_temperature(),
-                                                          get_multinomial_temperature_and_top_p(),
-                                                          get_multinomial_temperature_and_top_k()],
-                                                       prompts=["What is OpenVINO?",
-                                                                "How are you?",
-                                                                "Tell me something about Canada?",
-                                                                ],
-                                                       ref_texts=[ ["\n\nOpenVINO is a live platform that allows users to create and manage a new library for open source applications.\n\nOpenVINO is"],
-                                                                   ["  You're getting much better results from doing this, than you are by not doing this.  I have a BH and I was so far"],
-                                                                   ["\nI'm from Canada, and I'm from the US, so I'm not sure.\nI think you mean the Canadian version."]])
 
+multinomial_params = RandomSamplingTestStruct(
+    generation_config=[
+        get_multinomial_temperature(),
+        get_multinomial_temperature_and_top_p(),
+        get_multinomial_temperature_and_top_k(),
+    ],
+    prompts=[
+        "What is OpenVINO?",
+        "How are you?",
+        "Tell me something about Canada?",
+    ],
+    ref_texts=RandomSamplingRefTexts(
+        linux=[
+            [
+                "\n\nOpenVINO is a live platform that allows users to create and manage a new library for open source applications.\n\nOpenVINO is"
+            ],
+            [
+                "  You're getting much better results from doing this, than you are by not doing this.  I have a BH and I was so far"
+            ],
+            [
+                "\nI'm from Canada, and I'm from the US, so I'm not sure.\nI think you mean the Canadian version."
+            ],
+        ],
+        win32=[
+            [
+                "\n\nOpenVINO is a live platform that allows users to create and manage a new library of applications on the Virtuoso server, which can"
+            ],
+            [
+                "  You're getting much better results from doing this, than you are by not doing this.  If you are truly trying to do something good,"
+            ],
+            [
+                "\nI'm from Canada, and I'm from the US, so I'm not sure what you're talking about.\nI'm Canadian and I"
+            ],
+        ],
+    ).get_ref_texts(),
+)
 
 
 # todo: Anastasiia Pnevskaya: fix the test because it is hanging according max_new_tokens = std::numeric_limits<std::size_t>::max()
 @pytest.mark.parametrize("dynamic_split_fuse", [True, False])
 @pytest.mark.precommit
-@pytest.mark.xfail(raises=AssertionError, reason="assert ref_text == ov_text fails in CI.", condition=sys.platform in ["win32", "darwin"], strict=True)
 def test_preemption_with_multinomial(tmp_path, dynamic_split_fuse):
     generation_configs = multinomial_params.generation_config
     for config in generation_configs:
@@ -69,36 +94,73 @@ def test_preemption_with_multinomial(tmp_path, dynamic_split_fuse):
     scheduler_config = get_scheduler_config({"num_kv_blocks": 3, "block_size": 32, "dynamic_split_fuse": dynamic_split_fuse, "max_num_batched_tokens": 256, "max_num_seqs": 256})
     generate_and_compare_with_reference_text(model_path, multinomial_params.prompts, multinomial_params.ref_texts, generation_configs, scheduler_config)
 
-multinomial_params_n_seq = RandomSamplingTestStruct(generation_config=[
+
+multinomial_params_n_seq = RandomSamplingTestStruct(
+    generation_config=[
         get_multinomial_temperature(),
         get_multinomial_temperature_and_num_return_sequence(),
         get_multinomial_all_parameters(),
     ],
     prompts=[
-            "Artificial intelligence ",
-            "What is the current",
-            "Tell me something about UAE?",
+        "Artificial intelligence ",
+        "What is the current",
+        "Tell me something about UAE?",
+    ],
+    ref_texts=RandomSamplingRefTexts(
+        linux=[
+            [
+                "\nI've seen this expression used too many times without making sense.\nAs an AI engineer, and as a scientist, we should make everything easier"
             ],
-    ref_texts=[
-        [
-            "\nI've seen this expression used too many times without making sense.\nAs an AI engineer, and as a scientist, we should make everything easier"
+            [
+                ' position of the Z-shaped groove?\n0.41\nWhat is the current position of the Z-shaped groove?\n0.11\n',
+                " status of all of this? I can't stop thinking about it.\nIt's been a while since I've seen it. I found it a",
+                ' status of your blog? Do you accept feedback?\nYes, I’m happy to accept feedback at this time (I’m a'
+            ],
+            [
+                "\nIt's in the middle of nowhere if you haven’t seen one yet! It might be more convenient there than anywhere else.. maybe take",
+                "\nUAE is a country with some great culture that has been living under Islamic oppression for almost 60 years now (including 20 years as part of Arab",
+                "\nNope, just wanted to say how awesome and beautiful it was when my brother came back from an adventure trip across Asia - our 2nd year",
+                "\nI don't know anything.  I'm not sure what kind this sub wants though... but apparently they are pretty bad at making videos/photos",
+            ],
         ],
-        [
-            ' significance of 3862?\n3829\nWhat is the greatest common divisor of 15 and 7763?\n9\nCalculate the',
-            ' third derivative of 939*v**3*r**2 + 133*v**3*r**2 + v**3 - 16*',
-            " climate in the future?  Do we have things to catch on fire, and if so does that mean we'll have a new climate change or is"
+        win32=[
+            [
+                "\nI've had a friend with the capacity to test this in his own words.\nThe big problem with real-world results is the economics of"
+            ],
+            [
+                ' position of the patent application number of the present invention?\n\nIn the present invention, the present invention relates to an improved method for manufacturing a semic',
+                ' status of your town? How many houses do you have?\nThere are about three houses in our town. The closest place to us is about 25',
+                " status of all the other passengers?\nWe're the only ones left, so no...\nI don't think they'll really leave.\nThey"
+            ],
+            [
+                "\nI don't have any knowledge on them. We are based out near Dubai so hopefully they will take care of us soon enough :) thanks though :",
+                "\nUAE is not one of the richest countries in Asia but definitely among those most corrupt nations because this corruption (and its own endemic practices) still",
+                "\nNope, I'm just going through my first semester there right now and it was nice to see some people who were doing well haha - we",
+                "\nIt's a country where your parents can never give you anything at all!  It also has an extremely low education system for many years... You",
+            ],
         ],
-        [
-            "\nIt's in the middle of nowhere if you haven’t seen one yet! It might be more convenient there than anywhere else.. maybe take",
-            '\nUAE is a country with some great culture that has been living under Islamic oppression for almost 60 years now (including 20 years as part of Arab',
-            '\nNope, just wanted to say how awesome and beautiful it was when my brother came back from an adventure trip across Asia - our 2nd year',
-            '\nI don\'t know anything.  I\'m not sure what kind this sub wants though... but apparently they are pretty bad at making videos/photos'
+        darwin=[
+            [
+                "\nI've had a friend with the capacity to test this in his own words.\nThe big problem with real-world results is the rigidity"
+            ],
+            [
+               ' position of the patent application number of the present invention?\n\nIn the present invention, the present invention relates to an improved method for manufacturing a semic',
+               ' status of your town? How many houses do you have?\nThere are about three houses in our town. The closest place to us is about 25',
+               " status of all the other passengers?\nWe're the only ones left, so no...\nI don't think they'll really leave.\nThey"
+            ],
+            [
+                "\nI don't have any knowledge on them. We are based out near Dubai so hopefully they will take care of us soon enough :) thanks though :",
+                "\nUAE is not one of the richest countries in Asia but definitely among those most corrupt nations because this corruption (and its own endemic practices) still",
+                "\nNope, I'm just going through my first semester there right now and it was nice to see some people who were doing well haha - we",
+                "\nIt's a country where your parents can never give you anything at all!  It also has an extremely low education system for many years... You",
+            ],
         ],
-    ])
+    ).get_ref_texts(),
+)
+
 
 @pytest.mark.parametrize("dynamic_split_fuse", [True, False])
 @pytest.mark.precommit
-@pytest.mark.xfail(reason="assert ref_text == ov_text fails", condition=sys.platform in ["win32", "darwin"])
 def test_preemption_with_multinomial_n_seq(tmp_path, dynamic_split_fuse):
     generation_configs = multinomial_params_n_seq.generation_config
     for config in generation_configs:
