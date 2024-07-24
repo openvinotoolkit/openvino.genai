04012f473c0eac190701926366e9b05704b80196
Alexander Suvorov <alexander.suvorov@intel.com>
2024-07-24 09:40:37 +0200

    Skip test_preemption_with_multinomial_n_seq (#667)
    Random sampling

--- tests/python_tests/test_preemption.py
+++ tests/python_tests/test_preemption.py
@@ -161,6 +161,7 @@ multinomial_params_n_seq = RandomSamplingTestStruct(
 
 @pytest.mark.parametrize("dynamic_split_fuse", [True, False])
 @pytest.mark.precommit
+@pytest.mark.skip(reason="Random sampling results are non deterministic due to: discrete_distribution impl depends on platform, model inference results may depend on CPU. Test passes on CI but fails locally.")
 def test_preemption_with_multinomial_n_seq(tmp_path, dynamic_split_fuse):
     generation_configs = multinomial_params_n_seq.generation_config
     for config in generation_configs:
