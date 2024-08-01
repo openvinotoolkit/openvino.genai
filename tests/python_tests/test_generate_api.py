4962039c803d13b4aacd5fd235d46cac195e7fcf
Wovchena <vladimir.zlobin@intel.com>
2024-08-01 10:46:55 +0400

    skip

--- tests/python_tests/test_generate_api.py
+++ tests/python_tests/test_generate_api.py
@@ -711,6 +711,7 @@ def test_left_pad():
 @pytest.mark.parametrize("prompt", batched_prompts)
 @pytest.mark.parametrize("model_descr", get_models_list())
 @pytest.mark.precommit
+@pytest.mark.skip("continuous_batching seg faults with nightly ov. Ticket 147793")
 def test_continuous_batching_vs_stateful(model_descr, prompt, generation_config):
     model_id, path, tokenizer, model, stateful = read_model((
         "TinyLlama/TinyLlama-1.1B-Chat-v1.0",
@@ -730,6 +731,7 @@ def test_continuous_batching_vs_stateful(model_descr, prompt, generation_config)
 @pytest.mark.parametrize("prompt", prompts)
 @pytest.mark.parametrize("model_descr", get_models_list())
 @pytest.mark.precommit
+@pytest.mark.skip("continuous_batching seg faults with nightly ov. Ticket 147793")
 def test_cb_streamer_vs_return_vs_stateful(model_descr, prompt):
     model_id, path, tokenizer, model, stateful = read_model((
         "TinyLlama/TinyLlama-1.1B-Chat-v1.0",
