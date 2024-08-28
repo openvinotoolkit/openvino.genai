601c8a92324800b57df7c3affe4769e637de4907
Oleg Pipikin <oleg.pipikin@intel.com>
2024-08-28 13:01:45 +0200

    Improve py docs (#803)
    CVS-149422

--- src/python/openvino_genai/__init__.py
+++ src/python/openvino_genai/__init__.py
@@ -1,6 +1,8 @@
 # Copyright (C) 2024 Intel Corporation
 # SPDX-License-Identifier: Apache-2.0
 
+"""openvino genai module namespace, exposing pipelines and configs to create these pipelines."""
+
 import openvino  # add_dll_directory for openvino lib
 import os
 from .__version__ import __version__
