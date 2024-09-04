675c8f04b44242bddcdad467e5922b0ef04fcbe3
Wovchena <vladimir.zlobin@intel.com>
2024-09-04 13:29:49 +0400

    Beautify sample

--- samples/cpp/visual_language_chat/visual_language_chat.cpp
+++ samples/cpp/visual_language_chat/visual_language_chat.cpp
@@ -5,10 +5,8 @@
 #include <openvino/genai/vlm_pipeline.hpp>
 #include <openvino/runtime/intel_gpu/properties.hpp>
 
-namespace {
 bool callback(std::string&& subword) {
     return !(std::cout << subword);
-}
 }
 
 int main(int argc, char* argv[]) try {
@@ -17,35 +15,28 @@ int main(int argc, char* argv[]) try {
     }
     ov::Tensor image = utils::load_image(argv[2]);
     std::string device = "CPU";  // GPU can be used as well
-    ov::AnyMap enable_compile_cache;// = {ov::cache_dir("vlm_cache")};
+    ov::AnyMap enable_compile_cache;
+    if ("GPU" == device) {
+        // Cache compile models on disks for GPU to save time on the
+        // next run. It's not beneficial for CPU.
+        enable_compile_cache.insert({ov::cache_dir("vlm_cache")});
+    }
     ov::genai::VLMPipeline pipe(argv[1], device, enable_compile_cache);
     pipe.start_chat();
 
-    std::array<std::string, 3> prompts;
     std::string prompt;
     std::cout << "question:\n";
     if (!std::getline(std::cin, prompt)) {
         throw std::runtime_error("std::cin failed");
     }
-    prompts[0] = prompt;
     pipe.generate({prompt, image}, callback);
-    std::cout << "\nquestion:\n";
+    std::cout << "\n----------\n"
+        "question:\n";
     size_t counter = 1;
     while (std::getline(std::cin, prompt)) {
-        prompts[counter] = prompt;
         pipe.generate({prompt}, callback);
         std::cout << "\n----------\n"
             "question:\n";
-        ++counter;
-        if (3 == counter) {
-            break;
-        }
-    }
-    for (size_t limit = 0; ; ++limit) {
-        std::cout << limit << '\n';
-        for (size_t i = 0; i < 3; ++i) {
-            pipe.generate({prompts[i]});
-        }
     }
     pipe.finish_chat();
 } catch (const std::exception& error) {
