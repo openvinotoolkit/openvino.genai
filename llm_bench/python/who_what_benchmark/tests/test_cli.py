3ba0ccddfe0275fea3e791cf4b8f13e19cfa24ea
Alexander <kozzzloff@list.ru>
2024-09-19 11:12:14 +0400

    Added workflow for WWB tests

--- llm_bench/python/who_what_benchmark/tests/test_cli.py
+++ llm_bench/python/who_what_benchmark/tests/test_cli.py
@@ -1,44 +1,54 @@
-import subprocess
+import subprocess  # nosec B404
 import os
 import shutil
 import tempfile
 import pandas as pd
 import pytest
+import logging
 
 from transformers import AutoTokenizer
 from optimum.intel.openvino import OVModelForCausalLM, OVWeightQuantizationConfig
 
 
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+
 def run_wwb(args):
-    print(" ".join(["wwb"] + args))
+    logger.info(" ".join(["wwb"] + args))
     result = subprocess.run(
         ["wwb"] + args,
         capture_output=True,
         text=True
     )
-    print(result)
+    logger.info(result)
     return result
+
 
 model_id = "facebook/opt-125m"
 tmp_dir = tempfile.mkdtemp()
 base_model_path = os.path.join(tmp_dir, "opt125m")
 target_model_path = os.path.join(tmp_dir, "opt125m_int8")
 
+
 def setup_module():
-    print("Create models")
+    logger.info("Create models")
     tokenizer = AutoTokenizer.from_pretrained(model_id)
     base_model = OVModelForCausalLM.from_pretrained(model_id)
     base_model.save_pretrained(base_model_path)
     tokenizer.save_pretrained(base_model_path)
 
-    target_model = OVModelForCausalLM.from_pretrained(model_id, 
-        quantization_config=OVWeightQuantizationConfig(bits=8))
+    target_model = OVModelForCausalLM.from_pretrained(
+        model_id, quantization_config=OVWeightQuantizationConfig(bits=8)
+    )
     target_model.save_pretrained(target_model_path)
     tokenizer.save_pretrained(target_model_path)
 
+
 def teardown_module():
-    print("Remove models")
+    logger.info("Remove models")
     shutil.rmtree(tmp_dir)
+
 
 def test_target_model():
     result = run_wwb([
@@ -49,13 +59,12 @@ def test_target_model():
     ])
     assert result.returncode == 0
     assert "Metrics for model" in result.stdout
-    assert not "## Reference text" in result.stdout
+    assert "## Reference text" not in result.stdout
+
 
 @pytest.fixture
 def test_gt_data():
-    # with tempfile.TemporaryDirectory() as tmp:
-    #     temp_file = os.path.join(tmp.mkdtemp(), "gt.csv")
-    with tempfile.NamedTemporaryFile(suffix=".csv")  as tmpfile:
+    with tempfile.NamedTemporaryFile(suffix=".csv") as tmpfile:
         temp_file_name = tmpfile.name
 
     result = run_wwb([
@@ -67,18 +76,14 @@ def test_gt_data():
         "--num-samples", "2",
         "--device", "CPU"
     ])
-    print(result)
-    if os.path.exists(temp_file_name):
-        print("GT file was created")
-    else:
-        print("No GT file was created")
     import time
     time.sleep(1)
     data = pd.read_csv(temp_file_name)
     os.remove(temp_file_name)
-        
+
     assert result.returncode == 0
     assert len(data["questions"].values) == 2
+
 
 def test_output_directory():
     with tempfile.TemporaryDirectory() as temp_dir:
@@ -94,6 +99,7 @@ def test_output_directory():
         assert os.path.exists(os.path.join(temp_dir, "metrics_per_qustion.csv"))
         assert os.path.exists(os.path.join(temp_dir, "metrics.csv"))
 
+
 def test_verbose():
     result = run_wwb([
         "--base-model", base_model_path,
@@ -105,8 +111,9 @@ def test_verbose():
     assert result.returncode == 0
     assert "## Reference text" in result.stdout
 
+
 def test_language_autodetect():
-    with tempfile.NamedTemporaryFile(suffix=".csv")  as tmpfile:
+    with tempfile.NamedTemporaryFile(suffix=".csv") as tmpfile:
         temp_file_name = tmpfile.name
 
     result = run_wwb([
@@ -117,6 +124,6 @@ def test_language_autodetect():
     ])
     data = pd.read_csv(temp_file_name)
     os.remove(temp_file_name)
-        
+
     assert result.returncode == 0
     assert "马克" in data["questions"].values[0]
