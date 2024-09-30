378edb58c2d6bf295843352b11947e39700669ce
Sergey Lyalin <sergey.lyalin@gmail.com>
2024-09-30 14:54:48 +0000

    Removed unused extract_adapters_from_properties variant with std::optional. Fixed bug there with incorrect iterator used for property deletion.

--- src/cpp/src/lora_helper.cpp
+++ src/cpp/src/lora_helper.cpp
@@ -4,27 +4,15 @@
 namespace ov {
 namespace genai {
 
-std::optional<AnyMap> extract_adapters_from_properties (
-        const AnyMap& properties,
-        std::optional<AdapterConfig>& adapter_config) {
+std::optional<AnyMap> extract_adapters_from_properties (const AnyMap& properties, AdapterConfig& adapter_config) {
     auto adapters_iter = properties.find(AdaptersProperty::name());
     if (adapters_iter != properties.end()) {
         adapter_config = std::move(adapters_iter->second.as<AdapterConfig>());
         auto filtered_properties = properties;
-        filtered_properties.erase(adapters_iter);
+        filtered_properties.erase(AdaptersProperty::name());
         return filtered_properties;
     }
     return std::nullopt;
-}
-
-std::optional<AnyMap> extract_adapters_from_properties (
-        const AnyMap& properties,
-        AdapterConfig& adapter_config) {
-    std::optional<AdapterConfig> adapter_config_tmp;
-    if(auto updated_properties = extract_adapters_from_properties(properties, adapter_config_tmp)) {
-        adapter_config = std::move(*adapter_config_tmp);
-        return updated_properties;
-    }
 }
 
 }
--- src/cpp/src/lora_helper.hpp
+++ src/cpp/src/lora_helper.hpp
@@ -8,16 +8,10 @@
 namespace ov {
 namespace genai {
 
-// Search for `adapters` property in `properties` map. If it is found, set adapter_config with value of the found property and
-// return a copy of properties with the `adapter` property removed. If there is no `adapters` property, `adapter_config` is
+// Search for `adapters` property in `properties` map. If it is found, set adapter_config with found value and
+// return a copy of properties with the `adapter` removed. If there is no `adapters` property, `adapter_config` is
 // left unchanged and std::nullopt is returned.
-std::optional<AnyMap> extract_adapters_from_properties (
-    const AnyMap& properties,
-    std::optional<AdapterConfig>& adapter_config);
-
-std::optional<AnyMap> extract_adapters_from_properties (
-    const AnyMap& properties,
-    AdapterConfig& adapter_config);
+std::optional<AnyMap> extract_adapters_from_properties (const AnyMap& properties, AdapterConfig& adapter_config);
 
 }
 }
