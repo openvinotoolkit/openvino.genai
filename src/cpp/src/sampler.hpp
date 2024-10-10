dcb23365d634f431aa1b23ec79fce7a8680e597d
Irina Efode <irina.efode@intel.com>
2024-10-10 14:24:01 +0400

    [CB] Add validation mode to Sampler (#904)
    Tickets:
    * [153600](https://jira.devtools.intel.com/browse/CVS-153600)
    * [153601](https://jira.devtools.intel.com/browse/CVS-153601)

--- src/cpp/src/sampler.hpp
+++ src/cpp/src/sampler.hpp
@@ -41,10 +41,11 @@ struct SamplerOutput {
 class Sampler {
     class GroupBeamSearcher;
 
-    Logits _get_logit_vector(ov::Tensor logits, size_t batch_idx = 1);
+    Logits _get_logit_vector(ov::Tensor logits, size_t batch_idx, size_t token_idx);
     Token _greedy_sample(const Logits& logits) const;
     std::vector<Token> _multinomial_sample(const Logits& logits, size_t num_tokens_per_sequence);
     std::vector<int64_t> _try_finish_generation(SequenceGroup::Ptr & sequence_group);
+    void update_logit_processor(uint64_t request_id, uint64_t token_id);
 
     // request ID => beam search tracking information
     std::map<uint64_t, GroupBeamSearcher> m_beam_search_info;
@@ -56,9 +57,10 @@ class Sampler {
     Tokenizer m_tokenizer;
 
 public:
+    Sampler() = default;
     Sampler(Tokenizer & tokenizer) : m_tokenizer(tokenizer) {};
 
-    SamplerOutput sample(std::vector<SequenceGroup::Ptr> & sequence_groups, ov::Tensor logits);
+    SamplerOutput sample(std::vector<SequenceGroup::Ptr> & sequence_groups, ov::Tensor logits, bool is_validation_mode_enabled = false);
     void set_seed(size_t seed) { rng_engine.seed(seed); }
     void clear_beam_search_info(uint64_t request_id);
 };
