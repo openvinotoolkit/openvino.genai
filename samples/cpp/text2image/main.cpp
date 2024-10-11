316c8ffb35fa1bea5b5eaf730088b9534cbb147f
Ilya Lavrenov <ilya.lavrenov@intel.com>
2024-10-11 09:29:27 +0200

    Fixed review comments

--- samples/cpp/text2image/main.cpp
+++ samples/cpp/text2image/main.cpp
@@ -15,7 +15,8 @@ int32_t main(int32_t argc, char* argv[]) try {
     ov::Tensor image = pipe.generate(prompt,
         ov::genai::width(512),
         ov::genai::height(512),
-        ov::genai::num_inference_steps(20));
+        ov::genai::num_inference_steps(20),
+        ov::genai::num_images_per_prompt(1));
 
     // writes `num_images_per_prompt` images by pattern name
     imwrite("image_%d.bmp", image, true);
