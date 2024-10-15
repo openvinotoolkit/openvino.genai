527d0e523504297b95f0862218c0c7e47b5f847e
Vladimir Zlobin <vladimir.zlobin@intel.com>
2024-10-15 15:08:54 +0400

    lines

--- miniCPM-V-2_6.py
+++ miniCPM-V-2_6.py
@@ -11,7 +11,7 @@ cv2.imwrite("lines.png", im)
 model_id = "openbmb/MiniCPM-V-2_6"
 processor = AutoProcessor.from_pretrained(model_id, trust_remote_code=True)
 prompt = processor.tokenizer.apply_chat_template([{"role": "user", "content": "(<image>./</image>)\nWhat is unusual on this image?"}], tokenize=False, add_generation_prompt=True)
-image = Image.open("/home/vzlobin/r/g/g.png").convert('RGB')
+image = Image.open("/home/vzlobin/r/g/lines.png").convert('RGB')
 # image = Image.open(requests.get("https://github.com/openvinotoolkit/openvino_notebooks/assets/29454499/d5fbbd1a-d484-415c-88cb-9986625b7b11", stream=True).raw).convert('RGB')
 model = OVModelForVisualCausalLM.from_pretrained("MiniCPM-V-2_6", trust_remote_code=True)
 inputs = processor([prompt], [image], return_tensors="pt")
