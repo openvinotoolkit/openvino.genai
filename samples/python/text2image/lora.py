1e5f9f8c85f2ca647dbd7ad2a59e3a09f51b0480
Anastasiia Pnevskaia <anastasiia.pnevskaia@intel.com>
2024-10-18 13:01:19 +0200

    Minor correction.

--- samples/python/text2image/lora.py
+++ samples/python/text2image/lora.py
@@ -3,10 +3,11 @@
 # SPDX-License-Identifier: Apache-2.0
 
 import argparse
+import sys
 
+import numpy as np
 import openvino as ov
 import openvino_genai
-import numpy as np
 
 
 class Generator(openvino_genai.Generator):
@@ -30,9 +31,8 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('models_path')
     parser.add_argument('prompt')
-    args, adapters = parser.parse_known_args()
-    print("args: ".format(args))
-    print("adapters: ".format(adapters))
+    args, _ = parser.parse_known_args()
+    adapters = sys.argv[3:]
 
     prompt = args.prompt
 
