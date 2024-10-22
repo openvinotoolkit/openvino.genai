734ef9f70cf0ffe2a6111e77ec9984e966138bba
Irina Efode <irina.efode@intel.com>
2024-10-22 14:58:24 +0400

    replace avg to median

--- src/script/get_statistic_updated.py
+++ src/script/get_statistic_updated.py
@@ -1,6 +1,7 @@
 import csv
 import os
 from subprocess import Popen, TimeoutExpired, run, call
+from statistics import median
 
 model_path = "/home/efode/repo/openvino.genai/temp/speculative_decoding/customer_test"
 
@@ -21,17 +22,20 @@ models = {
 element_types = [""]
 
 # num_run = range(0, 3)
-num_run = range(0, 1)
+num_run = range(0, 3)
 
-prompt = "What is OpenVINO?"
+# prompt = "Article: While Google and Apple are developing self driving vehicles, Nasa has revealed its vision - and its a lot more fun. Called the Modular Robotic Vehicle, or MRV, it was developed at NASA's Johnson Space Center to show off the technologies that could let man move across other planets. On Earth, however, it also has some neat tricks - from the ability to park sideways to being able to spin on the spot, as well as being driven remotely at up to 70mph. Watch the buggy in action below . Unlike a normal car, the buggy has no mechanical linkages to the propulsion, steering, or brake actuators, the driver of an MRV relies completely on control inputs being converted to electrical signals and then transmitted by wires to the vehicle's motors. A turn of the steering wheel, for instance, is recorded by sensors and sent to computers at the rear of the vehicle. These computers interpret that signal and instruct motors at one or all four of the wheels to move at the appropriate rate, causing the vehicle to turn as commanded. Nasa says the vehicles was buit 'in order to advance technologies that have applications for future vehicles both in space and on Earth.' 'With seating for two people, MRV is a fully electric vehicle well-suited for busy urban environments, it says. MRV is driven by four independent wheel modules called e-corners. Each e-corner consists of a redundant steering actuator, a passive trailing arm suspension, an in-wheel propulsion motor, and a motor-driven friction braking system. Each e-corner can be controlled independently and rotated ±180 degrees about its axis. This allows for a suite of driving modes allowing MRV to maneuver unlike any traditional vehicle on the road. In addition to conventional front two wheel steering, the back wheels can also articulate allowing for turning radiuses as tight as zero. The driving mode can be switched so that all four wheels point and move in the same direction achieving an omni-directional, crab-like motion. This makes a maneuver such as parallel parking as easy as driving next to an available spot, stopping, and then operating sideways to slip directly in between two cars. 'This two-seater vehicle was designed to meet the growing challenges and demands of urban transportation,' said Mason Markee, also with Johnson. 'The MRV would be ideal for daily transportation in an urban environment with a designed top speed of 70 km/hr and range of 100 km of city driving on a single charge of the battery. 'The size and maneuverability of MRV gives it an advantage in navigating and parking in tight quarters.' With a designed top speed of around 70 km/hr, test driving proved to be a bit of fun. Justin Ridley told an Nasa magazine: 'It's like driving on ice but having complete control. 'It's a blast to ride in and even more fun to drive. We've talked about it being like an amusement park ride. 'The 'fun' of driving was not something we tried to design for, just something that came out of the design. 'Once we got it running many of us commented that we had no idea it was going to be able to do the things it does.' 'This work also allowed us to develop some technologies we felt were needed for our future rovers,' said Ridley. The driver controls MRV with a conventional looking steering wheel and accelerator/brake pedal assembly. The driving mode can be switched so that all four wheels point and move in the same direction - perfect for parallel parking. 'These include redundant by-wire systems, liquid cooling, motor technology, advanced vehicle control algorithms. 'We were able to learn a lot about these and other technologies by building this vehicle.' The buggy can also be controlled remotely, and in the future Nasa says this system can be expanded to allow for autonomous driving. The driver controls MRV with a conventional looking steering wheel and accelerator/brake pedal assembly. Both of these interfaces were specially designed to mimic the feel of the mechanical/hydraulic systems that people are used to feeling when driving their own cars. The buggy can also be controlled remotely, and in the future Nasa says this system can be expanded to allow for autonomous driving. 'While the vehicle as a whole is designed around operating in an urban environment, the core technologies are advancements used in many of our robotic systems and rovers,' said Mason. 'Actuators, motor controllers, sensors, batteries, BMS, component cooling, sealing, and software are all examples of technologies that are being devel oped and tested in MRV that will be used in next generation rover systems. 'The technologies developed in MRV have direct application in future manned vehicles undertaking missions on the surface of Earth's moon, on Mars, or even an asteroid. 'Additionally, MRV provides a platform to learn lessons that could drive the next generation of automobiles."\
+# "\n\nSummarize the above article in 5 sentence."
+
+prompt = "Tell me something about Russia's history"
 
 sampling_types = [ "greedy" ]
 # sampling_types = [ "multinomial", "greedy" ]
 
 speculative_decoding_type = [ "static", "dynamic" ]
 
-gen_len = [ 30 ]
-# gen_len = [ 100, 500, 1000 ]
+gen_len = [ 10 ]
+# gen_len = [ 100, 500 ]
 
 devices = [ "CPU" ]
 
@@ -161,15 +165,15 @@ with open(csv_path_all, "w", encoding='UTF-8') as csv_all_file:
                         for generation_len in gen_len:
                             for sampling in sampling_types:
                                 for sd_type in speculative_decoding_type:
-                                    avg_llm_duration = 0
+                                    avg_llm_duration = []
                                     avg_llm_token_per_sec = 0
-                                    avg_cb_duration = 0
+                                    avg_cb_duration = []
                                     avg_cb_token_per_sec = 0
-                                    avg_sd_duration = 0
-                                    avg_sd_draft_duration = 0
-                                    avg_sd_main_duration = 0
-                                    avg_sd_draft_duration_percentage = 0
-                                    avg_sd_main_duration_percentage = 0
+                                    avg_sd_duration = []
+                                    avg_sd_draft_duration = []
+                                    avg_sd_main_duration = []
+                                    avg_sd_draft_duration_percentage = []
+                                    avg_sd_main_duration_percentage = []
                                     avg_sd_token_per_second = 0
                                     avg_sd_iterations = 0
                                     avg_sd_avg_acceptance_rate = 0
@@ -224,10 +228,9 @@ with open(csv_path_all, "w", encoding='UTF-8') as csv_all_file:
                                             print(command_line_llm)
                                             run(command_line_llm, check=True, shell=True)
                                             llm_duration = parse_log()
-                                            llm_token_per_sec = float(llm_duration) / generation_len
+                                            llm_token_per_sec = llm_duration / generation_len
 
-                                            avg_llm_duration += llm_duration
-                                            avg_llm_token_per_sec += llm_token_per_sec
+                                            avg_llm_duration.append(llm_duration)
                                         except:
                                             pass
 
@@ -235,10 +238,9 @@ with open(csv_path_all, "w", encoding='UTF-8') as csv_all_file:
                                             print(command_line_cb)
                                             run(command_line_cb, check=True, shell=True)
                                             cb_duration = parse_log()
-                                            cd_token_per_sec = float(cb_duration) / generation_len
+                                            cd_token_per_sec = cb_duration / generation_len
 
-                                            avg_cb_duration += cb_duration
-                                            avg_cb_token_per_sec += cd_token_per_sec
+                                            avg_cb_duration.append(cb_duration)
                                         except:
                                             pass
 
@@ -247,12 +249,11 @@ with open(csv_path_all, "w", encoding='UTF-8') as csv_all_file:
                                             run(command_line_sd, check=True, shell=True)
                                             sd_duration, sd_draft_duration, sd_main_duration, sd_draft_duration_percentage, \
                                                 sd_main_duration_percentage, sd_token_per_second, sd_iterations, sd_avg_acceptance_rate, sd_accepted_token_cnt, sd_accepted_token_rate = sd_parse_log()
-                                            avg_sd_duration += sd_duration
-                                            avg_sd_draft_duration += sd_draft_duration
-                                            avg_sd_main_duration += sd_main_duration
-                                            avg_sd_draft_duration_percentage += sd_draft_duration_percentage
-                                            avg_sd_main_duration_percentage += sd_main_duration_percentage
-                                            avg_sd_token_per_second += sd_token_per_second
+                                            avg_sd_duration.append(sd_duration)
+                                            avg_sd_draft_duration.append(sd_draft_duration)
+                                            avg_sd_main_duration.append(sd_main_duration)
+                                            avg_sd_draft_duration_percentage.append(sd_draft_duration_percentage)
+                                            avg_sd_main_duration_percentage.append(sd_main_duration_percentage)
                                             avg_sd_iterations += sd_iterations
                                             avg_sd_avg_acceptance_rate += sd_avg_acceptance_rate
                                             avg_sd_accepted_token_cnt += sd_accepted_token_cnt
@@ -261,13 +262,9 @@ with open(csv_path_all, "w", encoding='UTF-8') as csv_all_file:
                                             pass
 
                                         llm_boost = llm_duration / sd_duration
-                                        avg_llm_boost += llm_boost
                                         llm_boost_ = llm_boost * 100 - 100
-                                        avg_llm_boost_ += llm_boost_
                                         cb_boost = cb_duration / sd_duration
-                                        avg_cb_boost += cb_boost
                                         cb_boost_ = cb_boost * 100 - 100
-                                        avg_cb_boost_ += cb_boost_
 
                                         csv_writer_all.writerow([
                                             model_pair[0], main_element_type, model_pair[1], draft_element_type,
@@ -281,24 +278,30 @@ with open(csv_path_all, "w", encoding='UTF-8') as csv_all_file:
 
                                         if run_id == len(num_run) - 1:
                                             l = len(num_run)
-                                            avg_llm_token_per_sec /= l
                                             avg_cd_token_per_sec /= l
                                             avg_sd_token_per_second /= l
                                             avg_sd_iterations /= l
                                             avg_sd_accepted_token_cnt /= l
                                             avg_sd_accepted_token_rate /= l
                                             avg_sd_avg_acceptance_rate /= l
-                                            avg_sd_draft_duration /= l
-                                            avg_sd_draft_duration_percentage /= l
-                                            avg_sd_main_duration /= l
-                                            avg_sd_main_duration_percentage /= l
-                                            avg_llm_duration /= l
-                                            avg_cb_duration /= l
-                                            avg_sd_duration /= l
-                                            avg_llm_boost /= l
-                                            avg_llm_boost_ /= l
-                                            avg_cb_boost /= l
-                                            avg_cb_boost_ /= l
+
+                                            avg_llm_duration = median(avg_llm_duration)
+                                            avg_cb_duration = median(avg_cb_duration)
+                                            avg_sd_duration = median(avg_sd_duration)
+                                            avg_sd_draft_duration = median(avg_sd_draft_duration)
+                                            avg_sd_main_duration = median(avg_sd_main_duration)
+                                            avg_sd_draft_duration_percentage = median(avg_sd_draft_duration_percentage)
+                                            avg_sd_main_duration_percentage = median(avg_sd_main_duration_percentage)
+                                            
+                                            avg_llm_token_per_sec = avg_llm_duration / generation_len
+                                            avg_cb_token_per_sec = avg_cb_duration / generation_len
+                                            avg_sd_token_per_second = avg_sd_duration / generation_len
+
+                                            avg_llm_boost = avg_llm_duration / avg_sd_duration
+                                            avg_cb_boost = avg_cb_duration / avg_sd_duration
+                                            avg_llm_boost_ = avg_llm_boost * 100 - 100
+                                            avg_cb_boost_ = avg_cb_boost * 100 - 100
+
 
                                     csv_writer.writerow([
                                             model_pair[0], main_element_type, model_pair[1], draft_element_type,
