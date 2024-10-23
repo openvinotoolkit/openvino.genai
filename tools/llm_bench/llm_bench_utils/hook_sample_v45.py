6bfbc10bb22b2489bac8e7ca93f790f2144ef5af
wgzintel <guozhong.wang@intel.com>
2024-10-23 16:05:46 +0800

    revert hook_sample_v45.py

--- tools/llm_bench/llm_bench_utils/hook_sample_v45.py
+++ tools/llm_bench/llm_bench_utils/hook_sample_v45.py
@@ -141,8 +141,7 @@ def new_sample(
 
             # Clone is needed to avoid keeping a hanging ref to outputs.logits which may be very large for first iteration
             # (the clone itself is always small)
-            #next_token_logits = outputs.logits.clone()[:, -1, :].float()
-            next_token_logits = outputs.logits[:, -1, :].clone().float()
+            next_token_logits = outputs.logits.clone()[:, -1, :].float()
 
             # pre-process distribution
             next_token_scores = logits_processor(input_ids, next_token_logits)
