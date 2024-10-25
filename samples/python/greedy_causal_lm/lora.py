36db3792048e066759e061fc57efa550d1cd3674
Anastasiia Pnevskaia <anastasiia.pnevskaia@intel.com>
2024-10-25 14:18:28 +0200

    Minor sample corrections.

--- samples/python/greedy_causal_lm/lora.py
+++ samples/python/greedy_causal_lm/lora.py
@@ -15,13 +15,11 @@ def main():
 
     device = 'CPU'  # GPU can be used as well
     adapter = openvino_genai.Adapter(args.adapter_path)
-    adapter_config = openvino_genai.AdapterConfig()
-    adapter_config.add(adapter, 0.75)
-
+    adapter_config = openvino_genai.AdapterConfig(adapter)
     pipe = openvino_genai.LLMPipeline(args.models_path, device, adapters=adapter_config)  # register all required adapters here
 
     print("Generate with LoRA adapter and alpha set to 0.75:")
-    print(pipe.generate(args.prompt, max_new_tokens=100, adapters=adapter_config))
+    print(pipe.generate(args.prompt, max_new_tokens=100, adapters=openvino_genai.AdapterConfig(adapter, 0.75)))
 
     print("\n-----------------------------")
     print("Generate without LoRA adapter:")
