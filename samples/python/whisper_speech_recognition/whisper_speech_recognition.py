501ae844aaaba26616edab33bedac55fee398b32
Alexander Suvorov <alexander.suvorov@intel.com>
2024-10-29 19:06:28 +0100

    Remove streamer and max_length from samples

--- samples/python/whisper_speech_recognition/whisper_speech_recognition.py
+++ samples/python/whisper_speech_recognition/whisper_speech_recognition.py
@@ -24,20 +24,16 @@ def main():
     config = openvino_genai.WhisperGenerationConfig(
         args.model_dir + "/generation_config.json"
     )
-    config.max_new_tokens = 100  # increase this based on your speech length
+
     # 'task' and 'language' parameters are supported for multilingual models only
     config.language = "<|en|>"  # can switch to <|zh|> for Chinese language
     config.task = "transcribe"
     config.return_timestamps = True
 
-    def streamer(word: str) -> bool:
-        print(word, end="")
-        return False
-
     raw_speech = read_wav(args.wav_file_path)
-    result = pipe.generate(raw_speech, config, streamer)
+    result = pipe.generate(raw_speech, config)
 
-    print()
+    print(result)
 
     for chunk in result.chunks:
         print(f"timestamps: [{chunk.start_ts}, {chunk.end_ts}] text: {chunk.text}")
