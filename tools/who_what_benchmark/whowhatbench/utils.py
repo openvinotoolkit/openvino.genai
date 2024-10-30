8ed61c173920e6df15ace33bbd811a4f680eea97
Alexander <kozzzloff@list.ru>
2024-10-30 18:18:26 +0400

    Style

--- tools/who_what_benchmark/whowhatbench/utils.py
+++ tools/who_what_benchmark/whowhatbench/utils.py
@@ -1,20 +1,21 @@
-from typing import Any, Union, Tuple, List, Optional
+from typing import Union, Tuple, List, Optional
 import torch
-import numpy as np
 
 from diffusers.utils import torch_utils
 
 
 def new_randn_tensor(
     shape: Union[Tuple, List],
-    generator: Optional[Union[List["torch.Generator"], "torch.Generator"]] = None,
+    generator: Optional[Union[List["torch.Generator"],
+                              "torch.Generator"]] = None,
     device: Optional["torch.device"] = None,
     dtype: Optional["torch.dtype"] = None,
     layout: Optional["torch.layout"] = None,
 ):
     latents = torch.zeros(shape).view(-1)
     for i in range(latents.shape[0]):
-        latents[i] = torch.randn(1, generator=generator, dtype=torch.float32).item()
+        latents[i] = torch.randn(
+            1, generator=generator, dtype=torch.float32).item()
 
     return latents.view(shape)
 
