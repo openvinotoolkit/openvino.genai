2ed141469fcba013f728f3c539b9cf521607a455
Alexander <kozzzloff@list.ru>
2024-10-30 14:00:15 +0400

    Added chat template for GenAI VLM (not working yet)

--- tools/who_what_benchmark/whowhatbench/wwb.py
+++ tools/who_what_benchmark/whowhatbench/wwb.py
@@ -468,6 +468,7 @@ def genai_gen_visual_text(model, prompt, image, processor, max_new_tokens, crop_
     config = openvino_genai.GenerationConfig()
     config.max_new_tokens = max_new_tokens
     config.do_sample = False
+    #model.set_chat_template("{'role: 'user', 'content': [{'type': 'text', 'text': 'What are these?'}, {'type': 'image'}]}")
     image_data = Tensor(np.array(image.getdata()).reshape(1, image.size[1], image.size[0], 3).astype(np.byte))
     out = model.generate(prompt, images=[image_data], generation_config=config)
     return out
