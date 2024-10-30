ec04a3191f8421926350f91a7eede47cd748fcdb
Chen Peter <peter.chen@intel.com>
2024-10-30 16:29:27 +0800

    Apply suggestions from code review

--- tools/llm_bench/benchmark.py
+++ tools/llm_bench/benchmark.py
@@ -136,7 +136,7 @@ def get_argprser():
         action='store_true',
         help='Stop the generation even if output token size does not achieve infer_count or max token size ({DEFAULT_OUTPUT_TOKEN_SIZE}}).'
     )
-    parser.add_argument('--set_torch_thread', default=0, type=num_infer_count_type, help='Set the number of torch thread')
+    parser.add_argument('--set_torch_thread', default=0, type=num_infer_count_type, help='Set the number of Torch thread. ')
 
     return parser.parse_args()
 
