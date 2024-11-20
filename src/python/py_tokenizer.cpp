cd05c8eb9ce1eb22411c2107afcdb1b3e2344fa9
Anastasiia Pnevskaia <anastasiia.pnevskaia@intel.com>
2024-11-20 20:03:33 +0100

    Fixed passing of generation config params to VLM generate. (#1180)
    - Fixed passing of generation config params to VLM generate().
    - Updated generation config params params list in
    `update_config_from_kwargs()` method.
    
    Ticket: CVS-157050
    
    ---------
    
    Co-authored-by: Ilya Lavrenov <ilya.lavrenov@intel.com>

--- src/python/py_tokenizer.cpp
+++ src/python/py_tokenizer.cpp
@@ -30,9 +30,18 @@ void init_tokenizer(py::module_& m) {
         R"(openvino_genai.Tokenizer object is used to initialize Tokenizer
            if it's located in a different path than the main model.)")
 
-        .def(py::init([](const std::filesystem::path& tokenizer_path, const std::map<std::string, py::object>& properties) {
+        .def(py::init([](const std::filesystem::path& tokenizer_path, const std::map<std::string, py::object>& properties, const py::kwargs& kwargs) {
             ScopedVar env_manager(pyutils::ov_tokenizers_module_path());
-            return std::make_unique<ov::genai::Tokenizer>(tokenizer_path, pyutils::properties_to_any_map(properties));
+            auto kwargs_properties = pyutils::kwargs_to_any_map(kwargs);
+            if (properties.size()) {
+                PyErr_WarnEx(PyExc_DeprecationWarning, 
+                         "'properties' parameters is deprecated, please use kwargs to pass config properties instead.", 
+                         1);
+                auto map_properties = pyutils::properties_to_any_map(properties);
+                kwargs_properties.insert(map_properties.begin(), map_properties.end());
+            }
+
+            return std::make_unique<ov::genai::Tokenizer>(tokenizer_path, kwargs_properties);
         }), py::arg("tokenizer_path"), py::arg("properties") = ov::AnyMap({}))
 
         .def("encode", [](Tokenizer& tok, std::vector<std::string>& prompts, bool add_special_tokens) {
