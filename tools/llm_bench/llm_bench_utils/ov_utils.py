c24405447a92198bc68ab77b6099b737eeff3ae1
Dmitry Matveev <dmitry.matveev@intel.com>
2024-12-12 07:51:01 +0000

    LLM benchmark: Pass config as config not **config (#1360)
    CVS-159167

--- tools/llm_bench/llm_bench_utils/ov_utils.py
+++ tools/llm_bench/llm_bench_utils/ov_utils.py
@@ -256,7 +256,7 @@ def create_genai_text_gen_model(model_path, device, ov_config, **kwargs):
         ov_config['adapters'] = adapter_config
 
     start = time.perf_counter()
-    llm_pipe = openvino_genai.LLMPipeline(model_path, device.upper(), **ov_config)
+    llm_pipe = openvino_genai.LLMPipeline(model_path, device.upper(), ov_config)
     end = time.perf_counter()
     log.info(f'Pipeline initialization time: {end - start:.2f}s')
 
