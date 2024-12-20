c7bc7b8d1a504360532dc9138928dc609b46f8cd
Irina Efode <irina.efode@intel.com>
2024-12-20 14:47:27 +0400

    Update lm_encoding.cpp

--- src/cpp/src/lm_encoding.cpp
+++ src/cpp/src/lm_encoding.cpp
@@ -133,7 +133,7 @@ std::pair<EncodedResults, int32_t> get_lm_encoded_results(
 
     SamplerOutput sampler_output = sampler.sample(sequence_groups, logits);
     stream_generated_tokens();
-    
+
     // "Generation" phase
 
     while (!active_sequence_groups.empty()) {
