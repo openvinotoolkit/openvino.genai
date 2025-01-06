b7051554de9df0441835dad3a43c8e5b527b9621
Ilya Lavrenov <ilya.lavrenov@intel.com>
2025-01-06 16:08:11 +0400

    [LLM Bench] Added support of OpenVINO pre-releases install and test (#1480)

--- tests/python_tests/test_vlm_pipeline.py
+++ tests/python_tests/test_vlm_pipeline.py
@@ -19,7 +19,7 @@ def get_ov_model(cache):
     ov_tokenizer, ov_detokenizer = openvino_tokenizers.convert_tokenizer(processor.tokenizer, with_detokenizer=True)
     openvino.save_model(ov_tokenizer, model_dir / "openvino_tokenizer.xml")
     openvino.save_model(ov_detokenizer, model_dir / "openvino_detokenizer.xml")
-    model = OVModelForVisualCausalLM.from_pretrained(model_id, compile=False, device="CPU", export=True, trust_remote_code=True)
+    model = OVModelForVisualCausalLM.from_pretrained(model_id, compile=False, device="CPU", export=True, load_in_8bit=False, trust_remote_code=True)
     processor.save_pretrained(model_dir)
     model.save_pretrained(model_dir)
     return model_dir
