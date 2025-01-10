28590fee069f46a0782b1aeabceffe0b0597595b
xufang <fang.xu@intel.com>
2025-01-10 18:44:17 +0800

    fix error

--- src/python/openvino_genai/py_openvino_genai.pyi
+++ src/python/openvino_genai/py_openvino_genai.pyi
@@ -865,19 +865,19 @@ class ImageGenerationConfig:
 class ImageGenerationPerfMetrics:
     """
 
-        Holds performance metrics for each image generate call.
+        Holds performance metrics for each generate call.
 
         PerfMetrics holds fields with mean and standard deviations for the following metrics:
-        - one generation iteration, ms
-        - inference duration for unet model, ms
-        - inference duration for transformer model, ms
+        - Generate iteration duration, ms
+        - Inference duration for unet model, ms
+        - Inference duration for transformer model, ms
 
         Additional fields include:
         - Load time, ms
-        - total duration of image generation, ms
+        - Generate total duration, ms
+        - inference durations for each encoder, ms
         - inference duration of vae_encoder model, ms
         - inference duration of vae_decoder model, ms
-        - inference duration of each encoder model, ms
 
         Preferable way to access values is via get functions. Getters calculate mean and std values from raw_metrics and return pairs.
         If mean and std were already calculated, getters return cached values.
@@ -885,52 +885,40 @@ class ImageGenerationPerfMetrics:
         :param get_load_time: Returns the load time in milliseconds.
         :type get_load_time: float
 
-        :param get_unet_inference_duration: Returns the mean and standard deviation of unet inference in millionseconds.
-        :type get_unet_inference_duration: MeanStdPair
+        :param get_generate_duration: Returns the generate duration in milliseconds.
+        :type get_generate_duration: float
 
-        :param get_transformer_inference_duration: Returns the mean and standard deviation of transformer inference in millionseconds.
-        :type get_transformer_inference_duration: MeanStdPair
-
-        :param get_iteration_duration: Returns the mean and standard deviation of one generation iteration in millionseconds.
-        :type get_iteration_duration: MeanStdPair
-
-        :param get_inference_total_duration: Returns all inference duration including encoder, decoder and transformer/unet inference.
+        :param get_inference_total_duration: Returns the total inference durations (including encoder, unet/transformer and decoder inference) in milliseconds.
         :type get_inference_total_duration: float
 
-        :param get_generate_duration: Returns generate duration in millionseconds.
-        :type get_generate_duration: float
+        :param get_iteration_duration: Returns the mean and standard deviation of one generation iteration in milliseconds.
+        :type get_iteration_duration: MeanStdPair
+
+        :param unet_inference_duration: Returns the mean and standard deviation of one unet inference in milliseconds.
+        :type unet_inference_duration: MeanStdPair
+
+        :param get_transformer_inference_duration: Returns the mean and standard deviation of one transformer inference in milliseconds.
+        :type get_transformer_inference_duration: MeanStdPair
 
         :param raw_metrics: A structure of RawImageGenerationPerfMetrics type that holds raw metrics.
         :type raw_metrics: RawImageGenerationPerfMetrics
     """
     def __init__(self) -> None:
         ...
-    @property
-    def load_time(self) -> float:
-        ...
-    @property
-    def generate_duration(self) -> float:
-        ...
-    @property
-    def vae_encoder_inference_duration(self) -> float:
-        ...
-    @property
-    def vae_decoder_inference_duration(self) -> float:
-        ...
-    @property
-    def encoder_inference_duration(self) -> dict[str, float]:
-        ...
-    def get_unet_inference_duration(self) -> MeanStdPair:
-        ...
-    def get_transformer_inference_duration(self) -> MeanStdPair:
-        ...
-    def get_iteration_duration(self) -> MeanStdPair:
+    def get_generate_duration(self) -> float:
         ...
     def get_inference_total_duration(self) -> float:
         ...
+    def get_iteration_duration(self) -> MeanStdPair:
+        ...
     def get_load_time(self) -> float:
         ...
-    def get_generate_duration(self) -> float:
+    def get_transformer_inference_duration(self) -> MeanStdPair:
+        ...
+    def get_unet_inference_duration(self) -> MeanStdPair:
+        ...
+    @property
+    def raw_metrics(self) -> RawImageGenerationPerfMetrics:
         ...
 class InpaintingPipeline:
     """
@@ -1308,25 +1296,25 @@ class RawImageGenerationPerfMetrics:
 
         Structure with raw performance metrics for each generation before any statistics are calculated.
 
-        :param unet_inference_durations: Inference time for each unet inference in microseconds.
+        :param unet_inference_durations: Durations for each unet inference in microseconds.
         :type unet_inference_durations: List[float]
 
-        :param transformer_inference_durations: Inference time for each transformer inference in microseconds.
+        :param transformer_inference_durations: Durations for each transformer inference in microseconds.
         :type transformer_inference_durations: List[float]
 
-        :param iteration_durations: durations for each step iteration in microseconds.
+        :param iteration_durations: Durations for each step iteration in microseconds.
         :type iteration_durations: List[float]
     """
     def __init__(self) -> None:
         ...
     @property
-    def unet_inference_durations(self) -> list[float]:
+    def iteration_durations(self) -> list[float]:
         ...
     @property
     def transformer_inference_durations(self) -> list[float]:
         ...
     @property
-    def iteration_durations(self) -> list[float]:
+    def unet_inference_durations(self) -> list[float]:
         ...
 class RawPerfMetrics:
     """
