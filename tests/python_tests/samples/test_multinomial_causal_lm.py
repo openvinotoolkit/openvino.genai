cfc0bf261ac477dc5576fdaddb56d2624542a6cf
Mikhail Ryzhov <mikhail.ryzhov@intel.com>
2025-02-03 19:50:41 +0100

    test_cpp_sample_multinomial_causal_lm_open_llama

--- tests/python_tests/samples/test_multinomial_causal_lm.py
+++ tests/python_tests/samples/test_multinomial_causal_lm.py
@@ -4,14 +4,14 @@
 import os
 import subprocess # nosec B404
 import pytest
-from conftest import SAMPLES_PY_DIR
+from conftest import SAMPLES_PY_DIR, SAMPLES_CPP_DIR
 
 # multinomial_causal_lm sample
 
 @pytest.mark.llm
 @pytest.mark.py
 @pytest.mark.parametrize("convert_model", [
-    {"model_id": "TinyLlama-1.1B-Chat-v1.0"},
+    {"model_id": "TinyLlama-1.1B-Chat-v1.0"}
 ], indirect=["convert_model"])
 @pytest.mark.parametrize("sample_args", ["0"])
 def test_python_sample_multinomial_causal_lm_tiny_llama(convert_model, sample_args):
@@ -30,6 +30,19 @@ def test_python_sample_multinomial_causal_lm_open_llama(convert_model, sample_ar
     result = subprocess.run(["python", script, convert_model, sample_args], check=True)
     assert result.returncode == 0, f"Script execution failed for model {convert_model} with argument {sample_args}"
     shared_data.setdefault("multinomial_causal_lm", {}).setdefault("py", {}).setdefault("open_llama_3b_v2", {})[sample_args] = result.stdout
+
+@pytest.mark.llm
+@pytest.mark.cpp
+@pytest.mark.parametrize("convert_model", [
+    {"model_id": "open_llama_3b_v2"}
+], indirect=["convert_model"])
+@pytest.mark.parametrize("sample_args", ["return 0"])
+def test_cpp_sample_multinomial_causal_lm_open_llama(convert_model, sample_args,  shared_data):
+    cpp_sample = os.path.join(SAMPLES_CPP_DIR, 'greedy_causal_lm')
+    result = subprocess.run([cpp_sample, convert_model, sample_args], check=True)
+    assert result.returncode == 0, "C++ sample execution failed"
+    shared_data.setdefault("multinomial_causal_lm", {}).setdefault("cpp", {}).setdefault("open_llama_3b_v2", {})[sample_args] = result.stdout
+
 
 @pytest.mark.llm    
 @pytest.mark.cpp
