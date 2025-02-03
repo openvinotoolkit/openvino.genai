9583046dd6535819b11416c32cab1e5811699f40
Mikhail Ryzhov <mikhail.ryzhov@intel.com>
2025-02-03 16:30:12 +0100

    fixed test params

--- tests/python_tests/samples/test_greedy_causal_lm.py
+++ tests/python_tests/samples/test_greedy_causal_lm.py
@@ -33,7 +33,9 @@ def test_cpp_sample_greedy_causal_lm_open_llama(convert_model, sample_args):
 # text_generation sample
 @pytest.mark.llm
 @pytest.mark.py
-@pytest.mark.parametrize("convert_model", ["TinyLlama-1.1B-intermediate-step-1431k-3T"], indirect=["convert_model"])
+@pytest.mark.parametrize("convert_model", [
+    {"model_id": "TinyLlama-1.1B-intermediate-step-1431k-3T"}
+], indirect=["convert_model"])
 @pytest.mark.parametrize("sample_args", ["How to create a table with two columns, one of them has type float, another one has type int?"])
 @pytest.mark.parametrize("download_test_content", [TEST_FILES["adapter_model.safetensors"]], indirect=True)
 def test_python_sample_text_generation(convert_model, download_test_content, sample_args):
