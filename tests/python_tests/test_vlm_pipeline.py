658052eec2a10f08d431c96fdb647061887cd572
yatarkan <yaroslav.tarkan@intel.com>
2025-02-05 00:17:30 +0400

    Add test for non chat mode

--- tests/python_tests/test_vlm_pipeline.py
+++ tests/python_tests/test_vlm_pipeline.py
@@ -67,8 +67,13 @@ def test_vlm_pipeline(model_id, cache):
         images = []
         for link in links:
             images.append(get_image_by_link(link))
+        
+        # Test non chat mode
+        result_from_streamer = []
+        res = ov_pipe.generate(prompts[0], images=images, generation_config=generation_config, streamer=streamer)
+        assert res.texts[0] == ''.join(result_from_streamer)
 
-
+        # Test chat mode
         for system_message in ["", "You are a helpful assistant."]:
             ov_pipe.start_chat(system_message)
 
