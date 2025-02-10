8c7eb0b2ecad28d5dfd91c2cf5e984a73cb25ab6
Irina Efode <irina.efode@intel.com>
2025-02-10 21:36:03 +0400

    review

--- tests/python_tests/test_vlm_pipeline.py
+++ tests/python_tests/test_vlm_pipeline.py
@@ -10,7 +10,7 @@ from openvino_genai import VLMPipeline, GenerationConfig
 from common import get_image_by_link
 
 from utils.generation_config import get_beam_search, get_multinomial_all_parameters
-from utils.constants import get_default_llm_propeties
+from utils.constants import get_default_llm_properties
 
 def get_ov_model(model_id, cache):
     model_dir = cache.mkdir(model_id.split('/')[-1])
@@ -21,7 +21,7 @@ def get_ov_model(model_id, cache):
     ov_tokenizer, ov_detokenizer = openvino_tokenizers.convert_tokenizer(processor.tokenizer, with_detokenizer=True)
     openvino.save_model(ov_tokenizer, model_dir / "openvino_tokenizer.xml")
     openvino.save_model(ov_detokenizer, model_dir / "openvino_detokenizer.xml")
-    model = OVModelForVisualCausalLM.from_pretrained(model_id, compile=False, device="CPU", export=True, load_in_8bit=False, trust_remote_code=True, ov_config=get_default_llm_propeties())
+    model = OVModelForVisualCausalLM.from_pretrained(model_id, compile=False, device="CPU", export=True, load_in_8bit=False, trust_remote_code=True, ov_config=get_default_llm_properties())
     if processor.tokenizer.chat_template is not None:
         processor.chat_template = processor.tokenizer.chat_template  # It seems that tiny-random-phi3-vision is saved incorrectly. That line works this around.
     processor.save_pretrained(model_dir)
