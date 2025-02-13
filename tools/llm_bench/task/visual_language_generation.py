ac4e97d1a504712bd2ad6ed0a888869d2afa6e74
eaidova <ekaterina.aidova@intel.com>
2025-02-13 20:50:11 +0400

    code style

--- tools/llm_bench/task/visual_language_generation.py
+++ tools/llm_bench/task/visual_language_generation.py
@@ -366,8 +366,10 @@ def get_image_text_prompt(args):
         if len(vlm_param_list) > 0:
             for vlm_file in vlm_param_list:
                 if args['prompt_file'] is not None and len(args['prompt_file']) > 0:
-                    vlm_file['media'] = os.path.join(os.path.dirname(args['prompt_file'][0]), vlm_file['media'].replace('./', '')) \
-                                         if not vlm_file["media"].startswith("https://") else vlm_file["media"]
+                    media_path = vlm_file["media"]
+                    if not (media_path.startswith("http://") or media_path.startswith("https://")):
+                        media_path = os.path.join(os.path.dirname(args['prompt_file'][0]), media_path.replace("./", ""))
+                    vlm_file['media'] = media_path
                 vlm_file_list.append(vlm_file)
     else:
         vlm_file_list.append(output_data_list)
