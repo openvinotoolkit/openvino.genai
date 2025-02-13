25543f52fdadd97a74e528a77d31c7275e860c00
likholat <anna.likholat@intel.com>
2025-02-13 20:27:57 +0100

    [Image Generation] SD3 image2image
    [Image Generation] SD3 Inpainting
    
    review fixes
    
    enable test
    
    fix
    
    FlowMatchEulerDiscreteScheduler fix
    
    codestyle fix
    
    reduce code duplication
    
    is_inpainting_model func fix
    
    fix img2img shape

--- src/cpp/include/openvino/genai/image_generation/inpainting_pipeline.hpp
+++ src/cpp/include/openvino/genai/image_generation/inpainting_pipeline.hpp
@@ -80,6 +80,23 @@ public:
         const FluxTransformer2DModel& transformer,
         const AutoencoderKL& vae);
 
+    // creates SD3 pipeline from building blocks
+    static InpaintingPipeline stable_diffusion_3(
+        const std::shared_ptr<Scheduler>& scheduler,
+        const CLIPTextModelWithProjection& clip_text_model_1,
+        const CLIPTextModelWithProjection& clip_text_model_2,
+        const T5EncoderModel& t5_encoder_model,
+        const SD3Transformer2DModel& transformer,
+        const AutoencoderKL& vae);
+
+    // creates SD3 pipeline from building blocks
+    static InpaintingPipeline stable_diffusion_3(
+        const std::shared_ptr<Scheduler>& scheduler,
+        const CLIPTextModelWithProjection& clip_text_model_1,
+        const CLIPTextModelWithProjection& clip_text_model_2,
+        const SD3Transformer2DModel& transformer,
+        const AutoencoderKL& vae);
+
     ImageGenerationConfig get_generation_config() const;
     void set_generation_config(const ImageGenerationConfig& generation_config);
 
