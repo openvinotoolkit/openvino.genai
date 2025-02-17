607bde87772d3ef9a4bc7e7562bf9ac77baf1165
Oleg Pipikin <oleg.pipikin@intel.com>
2025-02-17 18:32:04 +0100

    Add py binding for encrypted models and sample

--- src/python/openvino_genai/py_openvino_genai.pyi
+++ src/python/openvino_genai/py_openvino_genai.pyi
@@ -1175,6 +1175,15 @@ class LLMPipeline:
                     Add {"scheduler_config": ov_genai.SchedulerConfig} to config properties to create continuous batching pipeline.
                     kwargs: Device properties.
         """
+    @typing.overload
+    def __init__(self, models: str, weights: openvino._pyopenvino.Tensor, tokenizer : Tokenizer, device: str,  os.PathLike, device: str, generation_config: GenerationConfig | None = None, **kwargs) -> None:
+        """
+                    LLMPipeline class constructor.
+                    models_path (os.PathLike): Path to the model file.
+                    device (str): Device to run the model on (e.g., CPU, GPU). Default is 'CPU'.
+                    Add {"scheduler_config": ov_genai.SchedulerConfig} to config properties to create continuous batching pipeline.
+                    kwargs: Device properties.
+        """
     def finish_chat(self) -> None:
         ...
     def generate(self, inputs: openvino._pyopenvino.Tensor | TokenizedInputs | str | list[str], generation_config: GenerationConfig | None = None, streamer: typing.Callable[[str], int | None] | StreamerBase | None = None, **kwargs) -> EncodedResults | DecodedResults:
@@ -1889,7 +1898,12 @@ class Tokenizer:
             openvino.Model if the entry exists.
     """
     chat_template: str
+    @typing.overload
     def __init__(self, tokenizer_path: os.PathLike, properties: dict[str, typing.Any] = {}, **kwargs) -> None:
+        ...
+    @typing.overload
+    def __init__(self, tokenizer_model: str, tokenizer_weights: openvino._pyopenvino.Tensor,
+                 detokenizer_model: str, detokenizer_weights: openvino._pyopenvino.Tensor, **kwargs) -> None:
         ...
     def apply_chat_template(self, history: list[dict[str, str]], add_generation_prompt: bool, chat_template: str = '') -> str:
         """
