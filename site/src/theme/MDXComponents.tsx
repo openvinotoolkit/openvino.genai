99b9c27931b212b5146a64f70b88987fbfead61f
yatarkan <yaroslav.tarkan@intel.com>
2025-02-18 21:03:01 +0400

    Expose mdx components and language tabs

---
+++ site/src/theme/MDXComponents.tsx
@@ -0,0 +1,10 @@
+import MDXComponents from '@theme-original/MDXComponents'
+import { LanguageTabs, TabItemPython, TabItemCpp } from '@site/src/components/LanguageTabs';
+
+export default {
+  // Reusing the default mapping
+  ...MDXComponents,
+  LanguageTabs,
+  TabItemPython,
+  TabItemCpp,
+};
