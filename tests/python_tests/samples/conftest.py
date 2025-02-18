b1c7b2de021477c5d55af4eccebf0225aef77a25
Mikhail Ryzhov <mikhail.ryzhov@intel.com>
2025-02-18 10:20:16 +0100

    split hf and ov caches

--- tests/python_tests/samples/conftest.py
+++ tests/python_tests/samples/conftest.py
@@ -55,9 +55,10 @@ TEST_FILES = {
     "adapter_model.safetensors": "https://huggingface.co/smangrul/tinyllama_lora_sql/resolve/main/adapter_model.safetensors"
 }
 
-TEMP_DIR = os.environ.get("TEMP_DIR", tempfile.mkdtemp())
-MODELS_DIR = os.path.join(TEMP_DIR, "test_models")
-TEST_DATA = os.path.join(TEMP_DIR, "test_data")
+OV_CACHE = os.environ.get("OV_CACHE", tempfile.mkdtemp())
+MODELS_DIR = os.path.join(OV_CACHE, "test_models")
+TEST_DATA = os.path.join(OV_CACHE, "test_data")
+HF_CACHE = os.environ.get("HF_HOME", os.path.join(OV_CACHE, "hf_cache"))
 
 SAMPLES_PY_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), "../../../samples/python"))
 SAMPLES_CPP_DIR = os.environ.get("SAMPLES_CPP_DIR", os.getcwd())
@@ -69,11 +70,11 @@ def setup_and_teardown(request):
     os.makedirs(MODELS_DIR, exist_ok=True)
     os.makedirs(TEST_DATA, exist_ok=True)
     yield
-    if not os.environ.get("TEMP_DIR"):
-        logger.info(f"Removing temporary directory: {TEMP_DIR}")
-        shutil.rmtree(TEMP_DIR)
+    if not os.environ.get("OV_CACHE"):
+        logger.info(f"Removing temporary directory: {OV_CACHE}")
+        shutil.rmtree(OV_CACHE)
     else:
-        logger.info(f"Skipping cleanup of temporary directory: {TEMP_DIR}")
+        logger.info(f"Skipping cleanup of temporary directory: {OV_CACHE}")
 
 @pytest.fixture(scope="session")
 def convert_model(request):
@@ -82,13 +83,16 @@ def convert_model(request):
     model_name = MODELS[model_id].get("name")
     model_path = os.path.join(MODELS_DIR, model_name)
     model_args = MODELS[model_id].get("convert_args", [])
+    model_hf_cache = os.path.join(HF_CACHE, model_id)
     logger.info(f"Preparing model: {model_name}")
     # Convert the model if not already converted
     if not os.path.exists(model_path):
         logger.info(f"Converting model: {model_name}")
         command = [
             "optimum-cli", "export", "openvino",
-            "--model", model_name, model_path
+            "--model", model_name, 
+            "--cache_dir", model_hf_cache, 
+            model_path
         ]
         if model_args:
             command.extend(model_args)
@@ -108,12 +112,18 @@ def convert_model(request):
             
     yield model_path
     # Cleanup the model after tests
-    if not os.environ.get("TEMP_DIR"):
+    if not os.environ.get("OV_CACHE"):
         if os.path.exists(model_path):
             logger.info(f"Removing converted model: {model_path}")
             shutil.rmtree(model_path)
     else:
         logger.info(f"The model {model_path} will be removed when all the tests are finished")
+        
+    # Cleanup the model cache after test
+    if not os.environ.get("HF_HOME"):
+        if os.path.exists(model_hf_cache):
+            logger.info(f"Removing cached model: {model_hf_cache}")
+            shutil.rmtree(model_hf_cache)
 
 @pytest.fixture(scope="session")
 def download_test_content(request):
