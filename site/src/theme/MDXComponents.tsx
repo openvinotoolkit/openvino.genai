c5c7257e05546540ab46ba813d6440dd47b2df28
yatarkan <yaroslav.tarkan@intel.com>
2025-02-19 02:16:29 +0400

    Export theme tabs

--- site/src/theme/MDXComponents.tsx
+++ site/src/theme/MDXComponents.tsx
@@ -1,9 +1,15 @@
 import MDXComponents from '@theme-original/MDXComponents'
+import Tabs from '@theme/Tabs';
+import TabItem from '@theme/TabItem';
 import { LanguageTabs, TabItemPython, TabItemCpp } from '@site/src/components/LanguageTabs';
 
 export default {
   // Reusing the default mapping
   ...MDXComponents,
+  // Theme components
+  Tabs,
+  TabItem,
+  // Custom components
   LanguageTabs,
   TabItemPython,
   TabItemCpp,
