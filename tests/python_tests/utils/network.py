6a2603244f8c725a4d330f7d12856266bdfb09d0
Mikhail Ryzhov <mikhail.ryzhov@intel.com>
2025-02-19 21:34:23 +0100

    [TESTS] Retry model downloading and conversion (#1758)
    Add the ability to retry model downloading and conversion (3 times)

---
+++ tests/python_tests/utils/network.py
@@ -0,0 +1,26 @@
+# Copyright (C) 2025 Intel Corporation
+# SPDX-License-Identifier: Apache-2.0
+
+import requests
+import time
+from huggingface_hub.utils import HfHubHTTPError
+
+def retry_request(func, retries=3):
+    """
+    Retries a function that makes a request up to a specified number of times.
+
+    Parameters:
+    func (callable): The function to be retried. It should be a callable that makes a request.
+    retries (int): The number of retry attempts. Default is 3.
+
+    Returns:
+    Any: The return value of the function `func` if it succeeds.
+    """
+    for attempt in range(retries):
+        try:
+            return func()
+        except (requests.exceptions.ConnectionError, requests.exceptions.Timeout, HfHubHTTPError) as e:
+            if attempt < retries - 1:
+                time.sleep(2 ** attempt)
+            else:
+                raise e
