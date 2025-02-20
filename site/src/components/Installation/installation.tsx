8c3f7f6bd69673aa1b2256a242053b53a7c02f63
yatarkan <yaroslav.tarkan@intel.com>
2025-02-20 19:59:36 +0400

    Add install buttons with links to selector tool

--- site/src/components/Installation/installation.tsx
+++ site/src/components/Installation/installation.tsx
@@ -1,43 +1,55 @@
+import Link from '@docusaurus/Link';
 import styles from './styles.module.css';
 
-import Link from '@docusaurus/Link';
+import Button from '@site/src/components/Button';
 import LinuxLogo from '@site/static/img/linux-logo.svg';
 import MacOSLogo from '@site/static/img/mac-os-logo.svg';
 import WindowsLogo from '@site/static/img/windows-logo.svg';
 import Heading from '@theme/Heading';
 
+const getInstallLink = (os: 'WINDOWS' | 'MACOS' | 'LINUX') =>
+  `https://docs.openvino.ai/2025/get-started/install-openvino.html?PACKAGE=OPENVINO_GENAI&VERSION=v_2025_0_0&OP_SYSTEM=${os}&DISTRIBUTION=PIP`;
+
 const INSTALLATION_ITEMS = [
   {
-    title: 'Linux install',
+    title: 'Linux',
     Icon: LinuxLogo,
+    link: getInstallLink('LINUX'),
   },
   {
-    title: 'Windows install',
+    title: 'Windows',
     Icon: WindowsLogo,
+    link: getInstallLink('WINDOWS'),
   },
   {
-    title: 'MacOS install',
+    title: 'MacOS',
     Icon: MacOSLogo,
+    link: getInstallLink('MACOS'),
   },
 ];
 
-const InstallationItems = () => {
+const InstallationOptions = () => {
   return (
-    <ul className={styles.installationOptions}>
-      {INSTALLATION_ITEMS.map(({ title, Icon }) => (
-        <li key={title} className={styles.installationOptionsItem}>
-          <Icon role="img" aria-label={title} />
-          <span>{title}</span>
-        </li>
+    <div className={styles.installationOptions}>
+      {INSTALLATION_ITEMS.map(({ title, Icon, link }) => (
+        <Button
+          key={title}
+          label={title}
+          Icon={Icon}
+          link={link}
+          size="lg"
+          outline
+          variant="secondary"
+        />
       ))}
-    </ul>
+    </div>
   );
 };
 
 export const Installation = () => {
   return (
     <section className={styles.installation}>
-      <Heading as="h4" className={styles.installationTitle}>
+      <Heading as="h2" className={styles.installationTitle}>
         Install OpenVINO™ GenAI
       </Heading>
       <p className={styles.installationDescription}>
@@ -45,7 +57,7 @@ export const Installation = () => {
         Get started with seamless installation now!
       </p>
 
-      <InstallationItems />
+      <InstallationOptions />
 
       <p>
         Full list of installation options <Link href="docs/overview/installation">here</Link>
