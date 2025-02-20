a39b3adc42d576ca463e133f041149f4689325e8
Irina Efode <irina.efode@intel.com>
2025-02-20 13:20:06 +0400

    fix tests

--- tests/python_tests/test_continuous_batching.py
+++ tests/python_tests/test_continuous_batching.py
@@ -77,7 +77,7 @@ def test_continuous_batching_vs_stateful(prompt, generation_config):
     model_id = "facebook/opt-125m"
     _, _, models_path = download_and_convert_model(model_id, padding_side="left")
     ov_pipe = create_ov_pipeline(models_path, pipeline_type=PipelineType.STATEFUL)
-    cb_pipe = create_ov_pipeline(models_path, pipeline_type=PipelineType.CONTINIOUS_BATCHING)
+    cb_pipe = create_ov_pipeline(models_path, pipeline_type=PipelineType.PAGED_ATTENTION)
 
     reference = ov_pipe.generate(prompt, **generation_config)
     generated = cb_pipe.generate(prompt, **generation_config)
