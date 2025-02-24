2d37363250b03115e993442637865ad283149fbd
Mikhail Ryzhov <mikhail.ryzhov@intel.com>
2025-02-25 00:04:47 +0100

    fixed test

--- tests/python_tests/samples/test_speculative_decoding_lm.py
+++ tests/python_tests/samples/test_speculative_decoding_lm.py
@@ -8,37 +8,32 @@ import sys
 from conftest import SAMPLES_PY_DIR, SAMPLES_CPP_DIR, convert_model
 from test_utils import run_sample
 
-convert_model_1 = convert_model
-convert_model_2 = convert_model
+convert_draft_model = convert_model
 
 class TestSpeculativeDecodingLM:
     @pytest.mark.llm
     @pytest.mark.samples
     @pytest.mark.parametrize(
-        "convert_model_1, convert_model_2, sample_args",
+        "convert_model, convert_draft_model, sample_args",
         [
             pytest.param("SmolLM2-360M", "SmolLM2-135M", "Alan Turing was a"),
         ],
-        indirect=["convert_model_1", "convert_model_2"],
+        indirect=["convert_model", "convert_draft_model"],
     )
-    def test_sample_speculative_decoding_lm(self, convert_model_1, convert_model_2, sample_args):      
+    def test_sample_speculative_decoding_lm(self, convert_model, convert_draft_model, sample_args):      
         # Test Python sample
         py_script = os.path.join(SAMPLES_PY_DIR, "text_generation/speculative_decoding_lm.py")
-        py_command = [sys.executable, py_script, convert_model_1, convert_model_2, sample_args]
+        py_command = [sys.executable, py_script, convert_model, convert_draft_model, sample_args]
         py_result = run_sample(py_command)
 
         # Test CPP sample
         cpp_sample = os.path.join(SAMPLES_CPP_DIR, 'speculative_decoding_lm')
-        cpp_command =[cpp_sample, convert_model_1, sample_args]
+        cpp_command =[cpp_sample, convert_model, convert_draft_model, sample_args]
         cpp_result = run_sample(cpp_command)
         
         # Greedy decoding
-        cpp_sample_ref = os.path.join(SAMPLES_CPP_DIR, 'speculative_decoding_lm')
-        cpp_command_ref = [cpp_sample_ref, convert_model_1, convert_model_2, sample_args]
-        cpp_result_ref = run_sample(cpp_command_ref)
-        
-        # Greedy decoding
-        cpp_command_ref = [cpp_sample, convert_model_1, convert_model_2, sample_args]
+        cpp_sample_ref = os.path.join(SAMPLES_CPP_DIR, 'greedy_causal_lm')
+        cpp_command_ref = [cpp_sample_ref, convert_model, sample_args]
         cpp_result_ref = run_sample(cpp_command_ref)
 
         # Compare results
