d0972bd4aa6e15e0e7afa36247c9f534b94ee275
Mikhail Ryzhov <mikhail.ryzhov@intel.com>
2025-02-24 23:03:00 +0100

    replaced cpp-Phi-1_5

--- tests/python_tests/samples/test_greedy_causal_lm.py
+++ tests/python_tests/samples/test_greedy_causal_lm.py
@@ -5,7 +5,7 @@ import os
 import pytest
 import sys
 
-from conftest import SAMPLES_PY_DIR, SAMPLES_CPP_DIR
+from conftest import logger, MODELS, SAMPLES_PY_DIR, SAMPLES_CPP_DIR
 from test_utils import run_sample
 
 class TestGreedyCausalLM:
@@ -33,3 +33,46 @@ class TestGreedyCausalLM:
 
         # Compare results
         assert py_result.stdout == cpp_result.stdout, f"Results should match"
+    
+    @pytest.mark.llm
+    @pytest.mark.samples
+    @pytest.mark.parametrize("convert_model", ["phi-1_5"], indirect=True)
+    @pytest.mark.parametrize("sample_args",
+        [
+            ["Alan Turing was a"],
+        ],
+    )
+    def test_sample_greedy_causal_lm_refs(self, request, convert_model, sample_args):
+        # Python test
+        py_script = os.path.join(SAMPLES_PY_DIR, "text_generation/beam_search_causal_lm.py")
+        py_command = [sys.executable, py_script, convert_model] + [f'"{arg}"' for arg in sample_args]
+        py_result = run_sample(py_command)
+        py_predictions = py_result.stdout
+
+        # C++ test
+        cpp_sample = os.path.join(SAMPLES_CPP_DIR, 'beam_search_causal_lm')
+        cpp_command = [cpp_sample, convert_model] + [f'"{arg}"' for arg in sample_args]
+        cpp_result = run_sample(cpp_command)
+        cpp_predictions = cpp_result.stdout
+        
+        model_name = request.node.callspec.params['convert_model']
+        model = MODELS[model_name]
+        
+        import transformers
+        tokenizer = transformers.AutoTokenizer.from_pretrained(model['name'])
+        for prompt in sample_args:
+            if tokenizer.chat_template:
+                prompt = tokenizer.apply_chat_template([{'role': 'user', 'content': f'"{prompt}"'}], tokenize=False, add_generation_prompt=True)
+            tokenized = tokenizer(f'"{prompt}"', return_tensors='pt', add_special_tokens=False)
+        
+            for beam in transformers.LlamaForCausalLM.from_pretrained(model['name']).generate(**tokenized, max_length=100, do_sample=False):
+                ref = ': ' + tokenizer.decode(beam[tokenized['input_ids'].numel():], skip_special_tokens=True)
+                logger.info(f'Checking for "{ref=}"')
+                
+                idx = py_predictions.find(ref)
+                assert -1 != idx, f'Missing "{ref=}" from Python predictions'
+                py_predictions = py_predictions[:idx] + py_predictions[idx + len(ref):]
+
+                idx = cpp_predictions.find(ref)
+                assert -1 != idx, f'Missing "{ref=}" from C++ predictions'
+                cpp_predictions = cpp_predictions[:idx] + cpp_predictions[idx + len(ref):]
