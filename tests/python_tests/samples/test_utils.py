b113d41736365d698543b5079132659a2a000f18
Mikhail Ryzhov <mikhail.ryzhov@intel.com>
2025-02-25 12:00:10 +0100

    encoding='utf-8'

--- tests/python_tests/samples/test_utils.py
+++ tests/python_tests/samples/test_utils.py
@@ -6,6 +6,6 @@ import subprocess # nosec B404
 
 def run_sample(command):
     logger.info(f"Running sample command: {' '.join(command)}")
-    result = subprocess.run(command, capture_output=True, text=True, check=True)
+    result = subprocess.run(command, capture_output=True, text=True, check=True, encoding='utf-8')
     logger.info(f"Sample output: {result.stdout}")
     return result
