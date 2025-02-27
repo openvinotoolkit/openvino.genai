bb565407dc9aef0275c047cbae30059fc715b546
TolyaTalamanov <anatoliy.talamanov@intel.com>
2025-02-27 12:22:39 +0000

    Fix llm pipeline instanc

--- src/cpp/src/llm_pipeline.cpp
+++ src/cpp/src/llm_pipeline.cpp
@@ -127,8 +127,10 @@ ov::genai::LLMPipeline::LLMPipeline(
         m_pimpl = std::make_unique<ContinuousBatchingAdapter>(models_path, tokenizer, scheduler_config, device, device_properties);
     }
 
-    if (m_pimpl == nullptr && device == "NPU" && properties.count("STATIC_PIPELINE")) {
-        m_pimpl = static_llm::LLMPipelineFactory::create(models_path, tokenizer, properties);
+    if (m_pimpl == nullptr && device == "NPU") {
+        m_pimpl = properties.count("STATIC_PIPELINE")
+            ? static_llm::LLMPipelineFactory::create(models_path, tokenizer, properties)
+            : std::make_unique<StatefulLLMPipeline>(models_path, tokenizer, device, properties);
     }
 
     // try to call CB adapter one more time, but with safe guard to silent exception
@@ -165,8 +167,10 @@ ov::genai::LLMPipeline::LLMPipeline(
         m_pimpl = std::make_unique<ContinuousBatchingAdapter>(models_path, scheduler_config, device, device_properties);
     }
 
-    if (m_pimpl == nullptr && device == "NPU" && properties.count("STATIC_PIPELINE")) {
-        m_pimpl = static_llm::LLMPipelineFactory::create(models_path, properties);
+    if (m_pimpl == nullptr && device == "NPU") {
+        m_pimpl = properties.count("STATIC_PIPELINE")
+            ? static_llm::LLMPipelineFactory::create(models_path, properties)
+            : std::make_unique<StatefulLLMPipeline>(models_path, device, properties);
     }
 
     // try to call CB adapter one more time, but with safe guard to silent exception
@@ -207,13 +211,19 @@ ov::genai::LLMPipeline::LLMPipeline(
                                                               tokenizer, scheduler_config, device, device_properties, generation_config);
     }
 
-    if (m_pimpl == nullptr && device == "NPU" && properties.count("STATIC_PIPELINE")) {
-        m_pimpl = static_llm::LLMPipelineFactory::create(
-            utils::singleton_core().read_model(model_str, weights_tensor),
-            tokenizer,
-            properties,
-            generation_config
-        );
+    if (m_pimpl == nullptr && device == "NPU") {
+        m_pimpl = properties.count("STATIC_PIPELINE")
+            ? static_llm::LLMPipelineFactory::create(
+                  utils::singleton_core().read_model(model_str, weights_tensor),
+                  tokenizer,
+                  properties,
+                  generation_config)
+            : std::make_unique<StatefulLLMPipeline>(
+                utils::singleton_core().read_model(model_str, weights_tensor),
+                tokenizer,
+                device,
+                properties,
+                generation_config);
     }
 
     // try to call CB adapter one more time, but with safe guard to silent exception
