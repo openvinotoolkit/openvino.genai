4cd4fe625520f659af57e4991ed8011e22d77610
michalkulakowski <michal.kulakowski@intel.com>
2025-03-04 16:03:05 +0100

    fix

--- src/cpp/src/speculative_decoding/speculative_decoding_impl.cpp
+++ src/cpp/src/speculative_decoding/speculative_decoding_impl.cpp
@@ -240,7 +240,7 @@ ContinuousBatchingPipeline::SpeculativeDecodingImpl::generate(const std::vector<
 
     std::vector<GenerationHandle> main_generations;
     for (size_t request_id = 0; request_id < input_ids.size(); ++request_id) {
-        m_sd_metrics.set_generated_len(request_id, sampling_params[request_id].get_max_new_tokens(input_ids.get_size()));
+        m_sd_metrics.set_generated_len(request_id, sampling_params[request_id].get_max_new_tokens(input_ids[request_id].get_size()));
         OPENVINO_ASSERT(1 == input_ids[request_id].get_shape().at(0), "Use multiple tensors to pass a batch.");
         main_generations.push_back(m_main_pipeline->add_request(request_id, input_ids[request_id], sampling_params[request_id]));
 
