cb1ffb2bc59ba42871f2ef3c2f3a3468a995f402
sbalandi <sofya.balandina@intel.com>
2025-03-07 18:31:19 +0000

    Update tests to check encoded inputs with chat

--- tests/python_tests/test_llm_pipeline.py
+++ tests/python_tests/test_llm_pipeline.py
@@ -38,7 +38,8 @@ def test_string_inputs(model_id, generation_config_dict, prompt, pipeline_type):
 
 input_tensors_list = [
     # input_ids, attention_mask
-    (np.array([[1, 4, 42]], dtype=np.int64), None)
+    (np.array([[1, 4, 42]], dtype=np.int64), None),
+    (np.array([[1, 4, 42]], dtype=np.int64), np.array([[1, 1, 1]], dtype=np.int64)),
 ]
 @pytest.mark.parametrize("inputs", input_tensors_list)
 @pytest.mark.parametrize("model_id", get_models_list())
@@ -128,8 +129,9 @@ def test_different_input_types_works_same_and_change_nothing(model_id):
     tokenizer = ov_pipe.get_tokenizer()
     ov_tokens = tokenizer.encode(questions[0], add_special_tokens=True)
     res_encoded_input = ov_pipe.generate(ov_tokens, generation_config=ov_generation_config)
+    res_encoded_input_str = hf_tokenizer.decode(res_encoded_input.tokens[0], skip_special_tokens=True)
 
-    assert res_string_input_1 == res_encoded_input
+    assert res_string_input_1 == res_encoded_input_str
 
     res_string_input_2 = ov_pipe.generate(questions[0], generation_config=ov_generation_config)
 
@@ -162,7 +164,12 @@ def test_chat_scenario(model_id, intpus, string_inputs):
     chat_history_ov = []
 
     opt_model, hf_tokenizer, models_path = download_and_convert_model(model_id)
-    ov_pipe = create_ov_pipeline(models_path)
+    ov_pipe = None
+    if string_inputs:
+        ov_pipe = create_ov_pipeline(models_path)
+    else:
+        # chat is not supported for PA backend with encoded_inputs fromat
+        ov_pipe = create_ov_pipeline(models_path, pipeline_type=PipelineType.STATEFUL)
 
     generation_config_kwargs, system_message = intpus
 
@@ -188,7 +195,6 @@ def test_chat_scenario(model_id, intpus, string_inputs):
 
         if string_inputs:
             answer_ov = ov_pipe.generate(prompt, generation_config=ov_generation_config)
-            chat_history_ov.append({'role': 'assistant', 'content': answer_ov})
         else:
             input_ids = np.array([tokenized['input_ids'][0][prev_chat_len:]], dtype=np.int64)
             attention_mask = np.array([tokenized['attention_mask'][0][prev_chat_len:]], dtype=np.int64)
@@ -197,9 +203,9 @@ def test_chat_scenario(model_id, intpus, string_inputs):
             result_ov = ov_pipe.generate(inputs_ov, generation_config=ov_generation_config).tokens[0]
 
             answer_ov = hf_tokenizer.decode(result_ov, skip_special_tokens=True)
-            prev_chat_len = len(tokenized['input_ids'][0]) + len(answer_ov[0][0])
+            prev_chat_len = len(tokenized['input_ids'][0]) + len(result_ov)
 
-        chat_history_hf.append({'role': 'assistant', 'content': answer_str})
+        chat_history_ov.append({'role': 'assistant', 'content': answer_ov})
 
     ov_pipe.finish_chat()
 
