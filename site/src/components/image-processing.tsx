771410997ee10b412fc6d0d3e3ec563738450eda
yatarkan <yaroslav.tarkan@intel.com>
2025-03-11 19:41:45 +0400

    Reuse vlm code examples

--- site/src/components/image-processing.tsx
+++ site/src/components/image-processing.tsx
@@ -2,53 +2,17 @@ import { ExploreCodeSamples } from '@site/src/components/GoToLink/explore-code-s
 import { GoToDocumentation } from '@site/src/components/GoToLink/go-to-documentation';
 import { LanguageTabs, TabItemCpp, TabItemPython } from '@site/src/components/LanguageTabs';
 import { Section } from '@site/src/components/Section';
-import CodeBlock from '@theme/CodeBlock';
 
 import ImagePlaceholder from '@site/static/img/image-generation-placeholder.webp';
+
+import CodeExampleCpp from '@site/docs/use-cases/image-processing/_sections/_run_model/_code_example_cpp.mdx';
+import CodeExamplePython from '@site/docs/use-cases/image-processing/_sections/_run_model/_code_example_python.mdx';
 
 const FEATURES = [
   'Use different generation parameters (sampling types, etc.)',
   'Optimize for chat scenarios by using chat mode',
   'Pass multiple images to a model',
 ];
-
-const pythonCodeBlock = (
-  <CodeBlock language="python">
-    {`import numpy as np
-import openvino as ov
-import openvino_genai as ov_genai
-from PIL import Image
-
-# Choose GPU instead of CPU in the line below to run the model on Intel integrated or discrete GPU
-pipe = ov_genai.VLMPipeline("./MiniCPM-V-2_6/", "CPU")
-
-image = Image.open("dog.jpg")
-image_data = np.array(image.getdata()).reshape(1, image.size[1], image.size[0], 3).astype(np.uint8)
-image_data = ov.Tensor(image_data)
-
-prompt = "Can you describe the image?"
-print(pipe.generate(prompt, image=image_data, max_new_tokens=100))`}
-  </CodeBlock>
-);
-
-const cppCodeBlock = (
-  <CodeBlock language="cpp">
-    {`#include "load_image.hpp"
-#include <openvino/genai/visual_language/pipeline.hpp>
-#include <iostream>
-
-int main(int argc, char* argv[]) {
-    std::string models_path = argv[1];
-    ov::genai::VLMPipeline pipe(models_path, "CPU");
-    ov::Tensor rgb = utils::load_image(argv[2]);
-    std::cout << pipe.generate(
-        prompt,
-        ov::genai::image(rgb),
-        ov::genai::max_new_tokens(100)
-    ) << '\\n';
-}`}
-  </CodeBlock>
-);
 
 export const ImageProcessing = () => {
   return (
@@ -68,8 +32,12 @@ export const ImageProcessing = () => {
         <Section.Features features={FEATURES} />
         <hr />
         <LanguageTabs>
-          <TabItemPython>{pythonCodeBlock}</TabItemPython>
-          <TabItemCpp>{cppCodeBlock}</TabItemCpp>
+          <TabItemPython>
+            <CodeExamplePython />
+          </TabItemPython>
+          <TabItemCpp>
+            <CodeExampleCpp />
+          </TabItemCpp>
         </LanguageTabs>
         <hr />
         <ExploreCodeSamples link="docs/category/samples" />
