8e470523518fd94292d8dea50fe940b535fd5521
Vladimir Zlobin <vladimir.zlobin@intel.com>
2025-03-12 15:05:01 +0400

    Fix test

--- src/python/openvino_genai/py_openvino_genai.pyi
+++ src/python/openvino_genai/py_openvino_genai.pyi
@@ -2114,7 +2114,7 @@ class VLMPipeline:
             images used in previous prompts isn't implemented.
             A model's native image tag can be used instead of
             <ov_genai_image_i>. These tags are:
-            MiniCPM-V-2_6: <image>./</image>
+            MiniCPM-V-2_6: (<image>./</image>)\\n
             Qwen2-VL: <|vision_start|><|image_pad|><|vision_end|>
             If the prompt doesn't contain image tags, but images are
             provided, the tags are prepended to the prompt.
@@ -2146,7 +2146,7 @@ class VLMPipeline:
             images used in previous prompts isn't implemented.
             A model's native image tag can be used instead of
             <ov_genai_image_i>. These tags are:
-            MiniCPM-V-2_6: <image>./</image>
+            MiniCPM-V-2_6: (<image>./</image>)\\n
             Qwen2-VL: <|vision_start|><|image_pad|><|vision_end|>
             If the prompt doesn't contain image tags, but images are
             provided, the tags are prepended to the prompt.
@@ -2177,7 +2177,7 @@ class VLMPipeline:
             images used in previous prompts isn't implemented.
             A model's native image tag can be used instead of
             <ov_genai_image_i>. These tags are:
-            MiniCPM-V-2_6: <image>./</image>
+            MiniCPM-V-2_6: (<image>./</image>)\\n
             Qwen2-VL: <|vision_start|><|image_pad|><|vision_end|>
             If the prompt doesn't contain image tags, but images are
             provided, the tags are prepended to the prompt.
