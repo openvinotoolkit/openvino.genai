9534f2c02b74593fbb539458846707991f832b9f
Vladimir Zlobin <vladimir.zlobin@intel.com>
2025-03-15 17:11:24 +0400

    phi3_v: add universal tag (#1921)
    Replace unify_prompt() with normalize_prompt(). The new function doesn't
    insert/replace image tags multiple times. It doesn't search the same
    place multiple times for the same tag. phi3_v is able to reuse its
    parts.
    
    Ticket CVS-164259
    
    ---------
    
    Co-authored-by: Ilya Lavrenov <ilya.lavrenov@intel.com>

--- src/python/openvino_genai/py_openvino_genai.pyi
+++ src/python/openvino_genai/py_openvino_genai.pyi
@@ -2115,6 +2115,7 @@ class VLMPipeline:
             A model's native image tag can be used instead of
             <ov_genai_image_i>. These tags are:
             MiniCPM-V-2_6: (<image>./</image>)\\n
+            Phi-3-vision: <|image_i|>\\n - the index starts with one
             Qwen2-VL: <|vision_start|><|image_pad|><|vision_end|>
             If the prompt doesn't contain image tags, but images are
             provided, the tags are prepended to the prompt.
@@ -2147,6 +2148,7 @@ class VLMPipeline:
             A model's native image tag can be used instead of
             <ov_genai_image_i>. These tags are:
             MiniCPM-V-2_6: (<image>./</image>)\\n
+            Phi-3-vision: <|image_i|>\\n - the index starts with one
             Qwen2-VL: <|vision_start|><|image_pad|><|vision_end|>
             If the prompt doesn't contain image tags, but images are
             provided, the tags are prepended to the prompt.
@@ -2178,6 +2180,7 @@ class VLMPipeline:
             A model's native image tag can be used instead of
             <ov_genai_image_i>. These tags are:
             MiniCPM-V-2_6: (<image>./</image>)\\n
+            Phi-3-vision: <|image_i|>\\n - the index starts with one
             Qwen2-VL: <|vision_start|><|image_pad|><|vision_end|>
             If the prompt doesn't contain image tags, but images are
             provided, the tags are prepended to the prompt.
