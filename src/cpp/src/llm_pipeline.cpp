579327af5cacd8bfd009cdcc0fd66e744751b370
Ilya Lavrenov <ilya.lavrenov@intel.com>
2025-03-26 13:46:38 +0400

    Updated logic whether PA backend is explicitly required

--- src/cpp/src/llm_pipeline.cpp
+++ src/cpp/src/llm_pipeline.cpp
@@ -45,7 +45,8 @@ SchedulerConfig get_latency_oriented_scheduler_config() {
 }
 
 bool explicitly_requires_paged_attention(const ov::AnyMap& properties) {
-    if (properties.find(ov::genai::scheduler_config.name()) != properties.end()) {
+    if (properties.find(ov::genai::scheduler_config.name()) != properties.end() ||
+        properties.find("ATTENTION_BACKEND") != properties.end()) {
         if (is_paged_attention_available()) {
             return true;
         } else {
