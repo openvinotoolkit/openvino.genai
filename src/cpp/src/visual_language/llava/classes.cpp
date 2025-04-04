4b3fbaae59c0351ffe82211c104e3c415345f80e
Ilya Lavrenov <ilya.lavrenov@intel.com>
2025-04-04 14:53:11 +0400

    Revert "VLM: change infer to start_async/wait " (#2004)
    Reverts openvinotoolkit/openvino.genai#1948
    
    Should be fixed after
    https://github.com/openvinotoolkit/openvino/pull/29613

--- src/cpp/src/visual_language/llava/classes.cpp
+++ src/cpp/src/visual_language/llava/classes.cpp
@@ -76,8 +76,7 @@ EncodedImage VisionEncoderLLaVA::encode( const ov::Tensor& image, const ov::AnyM
     ov::Tensor pixel_values = get_pixel_values_llava(image, config);
 
     encoder.set_tensor("pixel_values", pixel_values);
-    encoder.start_async();
-    encoder.wait();
+    encoder.infer();
 
     const ov::Tensor& infer_output = encoder.get_output_tensor();
     ov::Tensor image_features(infer_output.get_element_type(), infer_output.get_shape());
