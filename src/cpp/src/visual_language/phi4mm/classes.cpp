2aceefd5025c0a2e12818d73ec3c6a717203312d
Vladimir Zlobin <vladimir.zlobin@intel.com>
2025-05-16 14:08:44 +0400

    Factor common out

--- src/cpp/src/visual_language/phi4mm/classes.cpp
+++ src/cpp/src/visual_language/phi4mm/classes.cpp
@@ -4,14 +4,12 @@
 #include "visual_language/phi4mm/classes.hpp"
 
 #include "visual_language/clip.hpp"
+#include "visual_language/phi3_vision/classes.hpp"
 
 #include "utils.hpp"
 
-namespace ov::genai {
-
 namespace {
-    
-constexpr size_t INPUT_IMAGE_SIZE = 336;
+
 const std::regex NATIVE_PATTERN{R"(<\|image_(\d+)\|>)"};
 
 // Similar to Phi3, but without newline after image tag
@@ -19,166 +17,9 @@ void write_native(std::ostream& os, size_t idx) {
     os << "<|image_" << idx + 1 << "|>";
 }
 
-// FIXME Copied from Phi3 - reuse
-std::string normalize_prompt(
-    const std::string& prompt, size_t base_id, size_t n_images
-) {
-    std::smatch match;
-    std::regex_search(prompt, match, NATIVE_PATTERN);
-    auto [image_prompt, image_sequence] = universal_to_native(prompt, write_native);
-    if (!image_sequence.empty()) {
-        OPENVINO_ASSERT(match.empty(), "Prompt can contain only one type of image tags.");
-        verify_ids(image_sequence, base_id, n_images);
-        return image_prompt;
-    }
-    // Restore ids from native tags
-    if (!match.empty()) {
-        size_t image_id = std::stoul(match.str(1));
-        OPENVINO_ASSERT(image_id != 0, "Image tags must be greater than 0");
-        image_sequence.push_back(image_id - 1);
-        constexpr int submatch_id_to_return = 1;
-        for (std::sregex_token_iterator iter{
-            match.suffix().first,
-            prompt.end(),
-            NATIVE_PATTERN,
-            submatch_id_to_return
-        }; iter != std::sregex_token_iterator{}; ++iter) {
-            size_t image_id = std::stoul(*iter);
-            OPENVINO_ASSERT(image_id != 0, "Image tags must be greater than 0");
-            image_sequence.push_back(image_id - 1);
-        }
-        if (!image_sequence.empty()) {
-            verify_ids(image_sequence, base_id, n_images);
-            return image_prompt;
-        }
-    }
-    // Prepend native tags
-    std::stringstream stream;
-    for (size_t relative_id = 0; relative_id < n_images; relative_id++) {
-        image_sequence.push_back(base_id + relative_id);
-        write_native(stream, image_sequence.back());
-    }
-    stream << prompt;
-    return stream.str();
-}
-
-// FIXME Copied from Phi3 - reuse
-/// @brief ov::Tensor is tokenized text, size_t is image tag
-std::vector<std::variant<ov::Tensor, size_t>> split_tokenize(const std::string& text, ov::genai::Tokenizer& tokenizer) {
-    std::vector<std::variant<ov::Tensor, size_t>> tokenized;
-    auto prefix_begin = text.begin();
-    bool is_submatch = false;
-    for (std::sregex_token_iterator iter{
-        prefix_begin,
-        text.end(),
-        NATIVE_PATTERN,
-        {0, 1}  // Every match emits two values: whole match and submatch
-    }; iter != std::sregex_token_iterator{}; ++iter) {
-        if (is_submatch) {
-            tokenized.push_back(std::stoul(iter->str()) - 1);
-        } else {
-            std::string regular_text{prefix_begin, iter->first};
-            if (!regular_text.empty()) {
-                tokenized.push_back(tokenizer.encode(regular_text, ov::genai::add_special_tokens(true)).input_ids);
-            }
-            prefix_begin = iter->second;
-        }
-        is_submatch = !is_submatch;
-    }
-    std::string regular_text{prefix_begin, text.end()};
-    if (!regular_text.empty()) {
-        tokenized.push_back(tokenizer.encode(regular_text, ov::genai::add_special_tokens(true)).input_ids);
-    }
-    return tokenized;
-}
-
-// FIXME Copied from Phi3 - reuse
-ov::Tensor insert_image_placeholders(
-    const std::vector<std::variant<ov::Tensor, size_t>>& chunks,
-    const std::vector<size_t>& tokens_per_images
-) {
-    size_t merged_length = 0;
-    for (const std::variant<ov::Tensor, size_t>& chunk : chunks) {
-        merged_length += std::visit(utils::overloaded{
-            [](const ov::Tensor& chunk) {
-                return chunk.get_shape().at(1);
-            },
-            [&](size_t image_id) {
-                return tokens_per_images.at(image_id);
-            }
-        }, chunk);
-    }
-    ov::Tensor merged{ov::element::i64, {1, merged_length}};
-    size_t offset = 0;
-    for (const std::variant<ov::Tensor, size_t>& chunk : chunks) {
-        offset += std::visit(utils::overloaded{
-            [&](const ov::Tensor& chunk) {
-                size_t length = chunk.get_shape().at(1);
-                std::copy_n(
-                    chunk.data<int64_t>(),
-                    length,
-                    merged.data<int64_t>() + offset
-                );
-                return length;
-            },
-            [&](size_t image_id) {
-                int64_t fill_value = -(static_cast<int64_t>(image_id)) - 1;
-                std::fill_n(
-                    merged.data<int64_t>() + offset,
-                    tokens_per_images.at(image_id),
-                    fill_value  // -1 to distinguish 0 token and 0 image id.
-                );
-                return tokens_per_images.at(image_id);
-            }
-        }, chunk);
-    }
-    return merged;
-}
-
-// FIXME Copied from Phi3 - reuse
-std::vector<std::variant<ov::Tensor, size_t>> drop_image_placeholders(const ov::Tensor& tokens) {
-    std::vector<std::variant<ov::Tensor, size_t>> chunks;
-    int64_t last_token = tokens.data<int64_t>()[0];
-    size_t text_start = 0;
-    for (size_t offset = 1; offset < tokens.get_shape().at(1); ++offset) {
-        // If last_token and next_token are not negative, it's continuation of the current chunk text - skip
-        // If last_token is negative and next_token is not negative, it's a start of text - save the offset, add image placeholder
-        // If last token is not negative and next_token is negative, it's an end of text - push_back a chunk
-        // If last_token and next_token are negative, it's continuation of an image placeholder - skip
-        // if last_token and next_token are negative but different, it's a start of a new image placeholder - save the previous image placeholder
-        int64_t next_token = tokens.data<int64_t>()[offset];
-        if (last_token < 0 && next_token >= 0) {
-            text_start = offset;
-            chunks.push_back(size_t(-(last_token + 1)));
-        } else if (last_token >= 0 && next_token < 0) {
-            chunks.emplace_back(
-                std::in_place_type<ov::Tensor>,
-                ov::element::i64,
-                ov::Shape{1, offset - text_start},
-                tokens.data<int64_t>() + text_start
-            );
-        } else if (last_token < 0 && next_token < 0 && last_token != next_token) {
-            chunks.push_back(size_t(-(last_token + 1)));
-        }
-        last_token = next_token;
-    }
-    // Add the last chunk
-    size_t full_length = tokens.get_shape().at(1);
-    if (last_token >= 0) {
-        chunks.emplace_back(
-            std::in_place_type<ov::Tensor>,
-            ov::element::i64,
-            ov::Shape{1, full_length - text_start},
-            tokens.data<int64_t>() + text_start
-        );
-    } else {
-        chunks.push_back(size_t(-(last_token + 1)));
-    }
-    return chunks;
-}
-
 } // namespace
 
+namespace ov::genai {
 
 VisionEncoderPhi4MM::VisionEncoderPhi4MM(
     const std::filesystem::path& model_dir,
@@ -305,7 +146,7 @@ ov::Tensor InputsEmbedderPhi4MM::get_inputs_embeds(
     bool recalculate_merged_embeddings
 ) {
     size_t base_id = m_tokens_per_images.size();
-    std::string image_prompt = normalize_prompt(prompt, base_id, images.size());
+    std::string image_prompt = util::normalize_prompt(prompt, base_id, images.size());
 
     std::vector<ov::Tensor> images_features_proj;
     for (const ov::genai::EncodedImage& encoded_image : images) {
@@ -319,7 +160,7 @@ ov::Tensor InputsEmbedderPhi4MM::get_inputs_embeds(
         constexpr bool add_generation_prompt = true;
         std::string new_templated_chat_history = m_tokenizer.apply_chat_template(m_history, add_generation_prompt);
         auto start_tokenizer_time = std::chrono::steady_clock::now();
-        new_chat_tokens = split_tokenize(new_templated_chat_history, m_tokenizer);
+        new_chat_tokens = util::split_tokenize(new_templated_chat_history, m_tokenizer);
         auto end_tokenizer_time = std::chrono::steady_clock::now();
         metrics.raw_metrics.tokenization_durations.emplace_back(PerfMetrics::get_microsec(end_tokenizer_time - start_tokenizer_time));
     } else {
@@ -332,17 +173,17 @@ ov::Tensor InputsEmbedderPhi4MM::get_inputs_embeds(
             templated_prompt = std::move(image_prompt);
         }
         auto start_tokenizer_time = std::chrono::steady_clock::now();
-        new_chat_tokens = split_tokenize(templated_prompt, m_tokenizer);
+        new_chat_tokens = util::split_tokenize(templated_prompt, m_tokenizer);
         auto end_tokenizer_time = std::chrono::steady_clock::now();
         metrics.raw_metrics.tokenization_durations.emplace_back(PerfMetrics::get_microsec(end_tokenizer_time - start_tokenizer_time));
     }
     
-    ov::Tensor new_merged_tokens = insert_image_placeholders(new_chat_tokens, m_tokens_per_images);
+    ov::Tensor new_merged_tokens = util::insert_image_placeholders(new_chat_tokens, m_tokens_per_images);
     ov::Tensor new_tokens = update_history(new_merged_tokens);
     m_prev_hist_length = m_kv_cache_state.get_state().size();
     m_kv_cache_state.add_inputs(new_tokens);
 
-    std::vector<std::variant<ov::Tensor, size_t>> tokens = drop_image_placeholders(new_tokens);
+    std::vector<std::variant<ov::Tensor, size_t>> tokens = util::drop_image_placeholders(new_tokens);
     ov::Tensor inputs_embeds{ov::element::f32, {1, new_tokens.get_shape().at(1), m_vlm_config.hidden_size}};
     size_t offset = 0;
     CircularBufferQueueElementGuard<EmbeddingsRequest> embeddings_request_guard(m_embedding->get_request_queue().get());
