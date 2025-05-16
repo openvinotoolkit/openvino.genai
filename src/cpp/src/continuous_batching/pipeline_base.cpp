660f730049314b0fe0195d83532c0fa23f3ac3f8
Anastasiia Pnevskaia <anastasiia.pnevskaia@intel.com>
2025-05-16 16:38:18 +0200

    Minor fix.

--- src/cpp/src/continuous_batching/pipeline_base.cpp
+++ src/cpp/src/continuous_batching/pipeline_base.cpp
@@ -176,6 +176,7 @@ ContinuousBatchingPipeline::IContinuousBatchingPipeline::generate(
 
         std::string templated_history = m_tokenizer.apply_chat_template(m_history, true);
 
+        m_inputs_embedder->set_apply_chat_template_status(false);
         input_embeds_list.push_back(m_inputs_embedder->get_inputs_embeds(templated_history, m_history_images, vlm_perf_metrics[0], rgbs.size() > 0, m_history_image_ids));
         auto end_get_inputs_embeds = std::chrono::steady_clock::now();
         vlm_perf_metrics[0].vlm_raw_metrics.prepare_embeddings_durations.emplace_back(PerfMetrics::get_microsec(end_get_inputs_embeds - start_get_inputs_embeds));
