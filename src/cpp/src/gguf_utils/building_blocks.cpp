770ceed28557ae825af38ca81f61e736db2a9e15
TianmengChen <tianmeng.chen@intel.com>
2025-05-27 16:13:31 +0800

    create a new function called make_rms_norm_qwen3

--- src/cpp/src/gguf_utils/building_blocks.cpp
+++ src/cpp/src/gguf_utils/building_blocks.cpp
@@ -255,6 +255,66 @@ std::pair<Output<ov::Node>, Output<ov::Node>> rope_emb(
     };
 }
 
+
+ov::Output<ov::Node> make_rms_norm_qwen3(
+    const std::string& key,
+    const ov::Output<ov::Node>& input,
+    const std::unordered_map<std::string, ov::Tensor>& consts,
+    float rms_norm_eps) {
+    auto eps_node = std::make_shared<ov::op::v0::Constant>(ov::element::f32, ov::Shape{1,1,1,1}, rms_norm_eps);
+    auto square = std::make_shared<ov::op::v1::Power>(
+        input, 
+        std::make_shared<ov::op::v0::Constant>(ov::element::f32, ov::Shape{1,1,1,1}, 2.0f));
+    
+    auto variance = std::make_shared<ov::op::v1::ReduceMean>(
+        square, 
+        std::make_shared<ov::op::v0::Constant>(ov::element::i32, ov::Shape{1}, -1),
+        true);
+
+    auto add_eps = std::make_shared<ov::op::v1::Add>(variance, eps_node);
+    auto sqrt_node = std::make_shared<ov::op::v0::Sqrt>(add_eps);
+    auto reciprocal = std::make_shared<ov::op::v1::Divide>(
+        std::make_shared<ov::op::v0::Constant>(
+            ov::element::f32, ov::Shape{1,1,1,1}, 1.0f),
+        sqrt_node);
+
+    std::shared_ptr<ov::Node> mul = std::make_shared<ov::op::v1::Multiply>(
+        reciprocal, input, AutoBroadcastType::NUMPY);
+
+    auto weight_tensor = consts.at(key + ".weight");
+    // Check if all elements are 1.0
+    bool all_ones = true;
+    if (weight_tensor.get_element_type() == ov::element::f32) {
+        const float* data = weight_tensor.data<float>();
+        for (size_t i = 0; i < weight_tensor.get_size(); ++i) {
+            if (data[i] != 1.0f) {
+                all_ones = false;
+                break;
+            }
+        }
+    } else if (weight_tensor.get_element_type() == ov::element::f16) {
+        const uint16_t* data = weight_tensor.data<uint16_t>();
+        const uint16_t one_in_fp16 = 0x3C00;
+        for (size_t i = 0; i < weight_tensor.get_size(); ++i) {
+            if (data[i] != one_in_fp16) {
+                all_ones = false;
+                break;
+            }
+        }
+    } else {
+        OPENVINO_THROW("Unsupported weight type ", weight_tensor.get_element_type());
+    }
+
+    if (!all_ones) {
+        weight_tensor.set_shape(ov::Shape{1, 1, 1,weight_tensor.get_shape()[0]});
+        auto weights_const = std::make_shared<ov::op::v0::Constant>(weight_tensor);
+        auto weights_f32 = std::make_shared<ov::op::v0::Convert>(weights_const, ov::element::f32);
+        mul = std::make_shared<ov::op::v1::Multiply>(mul, weights_f32, AutoBroadcastType::NUMPY);
+    }
+
+    return mul;
+}
+
 // Helper function to split heads
 // There are q_norm k_norm in Qwen3, if key_name + ".self_attn.q_norm" + ".weight" exists, a rms_norm will be built, if not it will go to else branch.
 std::shared_ptr<v1::Transpose> split_heads(const Output<Node>& x,
@@ -266,57 +326,8 @@ std::shared_ptr<v1::Transpose> split_heads(const Output<Node>& x,
     auto shape = std::make_shared<v0::Constant>(element::i64, Shape{4}, std::vector<int64_t>{0, 0, num_h, head_dim});
     auto reshaped = std::make_shared<v1::Reshape>(x, shape, true);
     if (consts.count(key + ".weight")) { //Qwen3 rms_norm
-        auto eps_node = std::make_shared<ov::op::v0::Constant>(ov::element::f32, ov::Shape{1,1,1,1}, rms_norm_eps);
-        auto square = std::make_shared<ov::op::v1::Power>(
-            reshaped, 
-            std::make_shared<ov::op::v0::Constant>(ov::element::f32, ov::Shape{1,1,1,1}, 2.0f));
-        
-        auto variance = std::make_shared<ov::op::v1::ReduceMean>(
-            square, 
-            std::make_shared<ov::op::v0::Constant>(ov::element::i32, ov::Shape{1}, -1),
-            true);
 
-        auto add_eps = std::make_shared<ov::op::v1::Add>(variance, eps_node);
-        auto sqrt_node = std::make_shared<ov::op::v0::Sqrt>(add_eps);
-        auto reciprocal = std::make_shared<ov::op::v1::Divide>(
-            std::make_shared<ov::op::v0::Constant>(
-                ov::element::f32, ov::Shape{1,1,1,1}, 1.0f),
-            sqrt_node);
-
-        std::shared_ptr<ov::Node> mul = std::make_shared<ov::op::v1::Multiply>(
-            reciprocal, reshaped, AutoBroadcastType::NUMPY);
-
-        auto weight_tensor = consts.at(key + ".weight");
-        // Check if all elements are 1.0
-        bool all_ones = true;
-        if (weight_tensor.get_element_type() == ov::element::f32) {
-            const float* data = weight_tensor.data<float>();
-            for (size_t i = 0; i < weight_tensor.get_size(); ++i) {
-                if (data[i] != 1.0f) {
-                    all_ones = false;
-                    break;
-                }
-            }
-        } else if (weight_tensor.get_element_type() == ov::element::f16) {
-            const uint16_t* data = weight_tensor.data<uint16_t>();
-            const uint16_t one_in_fp16 = 0x3C00;
-            for (size_t i = 0; i < weight_tensor.get_size(); ++i) {
-                if (data[i] != one_in_fp16) {
-                    all_ones = false;
-                    break;
-                }
-            }
-        } else {
-            OPENVINO_THROW("Unsupported weight type ", weight_tensor.get_element_type());
-        }
-
-        if (!all_ones) {
-            weight_tensor.set_shape(ov::Shape{1, 1, 1,weight_tensor.get_shape()[0]});
-            auto weights_const = std::make_shared<ov::op::v0::Constant>(weight_tensor);
-            auto weights_f32 = std::make_shared<ov::op::v0::Convert>(weights_const, ov::element::f32);
-            mul = std::make_shared<ov::op::v1::Multiply>(mul, weights_f32, AutoBroadcastType::NUMPY);
-        }
-        
+        auto mul = make_rms_norm_qwen3(key, reshaped, consts, rms_norm_eps);
         auto transpose_order = std::make_shared<v0::Constant>(element::i32, Shape{4}, std::vector<int32_t>{0, 2, 1, 3});
         
         return std::make_shared<v1::Transpose>(mul, transpose_order);
