925c63d54b34f56a1172dc8603fbd12c27429899
Kazantsev, Roman <roman.kazantsev@intel.com>
2025-05-28 22:54:17 +0400

    Patch qwen2.5 chat template
    Signed-off-by: Kazantsev, Roman <roman.kazantsev@intel.com>

--- src/cpp/src/gguf_utils/gguf_tokenizer.hpp
+++ src/cpp/src/gguf_utils/gguf_tokenizer.hpp
@@ -31,5 +31,28 @@ const T* get_if_exist(const std::map<std::string, GGUFMetaData>& tokenizer_confi
     return nullptr;
 }
 
+/**
+ * @brief This function aim to patch the chat template stored in gguf model to
+ * be consistent with chat template stored in the original tokenizer_config.json of huggingface models.
+ * If certain mismatched pattern found, then the pattern will be replaced with a specific substring.
+ * Otherwise, the original chat template is returned.
+ * Current this function is used to patch the chat template for Qwen2.5 models, but the logic can be extended to other
+ * models
+ *
+ *
+ * Example: The function finds the substring for Qwen2.5:
+ * "{{\"name\": <function-name>, \"arguments\": <args-json-object>}}"
+ * in the input string (str1_content) and replaces it with:
+ * "{\"name\": <function-name>, \"arguments\": <args-json-object>}"
+ *
+ * This assumes that "<function-name>" and "<args-json-object>" are literal
+ * parts of the substring to be found.
+ *
+ * @param chat_template A string contains original chat template stored in gguf models
+ * @return patched_chat_template A new string contains updated chat template with the specific replacement made if
+ * pattern matched.
+ */
+std::string patch_gguf_chat_template(const std::string& chat_template);
+
 }  // namespace genai
 }  // namespace ov
