470bc816847374a4dc362ecba8930798ea481ba6
Ekaterina Shiryaeva <ekaterina.shiriaeva@intel.com>
2025-06-04 17:47:11 +0100

    Update Whisper stateful pipeline for NPU

--- src/cpp/src/whisper/models/decoder.cpp
+++ src/cpp/src/whisper/models/decoder.cpp
@@ -12,7 +12,8 @@
 namespace ov::genai {
 std::shared_ptr<WhisperDecoder> WhisperDecoder::from_path(const std::filesystem::path& models_path,
                                                           const std::string& device,
-                                                          const ov::AnyMap& properties) {
+                                                          const ov::AnyMap& properties,
+                                                          const ov::PartialShape& lhs_shape) {
     bool has_decoder_with_past = std::filesystem::exists(models_path / "openvino_decoder_with_past_model.xml");
 
     if (has_decoder_with_past) {
@@ -22,7 +23,7 @@ std::shared_ptr<WhisperDecoder> WhisperDecoder::from_path(const std::filesystem:
         return std::make_shared<WhisperWithPastDecoder>(models_path, device, properties);
     }
 
-    return std::make_shared<WhisperStatefullDecoder>(models_path, device, properties);
+    return std::make_shared<WhisperStatefullDecoder>(models_path, device, properties, lhs_shape);
 }
 
 std::pair<int64_t, float> WhisperDecoder::detect_language(const ov::Tensor& encoder_hidden_state,
--- src/cpp/src/whisper/models/statefull_decoder.hpp
+++ src/cpp/src/whisper/models/statefull_decoder.hpp
@@ -12,7 +12,8 @@ class WhisperStatefullDecoder : public WhisperDecoder {
 public:
     WhisperStatefullDecoder(const std::filesystem::path& models_path,
                             const std::string& device,
-                            const ov::AnyMap& properties);
+                            const ov::AnyMap& properties,
+                            const ov::PartialShape& lhs_shape);
 
     void start_async(const Tensor& encoder_hidden_state, const Tensor& input_ids, const Tensor& beam_idx) override;
 
@@ -27,6 +28,5 @@ private:
 
 private:
     ov::InferRequest m_request;
-    bool m_is_npu = false;
 };
 }  // namespace ov::genai
