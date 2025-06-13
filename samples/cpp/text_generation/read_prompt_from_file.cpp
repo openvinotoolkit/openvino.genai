f48424435c1a9c4d7bc82f06e604bdd5640d62fb
guozhong wang <guozhong.wang@intel.com>
2025-06-13 20:43:16 +0800

    Update samples/cpp/text_generation/read_prompt_from_file.cpp
    Co-authored-by: Vladimir Zlobin <vladimir.zlobin@intel.com>

--- samples/cpp/text_generation/read_prompt_from_file.cpp
+++ samples/cpp/text_generation/read_prompt_from_file.cpp
@@ -11,8 +11,7 @@ std::string utils::read_prompt(const std::string& file_path) {
     if (file.is_open()) {
         std::stringstream buffer;
         buffer << file.rdbuf();
-        prompt = buffer.str();
-        file.close();        
+        return buffer.str();
     } else {
         // show message:
         std::cout << "Error opening prompt file: " << file_path << std::endl;
