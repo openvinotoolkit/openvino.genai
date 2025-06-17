openvinotoolkit/openvino.genai

commit 662c6b9d6246343877e3c8eeea200eb5d36624c5

[wwb]: load transformers model first, then only trust_remote_code (#2270)
General huggingface recommendation is to use `trust_remote_code` only if
it is necessary, andthe model requires it, as potentially remote code
may try to run arbitrary insecure code.
Besides that some models (e.g. phi-3) may have outdated remote code
version kept for backward compatibility and non-maintained by its
authors as the model is already integrated into transformers.

Suggestion is try to load model without remote code and only if it is
failed use remote code for resolve such issues

CVS-168307

---------

Co-authored-by: andreyanufr <andrey.anufriev@intel.com>

tools/who_what_benchmark/whowhatbench/wwb.py
@@ -212,17 +212,32 @@ def load_tokenizer(args):
             from llama_cpp.llama_tokenizer import LlamaHFTokenizer
             tokenizer = LlamaHFTokenizer.from_pretrained(args.tokenizer)
         else:
-            tokenizer = AutoTokenizer.from_pretrained(
-                args.tokenizer, trust_remote_code=True
-            )
+            try:
+                tokenizer = AutoTokenizer.from_pretrained(
+                    args.tokenizer, trust_remote_code=False
+                )
+            except Exception:
+                tokenizer = AutoTokenizer.from_pretrained(
+                    args.tokenizer, trust_remote_code=True
+                )
     elif args.base_model is not None:
-        tokenizer = AutoTokenizer.from_pretrained(
-            args.base_model, trust_remote_code=True
-        )
+        try:
+            tokenizer = AutoTokenizer.from_pretrained(
+                args.base_model, trust_remote_code=False
+            )
+        except Exception:
+            tokenizer = AutoTokenizer.from_pretrained(
+                args.base_model, trust_remote_code=True
+            )
     elif args.target_model is not None:
-        tokenizer = AutoTokenizer.from_pretrained(
-            args.target_model, trust_remote_code=True
-        )
+        try:
+            tokenizer = AutoTokenizer.from_pretrained(
+                args.target_model, trust_remote_code=False
+            )
+        except Exception:
+            tokenizer = AutoTokenizer.from_pretrained(
+                args.target_model, trust_remote_code=True
+            )
 
     return tokenizer
 
@@ -232,13 +247,19 @@ def load_processor(args):
     if model_id is None:
         return None, None
 
-    config = AutoConfig.from_pretrained(model_id, trust_remote_code=True)
+    trust_remote_code = False
+    try:
+        config = AutoConfig.from_pretrained(model_id, trust_remote_code=False)
+    except Exception:
+        config = AutoConfig.from_pretrained(model_id, trust_remote_code=True)
+        trust_remote_code = True
     if "llava-qwen" in config.model_type:
         preprocessor_id = config.mm_vision_tower
     else:
         preprocessor_id = model_id
 
-    return AutoProcessor.from_pretrained(preprocessor_id, trust_remote_code=True), config
+    preprocessor = AutoProcessor.from_pretrained(preprocessor_id, trust_remote_code=trust_remote_code)
+    return preprocessor, config
 
 
 def diff_strings(a: str, b: str, *, use_loguru_colors: bool = False) -> str:

tools/who_what_benchmark/examples/openvino_batched_eval.py
@@ -23,10 +23,7 @@ MAX_NEW_TOKENS = 128
 SEQS_PER_REQUEST = 5
 MAX_SEQUENCES = 100
 
-
-model = OVModelForCausalLM.from_pretrained(
-    model_id, export=True, trust_remote_code=True
-)
+model = OVModelForCausalLM.from_pretrained(model_id, export=True)
 tokenizer = AutoTokenizer.from_pretrained(model_id)
 model_path = PosixPath(tempfile.gettempdir()) / model_id
 model.save_pretrained(model_path)

tools/who_what_benchmark/whowhatbench/model_loaders.py
@@ -22,13 +22,15 @@ class GenAIModelWrapper:
 
         if model_type == "text" or model_type == "visual-text":
             try:
-                self.config = AutoConfig.from_pretrained(model_dir, trust_remote_code=True)
-            except Exception:
                 self.config = AutoConfig.from_pretrained(model_dir)
+            except Exception:
+                self.config = AutoConfig.from_pretrained(model_dir, trust_remote_code=True)
         elif model_type == "text-to-image":
             from diffusers import DiffusionPipeline
-            self.config = DiffusionPipeline.load_config(
-                model_dir, trust_remote_code=True)
+            try:
+                self.config = DiffusionPipeline.load_config(model_dir)
+            except Exception:
+                self.config = DiffusionPipeline.load_config(model_dir, trust_remote_code=True)
 
     def __getattr__(self, attr):
         if attr in self.__dict__:
@@ -89,7 +91,12 @@ def load_text_hf_pipeline(model_id, device):
     model_kwargs = {}
 
     if not torch.cuda.is_available or device.lower() == "cpu":
-        config = AutoConfig.from_pretrained(model_id, trust_remote_code=True)
+        trust_remote_code = False
+        try:
+            config = AutoConfig.from_pretrained(model_id)
+        except Exception:
+            config = AutoConfig.from_pretrained(model_id, trust_remote_code=True)
+            trust_remote_code = True
         is_gptq = False
         is_awq = False
         if getattr(config, "quantization_config", None):
@@ -99,13 +106,19 @@ def load_text_hf_pipeline(model_id, device):
             # infer in FP32
             model_kwargs["torch_dtype"] = torch.float32
         with mock_torch_cuda_is_available(is_gptq or is_awq):
-            model = AutoModelForCausalLM.from_pretrained(model_id, trust_remote_code=True, device_map="cpu", **model_kwargs)
+            model = AutoModelForCausalLM.from_pretrained(model_id, trust_remote_code=trust_remote_code, device_map="cpu", **model_kwargs)
         if is_awq:
             model.is_awq = is_awq
     else:
-        model = AutoModelForCausalLM.from_pretrained(
-            model_id, trust_remote_code=True, device_map=device.lower(), **model_kwargs
-        )
+        try:
+            model = AutoModelForCausalLM.from_pretrained(
+                model_id, trust_remote_code=False, device_map=device.lower(), **model_kwargs
+            )
+        except Exception:
+            model = AutoModelForCausalLM.from_pretrained(
+                model_id, trust_remote_code=True, device_map=device.lower(), **model_kwargs
+            )
+
     model.eval()
     return model
 
@@ -126,7 +139,7 @@ def load_text_model(
         from optimum.intel.openvino import OVModelForCausalLM
         try:
             model = OVModelForCausalLM.from_pretrained(
-                model_id, trust_remote_code=True, device=device, ov_config=ov_config
+                model_id, device=device, ov_config=ov_config
             )
         except Exception:
             try:
@@ -177,8 +190,10 @@ def load_text2image_model(
     elif use_hf:
         from diffusers import DiffusionPipeline
         logger.info("Using HF Transformers API")
-        model = DiffusionPipeline.from_pretrained(
-            model_id, trust_remote_code=True)
+        try:
+            model = DiffusionPipeline.from_pretrained(model_id)
+        except Exception:
+            model = DiffusionPipeline.from_pretrained(model_id, trust_remote_code=True)
     else:
         logger.info("Using Optimum API")
         from optimum.intel import OVPipelineForText2Image
@@ -186,14 +201,11 @@ def load_text2image_model(
 
         try:
             model = TEXT2IMAGEPipeline.from_pretrained(
-                model_id, trust_remote_code=True, device=device, ov_config=ov_config, safety_checker=None,
+                model_id, device=device, ov_config=ov_config, safety_checker=None,
             )
         except ValueError:
-            config = AutoConfig.from_pretrained(
-                model_id, trust_remote_code=True)
             model = TEXT2IMAGEPipeline.from_pretrained(
                 model_id,
-                config=config,
                 trust_remote_code=True,
                 use_cache=True,
                 device=device,
@@ -223,15 +235,21 @@ def load_visual_text_model(
 ):
     if use_hf:
         logger.info("Using HF Transformers API")
-        config = AutoConfig.from_pretrained(model_id, trust_remote_code=True)
+        trust_remote_code = False
+        try:
+            config = AutoConfig.from_pretrained(model_id, trust_remote_code=False)
+        except Exception:
+            config = AutoConfig.from_pretrained(model_id, trust_remote_code=True)
+            trust_remote_code = True
+
         try:
             model = AutoModelForVision2Seq.from_pretrained(
-                model_id, trust_remote_code=True, device_map=device.lower()
+                model_id, trust_remote_code=trust_remote_code, device_map=device.lower()
             )
         except ValueError:
             try:
                 model = AutoModel.from_pretrained(
-                    model_id, trust_remote_code=True, device_map=device.lower()
+                    model_id, trust_remote_code=trust_remote_code, device_map=device.lower()
                 )
             except ValueError:
                 from_pretrained_kwargs = {}
@@ -243,7 +261,7 @@ def load_visual_text_model(
 
                 model = AutoModelForCausalLM.from_pretrained(
                     model_id,
-                    trust_remote_code=True,
+                    trust_remote_code=trust_remote_code,
                     device_map=device.lower(),
                     use_flash_attention_2=False,
                     **from_pretrained_kwargs,
@@ -281,7 +299,7 @@ def load_visual_text_model(
         from optimum.intel.openvino import OVModelForVisualCausalLM
         try:
             model = OVModelForVisualCausalLM.from_pretrained(
-                model_id, trust_remote_code=True, device=device, ov_config=ov_config
+                model_id, device=device, ov_config=ov_config
             )
         except ValueError:
             config = AutoConfig.from_pretrained(model_id, trust_remote_code=True)
@@ -327,13 +345,11 @@ def load_imagetext2image_model(
         from optimum.intel.openvino import OVPipelineForImage2Image
         try:
             model = OVPipelineForImage2Image.from_pretrained(
-                model_id, trust_remote_code=True, device=device, ov_config=ov_config, safety_checker=None,
+                model_id, device=device, ov_config=ov_config, safety_checker=None,
             )
         except ValueError:
-            config = AutoConfig.from_pretrained(model_id, trust_remote_code=True)
             model = OVPipelineForImage2Image.from_pretrained(
                 model_id,
-                config=config,
                 trust_remote_code=True,
                 use_cache=True,
                 device=device,
@@ -374,14 +390,12 @@ def load_inpainting_model(
         from optimum.intel.openvino import OVPipelineForInpainting
         try:
             model = OVPipelineForInpainting.from_pretrained(
-                model_id, trust_remote_code=True, device=device, ov_config=ov_config, safety_checker=None,
+                model_id, device=device, ov_config=ov_config, safety_checker=None,
             )
         except ValueError as e:
             logger.error("Failed to load inpaiting pipeline. Details:\n", e)
-            config = AutoConfig.from_pretrained(model_id, trust_remote_code=True)
             model = OVPipelineForInpainting.from_pretrained(
                 model_id,
-                config=config,
                 trust_remote_code=True,
                 use_cache=True,
                 device=device,

tools/who_what_benchmark/whowhatbench/whowhat_metrics.py
@@ -110,12 +110,18 @@ def evaluate_divergency(tokenizer, data_gold, data_prediction):
 
 class TextSimilarity:
     def __init__(self, model_id) -> None:
-        tokenizer = AutoTokenizer.from_pretrained(model_id, trust_remote_code=True)
+        trust_remote_code = False
+        try:
+            tokenizer = AutoTokenizer.from_pretrained(model_id, trust_remote_code=False)
+        except Exception:
+            trust_remote_code = True
+            tokenizer = AutoTokenizer.from_pretrained(model_id, trust_remote_code=True)
+
         if hasattr(tokenizer, "pad_token") and tokenizer.pad_token:
             pad_token = tokenizer.pad_token
         else:
             pad_token = tokenizer.eos_token
-        self.model = SentenceTransformer(model_id, tokenizer_kwargs={"pad_token": pad_token}, trust_remote_code=True)
+        self.model = SentenceTransformer(model_id, tokenizer_kwargs={"pad_token": pad_token}, trust_remote_code=trust_remote_code)
 
     def evaluate(self, gt, prediction):
         return evaluate_similarity(self.model, gt, prediction)

commit 4b299277c8d32c00ee92f49e4617c549044852a0

[WWB] Add try-except block for processor loading (#2352)
This fixes a regression introduced in
https://github.com/openvinotoolkit/openvino.genai/pull/2270

Ticket: 169203

tools/who_what_benchmark/whowhatbench/wwb.py
@@ -247,18 +247,19 @@ def load_processor(args):
     if model_id is None:
         return None, None
 
-    trust_remote_code = False
     try:
         config = AutoConfig.from_pretrained(model_id, trust_remote_code=False)
     except Exception:
         config = AutoConfig.from_pretrained(model_id, trust_remote_code=True)
-        trust_remote_code = True
     if "llava-qwen" in config.model_type:
         preprocessor_id = config.mm_vision_tower
     else:
         preprocessor_id = model_id
 
-    preprocessor = AutoProcessor.from_pretrained(preprocessor_id, trust_remote_code=trust_remote_code)
+    try:
+        preprocessor = AutoProcessor.from_pretrained(preprocessor_id, trust_remote_code=False)
+    except Exception:
+        preprocessor = AutoProcessor.from_pretrained(preprocessor_id, trust_remote_code=True)
     return preprocessor, config
 
 