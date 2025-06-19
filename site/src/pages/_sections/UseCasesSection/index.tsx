a5edb08c74c731154f47f3ce993fc63ddcce0df4
yatarkan <yaroslav.tarkan@intel.com>
2025-06-19 15:59:41 +0400

    Add use cases section

---
+++ site/src/pages/_sections/UseCasesSection/index.tsx
@@ -0,0 +1,22 @@
+import styles from './styles.module.css';
+
+import Heading from '@theme/Heading';
+
+import { ImageGeneration } from './components/image-generation';
+import { ImageProcessing } from './components/image-processing';
+import { SpeechRecognition } from './components/speech-recognition';
+import { TextGeneration } from './components/text-generation';
+
+export const UseCasesSection = () => (
+  <section className={styles.useCasesSection}>
+    <Heading as="h2" className={styles.title}>
+      Use Cases
+    </Heading>
+    <div className={styles.content}>
+      <TextGeneration />
+      <ImageGeneration />
+      <SpeechRecognition />
+      <ImageProcessing />
+    </div>
+  </section>
+);
