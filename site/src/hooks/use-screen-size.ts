1b319e2022284d105a863c92dba3b42536a7f40b
yatarkan <yaroslav.tarkan@intel.com>
2025-06-23 20:47:58 +0400

    Add screen size hook

---
+++ site/src/hooks/use-screen-size.ts
@@ -0,0 +1,27 @@
+import { useEffect, useState } from 'react';
+
+const useScreenSize = () => {
+  const [screenSize, setScreenSize] = useState({
+    width: window.innerWidth,
+    height: window.innerHeight,
+  });
+
+  useEffect(() => {
+    const handleResize = () => {
+      setScreenSize({
+        width: window.innerWidth,
+        height: window.innerHeight,
+      });
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+  return screenSize;
+};
+
+export default useScreenSize;
