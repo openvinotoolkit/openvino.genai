6bd5afd50a9c96565bb8d535eff58b0b57bd6550
Liubov Talamanova <liubov.talamanova@intel.com>
2025-06-24 17:41:58 +0100

    Fix data downloading

--- tests/python_tests/samples/test_inpainting.py
+++ tests/python_tests/samples/test_inpainting.py
@@ -5,10 +5,9 @@ import os
 import pytest
 import sys
 
-from conftest import SAMPLES_PY_DIR, SAMPLES_CPP_DIR, download_test_content
+from conftest import SAMPLES_PY_DIR, SAMPLES_CPP_DIR
 from test_utils import run_sample
 
-download_mask_image = download_test_content
 
 class TestInpainting:
     @pytest.mark.samples
