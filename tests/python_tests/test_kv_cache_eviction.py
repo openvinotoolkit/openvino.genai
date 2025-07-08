1ace3238954c8a7c00892392205b940df2c61990
Vladimir Zlobin <vladimir.zlobin@intel.com>
2025-07-08 12:24:14 +0400

    Remove devie arg

--- tests/python_tests/test_kv_cache_eviction.py
+++ tests/python_tests/test_kv_cache_eviction.py
@@ -195,7 +195,7 @@ class LongBenchTestData:
     LongBenchTestData("trec", 3.2, 2.0, 3.3),
     LongBenchTestData("qasper", 5.8, 1.7, 3.6),
 ])
-def test_optimized_generation_longbench(device, test_struct):
+def test_optimized_generation_longbench(test_struct):
     seqs_per_request = 32
     device = "CPU"
     num_kv_blocks = 1000 if device == "CPU" else 500
