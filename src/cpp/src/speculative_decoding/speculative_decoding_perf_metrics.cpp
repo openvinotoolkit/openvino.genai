a695d4e6cd6506098fc96594ada6ff5a6a94a7d4
Sofya Balandina <sofya.balandina@intel.com>
2025-07-10 10:30:10 +0100

    Add extended perf metrics for speculative decoding (#2321)
    Task: [CVS-167752](https://jira.devtools.intel.com/browse/CVS-167752)
    
    Value `std::shared_ptr<PerfMetrics> extended_perf_metrics` is added to
    all output types. `SDPerModelsPerfMetrics` is added as an extension to
    performance metrics. `SDPerModelsPerfMetrics` includes `SDPerfMetrics`
    for main and draft model. `SDPerfMetrics` is extended version of
    `PerfMetrics`. `SDPerModelsPerfMetrics` can be casted to `PerfMetrics`
    in user code.
    It was added to `SDPerfMetrics` values: latency of the second token,
    latency of the step(), amount of tokens accepted by main model, number
    of inference(for main model it is not eq to num_generated_tokens).
    
    How I suppose to use it:
    
    ```
    auto out = pipe.generate(prompt);
    auto& metrics = out.perf_metrics;
    
    if (out.extended_perf_metrics) {
            auto metrics_extra = std::dynamic_pointer_cast<ov::genai::SDPerModelsPerfMetrics>(out.extended_perf_metrics);
            std::cout << "MAIN MODEL: " << std::endl;
            std::cout << "Generate time: " << metrics_extra->main_model_metrics.get_generate_duration().mean << " ± " << metrics_extra->main_model_metrics.get_generate_duration().std << " ms" << std::endl;
            std::cout << "TTFT: " << metrics_extra->main_model_metrics.get_ttft().mean  << " ± " << metrics_extra->main_model_metrics.get_ttft().std << " ms" << std::endl;
            std::cout << "TTST: " << metrics_extra->main_model_metrics.get_ttst().mean  << " ± " << metrics_extra->main_model_metrics.get_ttst().std << " ms/token " << std::endl;
            std::cout << "TPOT: " << metrics_extra->main_model_metrics.get_tpot().mean  << " ± " << metrics_extra->main_model_metrics.get_tpot().std << " ms/token " << std::endl;
            std::cout << "Latency: " << metrics_extra->main_model_metrics.get_latency().mean  << " ± " << metrics_extra->main_model_metrics.get_latency().std << " ms/token " << std::endl;
            std::cout << "Throughput: " << metrics_extra->main_model_metrics.get_throughput().mean  << " ± " << metrics_extra->main_model_metrics.get_throughput().std << " tokens/s" << std::endl;
            std::cout << "Num generated token: " << metrics_extra->main_model_metrics.get_num_generated_tokens() << " tokens/s" << std::endl;
            std::cout << "Num accepted token: " << metrics_extra->main_model_metrics.get_num_accepted_tokens() << " tokens/s" << std::endl;
    
            std::cout << "DRAFT MODEL: " << std::endl;
            std::cout << "Generate time: " << metrics_extra->draft_model_metrics.get_generate_duration().mean << " ± " << metrics_extra->draft_model_metrics.get_generate_duration().std << " ms" << std::endl;
            std::cout << "TTFT: " << metrics_extra->draft_model_metrics.get_ttft().mean  << " ± " << metrics_extra->draft_model_metrics.get_ttft().std << " ms" << std::endl;
            std::cout << "TTST: " << metrics_extra->draft_model_metrics.get_ttst().mean  << " ± " << metrics_extra->draft_model_metrics.get_ttst().std << " ms/token " << std::endl;
            std::cout << "TPOT: " << metrics_extra->draft_model_metrics.get_tpot().mean  << " ± " << metrics_extra->draft_model_metrics.get_tpot().std << " ms/token " << std::endl;
            std::cout << "Throughput: " << metrics_extra->draft_model_metrics.get_throughput().mean  << " ± " << metrics_extra->draft_model_metrics.get_throughput().std << " tokens/s" << std::endl;
            std::cout << "Num generated token: " << metrics_extra->draft_model_metrics.get_num_generated_tokens() << " tokens/s" << std::endl;
       }
    ```
    
    Need to discusse:
    - Is this an acceptable way to extend the API for debug info or it will
    be better to make PerfMetrcics as ptr or made something else?
    - Why do we need second token latency ? How it should be calculated for
    main model (latency of step() or latency of step() / (accepted tokens +
    1) ?
    - What else could be added / what could be removed ?

---
+++ src/cpp/src/speculative_decoding/speculative_decoding_perf_metrics.cpp
@@ -0,0 +1,110 @@
+// Copyright (C) 2023-2025 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
+
+// #pragma once
+
+#include <chrono>
+#include <map>
+#include <string>
+#include <vector>
+#include <ostream>
+#include <iostream>
+
+#include "openvino/genai/perf_metrics.hpp"
+#include "openvino/genai/speculative_decoding/perf_metrics.hpp"
+
+namespace ov::genai {
+
+MeanStdPair calc_mean_and_std(const std::vector<MicroSeconds>& durations);
+
+MeanStdPair ov::genai::SDPerfMetrics::get_ttst() {
+    evaluate_statistics();
+    return ttst;
+};
+
+MeanStdPair ov::genai::SDPerfMetrics::get_latency() {
+    evaluate_statistics();
+    return avg_latency;
+};
+
+void ov::genai::SDPerfMetrics::evaluate_statistics(std::optional<TimePoint> start_time) {
+    if (m_evaluated)
+        return;
+
+    std::vector<ov::genai::MicroSeconds> second_token_duration;
+    std::vector<ov::genai::MicroSeconds> topt_durations;
+    std::vector<ov::genai::MicroSeconds> latency_durations;
+    std::vector<ov::genai::MicroSeconds> apt_durations;
+    if (raw_metrics.m_durations.size() > 0 && raw_metrics.m_batch_sizes.size() > 0) {
+        auto& durations = raw_metrics.m_durations;
+        auto& batch_sizes = raw_metrics.m_batch_sizes;
+
+        raw_metrics.m_times_to_first_token.clear();
+        raw_metrics.m_times_to_first_token.emplace_back(durations[0]);
+
+        num_generated_tokens = batch_sizes[0];
+
+        raw_metrics.generate_durations.clear();
+        raw_metrics.generate_durations.emplace_back(durations[0]);
+
+        for (size_t i = 1; i < durations.size(); ++i) {
+            if (i == 1) {
+                second_token_duration.emplace_back(durations[i]);
+            } else {
+                latency_durations.emplace_back(durations[i]);
+                topt_durations.emplace_back(durations[i] / batch_sizes[i]);
+            }
+
+            raw_metrics.generate_durations[0] += durations[i];
+            num_generated_tokens += batch_sizes[i];
+        }
+    }
+
+    generate_duration = calc_mean_and_std(raw_metrics.generate_durations);
+
+    ttft = ov::genai::calc_mean_and_std(raw_metrics.m_times_to_first_token);
+    ttst = ov::genai::calc_mean_and_std(second_token_duration);
+    tpot = ov::genai::calc_mean_and_std(topt_durations);
+
+    avg_latency = ov::genai::calc_mean_and_std(latency_durations);
+
+    inference_duration = ov::genai::calc_mean_and_std(raw_metrics.m_inference_durations);
+
+    throughput = {1000.0f / tpot.mean, (tpot.std * 1000.0f) / (tpot.mean * tpot.mean)};
+
+    m_evaluated = true;
+}
+
+ov::genai::SDPerModelsPerfMetrics::SDPerModelsPerfMetrics() {
+    raw_metrics.m_inference_durations =  {{ MicroSeconds(0.0f) }};
+    main_model_metrics.raw_metrics.m_inference_durations =  {{ MicroSeconds(0.0f) }};
+    draft_model_metrics.raw_metrics.m_inference_durations =  {{ MicroSeconds(0.0f) }};
+}
+
+size_t ov::genai::SDPerModelsPerfMetrics::get_num_accepted_tokens() {
+    evaluate_statistics();
+    return num_accepted_tokens;
+};
+    
+void ov::genai::SDPerModelsPerfMetrics::evaluate_statistics(std::optional<TimePoint> start_time) {
+    if (m_evaluated)
+        return;
+
+    ov::genai::PerfMetrics::evaluate_statistics(start_time);
+    // recalculate tpot to take into account all generated tokens and calculate num_accepted_tokens
+    if (raw_metrics.m_new_token_times.size() > 0 && raw_metrics.m_batch_sizes.size() > 0) {
+        auto& tok_times = raw_metrics.m_new_token_times;
+        auto& batch_sizes = raw_metrics.m_batch_sizes;
+        num_accepted_tokens = 0;
+
+        for (size_t i = 1; i < tok_times.size(); ++i) {
+            num_accepted_tokens += batch_sizes[i] - 1;
+        }
+    }
+    main_model_metrics.evaluate_statistics(start_time);
+    draft_model_metrics.evaluate_statistics(start_time);
+
+    m_evaluated = true;
+}
+
+}  // namespace ov::genai
