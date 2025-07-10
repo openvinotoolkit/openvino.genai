3a596ceba5b2aecd8a728e755c37bbfd1cb8068b
Brandon Weng <wengbrandon@gmail.com>
2025-07-10 10:40:29 -0400

    revert ov_genai_generation_config_set_num_beam_groups change

--- src/c/include/openvino/genai/c/generation_config.h
+++ src/c/include/openvino/genai/c/generation_config.h
@@ -151,7 +151,7 @@ OPENVINO_GENAI_C_EXPORTS ov_status_e ov_genai_generation_config_set_stop_token_i
  * @param value The number of beam groups.
  * @return ov_status_e A status code, return OK(0) if successful.
  */
-OPENVINO_GENAI_C_EXPORTS ov_status_e ov_genai_generation_config_set_num_beam_group(ov_genai_generation_config* config,
+OPENVINO_GENAI_C_EXPORTS ov_status_e ov_genai_generation_config_set_num_beam_groups(ov_genai_generation_config* config,
                                                                                    const size_t value);
 
 /**
