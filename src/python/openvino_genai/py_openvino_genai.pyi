e2926bd1707b04d2cb9f2422e417d6df5eaf88c3
Pavel Esir <pavel.esir@intel.com>
2025-07-11 11:00:22 +0200

    resolve conflicts

--- src/python/openvino_genai/py_openvino_genai.pyi
+++ src/python/openvino_genai/py_openvino_genai.pyi
@@ -546,6 +546,12 @@ class ExtendedPerfMetrics:
         :param get_detokenization_duration: Returns the mean and standard deviation of detokenization durations in milliseconds.
         :type get_detokenization_duration: MeanStdPair
     
+        :param get_grammar_compiler_init_times: Returns a map with the time to initialize the grammar compiler for each backend in milliseconds.
+        :type get_grammar_compiler_init_times: dict[str, float]
+    
+        :param get_grammar_compile_time: Returns the mean, standard deviation, min, and max of grammar compile times in milliseconds.
+        :type get_grammar_compile_time: SummaryStats
+    
         :param raw_metrics: A structure of RawPerfMetrics type that holds raw metrics.
         :type raw_metrics: RawPerfMetrics
     """
