7e488e0b41869e027d964adec71981cd24a0c1c3
Alexander Suvorov <alexander.suvorov@intel.com>
2025-07-11 13:51:04 +0200

    Add samples test

--- tests/python_tests/samples/test_rag.py
+++ tests/python_tests/samples/test_rag.py
@@ -8,6 +8,7 @@ import sys
 from conftest import SAMPLES_PY_DIR, SAMPLES_CPP_DIR
 from test_utils import run_sample
 
+
 class TestTextEmbeddingPipeline:
     @pytest.mark.rag
     @pytest.mark.samples
@@ -22,6 +23,25 @@ class TestTextEmbeddingPipeline:
         cpp_sample = os.path.join(SAMPLES_CPP_DIR, "text_embeddings")
         cpp_command = [cpp_sample, convert_model, "Document 1", "Document 2"]
         cpp_result = run_sample(cpp_command)
-        
+
+        # Compare results
+        assert py_result.stdout == cpp_result.stdout, "Python and C++ results should match"
+
+
+class TestTextRerankPipeline:
+    @pytest.mark.rag
+    @pytest.mark.samples
+    @pytest.mark.parametrize("convert_model", ["cross-encoder/ms-marco-TinyBERT-L2-v2"], indirect=True)
+    def test_sample_text_rerank_pipeline(self, convert_model):
+        # Run Python sample
+        py_script = os.path.join(SAMPLES_PY_DIR, "rag/text_rerank.py")
+        py_command = [sys.executable, py_script, convert_model, "Document 1", "Document 2"]
+        py_result = run_sample(py_command)
+
+        # Run C++ sample
+        cpp_sample = os.path.join(SAMPLES_CPP_DIR, "text_rerank")
+        cpp_command = [cpp_sample, convert_model, "Document 1", "Document 2"]
+        cpp_result = run_sample(cpp_command)
+
         # Compare results
         assert py_result.stdout == cpp_result.stdout, "Python and C++ results should match"
