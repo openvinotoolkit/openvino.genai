6485d9aa7358701ab71799978830a3c026777832
yatarkan <yaroslav.tarkan@intel.com>
2025-07-15 17:38:03 +0400

    Fix duplicated test

--- tests/python_tests/test_tokenizer.py
+++ tests/python_tests/test_tokenizer.py
@@ -216,23 +216,6 @@ def test_non_string_chat_template(ov_hf_tokenizers):
 
 
 @pytest.mark.precommit
-@pytest.mark.parametrize("ov_hf_tokenizers", ["CohereLabs/aya-23-8B"], indirect=True)
-def test_non_string_chat_template(ov_hf_tokenizers):
-    ov_tokenizer, hf_tokenizer = ov_hf_tokenizers
-    
-    hf_full_history_str = hf_tokenizer.apply_chat_template(
-        conversation, add_generation_prompt=False, tokenize=False,
-    )
-
-    ov_full_history_str = ov_tokenizer.apply_chat_template(conversation, add_generation_prompt=False)
-
-    if ov_full_history_str != hf_full_history_str:
-        print(f"hf reference: {hf_full_history_str}")
-        print(f"ov_genai out: {ov_full_history_str}")
-    assert ov_full_history_str == hf_full_history_str
-
-
-@pytest.mark.precommit
 @pytest.mark.parametrize("ov_hf_tokenizers", get_models_list(), indirect=True)
 def test_set_chat_template(ov_hf_tokenizers):
     ov_tokenizer, hf_tokenizer = ov_hf_tokenizers
