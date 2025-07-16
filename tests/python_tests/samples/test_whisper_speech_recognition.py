d1446d4bc77e5b0a598838f21463090877040929
Brandon Weng <wengbrandon@gmail.com>
2025-07-16 11:30:08 -0400

    Look for the cBinary in the right place for Python test

--- tests/python_tests/samples/test_whisper_speech_recognition.py
+++ tests/python_tests/samples/test_whisper_speech_recognition.py
@@ -13,7 +13,7 @@ class TestWhisperSpeechRecognition:
     @pytest.mark.samples
     @pytest.mark.parametrize("convert_model", ["WhisperTiny"], indirect=True)
     @pytest.mark.parametrize("download_test_content", ["how_are_you_doing_today.wav"], indirect=True)
-    def test_sample_whisper_speech_recognition(self, convert_model, download_test_content):           
+    def test_sample_whisper_speech_recognition(self, convert_model, download_test_content):
         # Run Python sample
         py_script = os.path.join(SAMPLES_PY_DIR, "whisper_speech_recognition/whisper_speech_recognition.py")
         py_command = [sys.executable, py_script, convert_model, download_test_content]
@@ -25,7 +25,7 @@ class TestWhisperSpeechRecognition:
         cpp_result = run_sample(cpp_command)
 
         # Run C sample
-        c_sample = os.path.join(SAMPLES_C_DIR, 'whisper_speech_recognition', 'whisper_speech_recognition_c')
+        c_sample = os.path.join(SAMPLES_C_DIR, 'whisper_speech_recognition_c')
         c_command = [c_sample, '-m', convert_model, '-i', download_test_content]
         c_result = run_sample(c_command)
 
