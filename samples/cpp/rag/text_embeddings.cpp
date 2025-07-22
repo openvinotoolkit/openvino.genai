e8d0bd744c3d2e67f10459e077d1b8811d4b6dd5
Alexander Suvorov <alexander.suvorov@intel.com>
2025-07-22 13:03:30 +0200

    Revert cpp sample

--- samples/cpp/rag/text_embeddings.cpp
+++ samples/cpp/rag/text_embeddings.cpp
@@ -4,15 +4,18 @@
 #include "openvino/genai/rag/text_embedding_pipeline.hpp"
 
 int main(int argc, char* argv[]) try {
+    if (argc < 3) {
+        throw std::runtime_error(std::string{"Usage: "} + argv[0] + " <MODEL_DIR> '<TEXT 1>' ['<TEXT 2>' ...]");
+    }
     auto documents = std::vector<std::string>(argv + 2, argv + argc);
     std::string models_path = argv[1];
 
-    ov::genai::TextEmbeddingPipeline pipeline(
-        models_path,
-        "CPU",
-        ov::genai::pooling_type(ov::genai::TextEmbeddingPipeline::PoolingType::MEAN),
-        ov::genai::normalize(true)
-    );
+    std::string device = "CPU";  // GPU can be used as well
+
+    ov::genai::TextEmbeddingPipeline::Config config;
+    config.pooling_type = ov::genai::TextEmbeddingPipeline::PoolingType::MEAN;
+
+    ov::genai::TextEmbeddingPipeline pipeline(models_path, device, config);
 
     ov::genai::EmbeddingResults embeddings = pipeline.embed_documents(documents);
     ov::genai::EmbeddingResult query_embedding = pipeline.embed_query("What is the capital of France?");
