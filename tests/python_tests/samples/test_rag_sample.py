7bf4756ffd27494bb86f9b7061578bbeb5bc07fd
Xiake Sun <xiake.sun@intel.com>
2025-07-30 09:56:10 +0800

    Fix pytest import error due to ambiguous file name

--- tests/python_tests/samples/test_rag_sample.py
+++ tests/python_tests/samples/test_rag_sample.py
@@ -10,7 +10,7 @@ from test_utils import run_sample
 
 
 class TestTextEmbeddingPipeline:
-    @pytest.mark.rag
+    #@pytest.mark.rag
     @pytest.mark.samples
     @pytest.mark.parametrize("convert_model", ["bge-small-en-v1.5"], indirect=True)
     def test_sample_text_embedding_pipeline(self, convert_model):
@@ -35,7 +35,7 @@ class TestTextEmbeddingPipeline:
 
 
 class TestTextRerankPipeline:
-    @pytest.mark.rag
+    #@pytest.mark.rag
     @pytest.mark.samples
     @pytest.mark.parametrize("convert_model", ["ms-marco-TinyBERT-L2-v2"], indirect=True)
     def test_sample_text_rerank_pipeline(self, convert_model):
