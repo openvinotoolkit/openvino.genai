190c2c06c4cc442f6a2c29ee9d7297414dbeea27
Artur Paniukov <artur.paniukov@intel.com>
2025-08-12 17:22:15 +0100

    Update Tests

--- tests/python_tests/samples/test_compound_grammar_sample.py
+++ tests/python_tests/samples/test_compound_grammar_sample.py
@@ -3,7 +3,6 @@
 
 import pytest
 import sys
-from pathlib import Path
 
 from conftest import SAMPLES_PY_DIR
 from test_utils import run_sample
@@ -13,6 +12,6 @@ from test_utils import run_sample
 @pytest.mark.samples
 @pytest.mark.parametrize("convert_model", ["TinyLlama-1.1B-Chat-v1.0"], indirect=True)
 def test_structured_output_sample(convert_model):
-    py_script = Path(SAMPLES_PY_DIR) / "text_generation" / "compound_grammar_generation.py"
-    py_command = [sys.executable, str(py_script), convert_model]
+    py_script = SAMPLES_PY_DIR / "text_generation" / "compound_grammar_generation.py"
+    py_command = [sys.executable, py_script, convert_model]
     run_sample(py_command)
