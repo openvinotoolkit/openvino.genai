6f1e1d2a788880c90db629a97e546a811fab8b76
John Feng <john.feng@intel.com>
2025-08-19 23:19:40 +0800

    update image 2 image sample to support text only input

--- samples/python/image_generation/image2image.py
+++ samples/python/image_generation/image2image.py
@@ -18,17 +18,26 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('model_dir')
     parser.add_argument('prompt')
-    parser.add_argument('image')
+    parser.add_argument('image', nargs='?', default=None)
     args = parser.parse_args()
 
     device = 'CPU'  # GPU can be used as well
     pipe = openvino_genai.Image2ImagePipeline(args.model_dir, device)
 
-    image = read_image(args.image)
-
-    image_tensor = pipe.generate(args.prompt, image,
-        strength=0.8 # controls how initial image is noised after being converted to latent space. `1` means initial image is fully noised
-    )
+    if args.image is None:
+        image = read_image(args.image)
+        image_tensor = pipe.generate(args.prompt, image,
+            strength=0.8 # controls how initial image is noised after being converted to latent space. `1` means initial image is fully noised
+        )
+    else:
+        image_tensor = pipe.generate(
+            args.prompt,
+            width=512,
+            height=512,
+            num_inference_steps=20,
+            num_images_per_prompt=1,
+            strength=1.0 # 'strength' must be 1.0f for Image 2 image pipeline without initial image
+        )
 
     image = Image.fromarray(image_tensor.data[0])
     image.save("image.bmp")
