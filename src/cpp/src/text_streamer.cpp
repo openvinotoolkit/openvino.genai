28a646141d7647ddcc651b20efbe884b8011373f
Damian Kalinowski <damian.kalinowski@intel.com>
2025-09-11 12:44:28 +0200

    save

--- src/cpp/src/text_streamer.cpp
+++ src/cpp/src/text_streamer.cpp
@@ -23,7 +23,7 @@ TextStreamer::TextStreamer(const Tokenizer& tokenizer,
 StreamingStatus TextStreamer::write(int64_t token) {
     std::stringstream res;
     m_tokens_cache.push_back(token);
-    std::string text = m_tokenizer.decode(m_tokens_cache);
+    std::string text = m_tokenizer.decode(m_tokens_cache, ov::AnyMap{ov::genai::skip_special_tokens(false)});
     m_decoded_lengths.push_back(text.length());
 
     if (!text.empty() && '\n' == text.back() && text.size() > m_printed_len) {
@@ -69,7 +69,7 @@ void TextStreamer::compute_decoded_length_for_position(size_t cache_position) {
     }
 
     auto cache_for_position = std::vector(m_tokens_cache.begin(), m_tokens_cache.begin() + cache_position + 1);
-    std::string text_for_position = m_tokenizer.decode(cache_for_position);
+    std::string text_for_position = m_tokenizer.decode(cache_for_position, ov::AnyMap{ov::genai::skip_special_tokens(false)});
 
     if (is_incomplete(text_for_position)) {
         m_decoded_lengths[cache_position] = -1;
@@ -109,7 +109,7 @@ StreamingStatus TextStreamer::run_callback_if_needed(const std::string& text) {
 
 void TextStreamer::end() {
     std::stringstream res;
-    std::string text = m_tokenizer.decode(m_tokens_cache);
+    std::string text = m_tokenizer.decode(m_tokens_cache, ov::AnyMap{ov::genai::skip_special_tokens(false)});
     if (text.size() <= m_printed_len)
         return;
     res << std::string_view{text.data() + m_printed_len, text.size() - m_printed_len} << std::flush;
