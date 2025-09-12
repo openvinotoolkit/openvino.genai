fca21eb93b05e87226a660330c0f0343d422ad64
ceciliapeng2011 <cecilia.peng@intel.com>
2025-09-12 15:29:17 +0800

    chang gpu_block_size to 256

--- src/cpp/src/continuous_batching/cache_manager.hpp
+++ src/cpp/src/continuous_batching/cache_manager.hpp
@@ -46,7 +46,7 @@ public:
                         "Continuous batching: execution device is expected to be single CPU / single GPU / multi GPUs");
         m_device = execution_devices[0];
         // set block_size depending on device
-        const size_t cpu_block_size = 32, gpu_block_size = 16;
+        const size_t cpu_block_size = 32, gpu_block_size = 256;
         m_block_size = all_gpu_device ? gpu_block_size : cpu_block_size;
 
         if (all_gpu_device) {
