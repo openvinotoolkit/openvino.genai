13f9db4db6b39585986758c277500d2b8ca4b5c0
Mikhail Ryzhov <mikhail.ryzhov@intel.com>
2025-09-19 15:00:49 +0200

    fixed cmd

--- .github/actions/install_wheel/dist/index.js
+++ .github/actions/install_wheel/dist/index.js
@@ -35844,8 +35844,7 @@ async function installPackages(packages, localWheelDir, requirementsFiles) {
       try {
         core.debug(`Attempt ${attempt} of 3`);
         const { stdout, stderr } = await execAsync(
-          'python',
-          ['-m', 'pip', 'install', ...installArgs],
+          ['python', '-m', 'pip', 'install', ...installArgs],
           {
             stdio: 'inherit'
           }
