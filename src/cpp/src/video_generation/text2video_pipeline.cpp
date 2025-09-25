42af2e89e9bfa7f3109032ce8ea66020e63b436c
Vladimir Zlobin <vladimir.zlobin@intel.com>
2025-09-26 01:22:06 +0400

    apply todo

--- src/cpp/src/video_generation/text2video_pipeline.cpp
+++ src/cpp/src/video_generation/text2video_pipeline.cpp
@@ -25,72 +25,17 @@
 using namespace ov::genai;
 
 class Text2VideoPipeline::LTXPipeline {
-public:
     using Ms = std::chrono::duration<float, std::ratio<1, 1000>>;
 
-    float m_load_time_ms{0};
     std::shared_ptr<IScheduler> m_scheduler;
     std::shared_ptr<T5EncoderModel> m_t5_text_encoder;
     std::shared_ptr<LTXVideoTransformer3DModel> m_transformer;
     std::shared_ptr<AutoencoderKLLTXVideo> m_vae;
-    VideoGenerationConfig m_generation_config;
     VideoGenerationPerfMetrics m_perf_metrics;
     double m_latent_timestep = -1.0;  // TODO: float?
-    LTXPipeline(const std::filesystem::path& models_dir, const std::string& device, const ov::AnyMap& properties) {
-        auto start_time = std::chrono::steady_clock::now();
-        // TODO: move to common
-        const std::filesystem::path model_index_path = models_dir / "model_index.json";
-        std::ifstream file(model_index_path);
-        OPENVINO_ASSERT(file.is_open(), "Failed to open ", model_index_path);
+    float m_load_time_ms;
 
-        nlohmann::json data = nlohmann::json::parse(file);
-        using utils::read_json_param;
-
-        const std::string class_name = data["_class_name"].get<std::string>();
-        OPENVINO_ASSERT(class_name == "LTXPipeline");
-
-        // TODO: initializer list
-        set_scheduler(Scheduler::from_config(models_dir / "scheduler/scheduler_config.json"));
-
-        const std::string text_encoder = data["text_encoder"][1].get<std::string>();
-        OPENVINO_ASSERT("T5EncoderModel" == text_encoder);
-        m_t5_text_encoder = std::make_shared<T5EncoderModel>(models_dir / "text_encoder", device, properties);
-
-        const std::string transformer = data["transformer"][1].get<std::string>();
-        OPENVINO_ASSERT("LTXVideoTransformer3DModel" == transformer);
-        m_transformer = std::make_shared<LTXVideoTransformer3DModel>(models_dir / "transformer", device, properties);
-
-        const std::string vae = data["vae"][1].get<std::string>();
-        OPENVINO_ASSERT("AutoencoderKLLTXVideo" == vae);
-        m_vae = std::make_shared<AutoencoderKLLTXVideo>(models_dir / "vae_decoder", device, properties);
-
-        initialize_generation_config(class_name);
-        m_load_time_ms = Ms{std::chrono::steady_clock::now() - start_time}.count();
-    }
-    void check_image_size(const int height, const int width) const {
-        // TODO:
-        // if height % 32 != 0 or width % 32 != 0:
-        //     raise ValueError(f"`height` and `width` have to be divisible by 32 but are {height} and {width}.")
-        OPENVINO_ASSERT(m_transformer != nullptr);
-        const size_t vae_scale_factor = m_vae->get_vae_scale_factor();
-        OPENVINO_ASSERT((height % vae_scale_factor == 0 || height < 0) && (width % vae_scale_factor == 0 || width < 0),
-                        "Both 'width' and 'height' must be divisible by ",
-                        vae_scale_factor);
-    }
-    void check_inputs(const ImageGenerationConfig& generation_config) const {
-        check_image_size(generation_config.width, generation_config.height);
-
-        OPENVINO_ASSERT(generation_config.max_sequence_length <= 512, "T5's 'max_sequence_length' must be less or equal to 512");
-
-        OPENVINO_ASSERT(generation_config.negative_prompt == std::nullopt, "Negative prompt is not used by FluxPipeline");
-        OPENVINO_ASSERT(generation_config.negative_prompt_2 == std::nullopt, "Negative prompt 2 is not used by FluxPipeline");
-        OPENVINO_ASSERT(generation_config.negative_prompt_3 == std::nullopt, "Negative prompt 3 is not used by FluxPipeline");
-        OPENVINO_ASSERT(generation_config.prompt_3 == std::nullopt, "Prompt 3 is not used by FluxPipeline");
-
-        OPENVINO_ASSERT(generation_config.strength == 1.0f, "'Strength' generation parameter must be 1.0f for Text 2 image pipeline");
-    }
-    void compute_hidden_states(const std::string& positive_prompt, const std::string& negative_prompt, const ImageGenerationConfig& generation_config) {
-        // std::string prompt_2_str = generation_config.prompt_2 != std::nullopt ? *generation_config.prompt_2 : positive_prompt;
+    void compute_hidden_states(const std::string& positive_prompt, const std::string& negative_prompt, const VideoGenerationConfig& generation_config) {
         auto infer_start = std::chrono::steady_clock::now();
         bool do_classifier_free_guidance = generation_config.guidance_scale > 1.0;
         ov::Tensor prompt_embeds = m_t5_text_encoder->infer(
@@ -107,8 +52,6 @@ public:
         m_perf_metrics.encoder_inference_duration["text_encoder"] = Ms{infer_end - infer_start}.count();
 
         prompt_embeds = numpy_utils::repeat(prompt_embeds, generation_config.num_images_per_prompt);
-        ov::Tensor ref_prompt_embeds = from_npy("prompt_embeds.npy");
-        OPENVINO_ASSERT(max_diff(prompt_embeds, ref_prompt_embeds) == 0.0f);
 
         // text_ids = torch.zeros(prompt_embeds.shape[1], 3)
         ov::Shape text_ids_shape = {prompt_embeds.get_shape()[1], 3};
@@ -133,16 +76,36 @@ public:
         // m_transformer->set_hidden_states("txt_ids", text_ids);
         // m_transformer->set_hidden_states("img_ids", latent_image_ids);
     }
-    /**
-     * Generates image(s) based on prompt and other image generation parameters
-     * @param positive_prompt Prompt to generate image(s) from
-     * @param negative_prompt
-     * @param properties Image generation parameters specified as properties. Values in 'properties' override default value for generation parameters.
-     * @returns A tensor which has dimensions [num_images_per_prompt, height, width, 3]
-     */
+
+public:
+    VideoGenerationConfig m_generation_config;
+
+    LTXPipeline(
+        const std::filesystem::path& models_dir,
+        const std::string& device,
+        const ov::AnyMap& properties,
+        std::chrono::steady_clock::time_point start_time = std::chrono::steady_clock::now()
+    ) :
+            m_scheduler{cast_scheduler(Scheduler::from_config(models_dir / "scheduler/scheduler_config.json"))},
+            m_t5_text_encoder{std::make_shared<T5EncoderModel>(models_dir / "text_encoder", device, properties)},
+            m_transformer{std::make_shared<LTXVideoTransformer3DModel>(models_dir / "transformer", device, properties)},
+            m_vae{std::make_shared<AutoencoderKLLTXVideo>(models_dir / "vae_decoder", device, properties)},
+            m_generation_config{VideoGenerationConfig{ImageGenerationConfig{
+                .guidance_scale = 3.0f,
+                .height = 512,
+                .width = 704,
+                .num_inference_steps = 50,
+                .max_sequence_length = 128,
+                .strength = 1.0f,
+            }}} {
+        const std::filesystem::path model_index_path = models_dir / "model_index.json";
+        std::ifstream file(model_index_path);
+        OPENVINO_ASSERT(file.is_open(), "Failed to open ", model_index_path);
+        OPENVINO_ASSERT("LTXPipeline" == nlohmann::json::parse(file)["_class_name"].get<std::string>());
+        m_load_time_ms = Ms{std::chrono::steady_clock::now() - start_time}.count();
+    }
+
     ov::Tensor generate(const std::string& positive_prompt, const std::string& negative_prompt, const ov::AnyMap& properties = {}) {
-        // TODO: OVLTXPipeline allows prompt_embeds and prompt_attention_mask instead of prompt; Same for negative_prompt_embeds and negative_prompt_attention_mask
-        // TODO: OVLTXPipeline allows batched generation with multiple prompts
         const auto gen_start = std::chrono::steady_clock::now();
         m_perf_metrics.clean_up();
         VideoGenerationConfig merged_generation_config = m_generation_config;
@@ -161,9 +124,7 @@ public:
         const size_t vae_scale_factor = m_vae->get_vae_scale_factor();
         const auto& transformer_config = m_transformer->get_config();
 
-        check_inputs(merged_generation_config);
-
-        // set_lora_adapters(merged_generation_config.adapters);
+        check_inputs(merged_generation_config, vae_scale_factor);
 
         compute_hidden_states(positive_prompt, negative_prompt, merged_generation_config);
 
@@ -180,7 +141,6 @@ public:
             transformer_spatial_patch_size,
             transformer_temporal_patch_size
         );
-        OPENVINO_ASSERT(max_diff(latents, from_npy("latents.npy")) == 0.0f);
 
         // Prepare timesteps
         size_t latent_num_frames = (merged_generation_config.num_frames - 1) / temporal_compression_ratio + 1;
@@ -189,8 +149,6 @@ public:
         size_t video_sequence_length = latent_num_frames * latent_height * latent_width;
         m_scheduler->set_timesteps(video_sequence_length, merged_generation_config.num_inference_steps, merged_generation_config.strength);
         std::vector<float> timesteps = m_scheduler->get_float_timesteps();
-        ov::Tensor ref_timesteps = from_npy("timesteps.npy");
-        OPENVINO_ASSERT(max_diff(ov::Tensor{ov::element::f32, {timesteps.size()}, timesteps.data()}, ref_timesteps) < 0.0002f);
 
         // Denoising loop
         // ov::Tensor timestep(ov::element::f32, {1});
@@ -237,38 +195,6 @@ public:
         //     std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::steady_clock::now() - gen_start).count();
         // return image;
         return {};
-    }
-
-    template <typename... Properties>
-    ov::util::EnableIfAllStringAny<ov::Tensor, Properties...> generate(
-            const std::string& positive_prompt,
-            const std::string& negative_prompt,
-            Properties&&... properties) {
-        return generate(positive_prompt, negative_prompt, ov::AnyMap{std::forward<Properties>(properties)...});
-    }
-    void initialize_generation_config(const std::string& class_name) {
-        // TODO: move to common
-        OPENVINO_ASSERT(m_transformer != nullptr);
-        OPENVINO_ASSERT(m_vae != nullptr);
-
-        const auto& transformer_config = m_transformer->get_config();
-        const size_t vae_scale_factor = m_vae->get_vae_scale_factor();
-
-        m_generation_config = VideoGenerationConfig();  // TODO: Would video generation be config different?
-
-        m_generation_config.height = 512;
-        m_generation_config.width = 704;
-
-        m_generation_config.guidance_scale = 3.0f;
-        m_generation_config.num_inference_steps = 50;
-        m_generation_config.strength = 1.0f;
-        m_generation_config.max_sequence_length = 128;
-    }
-
-    void set_scheduler(std::shared_ptr<Scheduler> scheduler) {
-        auto casted = std::dynamic_pointer_cast<IScheduler>(scheduler);
-        OPENVINO_ASSERT(casted != nullptr, "Passed incorrect scheduler type");
-        m_scheduler = casted;
     }
 };
 namespace {
