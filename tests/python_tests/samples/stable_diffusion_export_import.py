e117fde3407fa0b9c08d3a1c81977da715c422ed
Alexander Suvorov <alexander.suvorov@intel.com>
2025-09-26 13:56:24 +0200

    Add samples test

---
+++ tests/python_tests/samples/stable_diffusion_export_import.py
@@ -0,0 +1,25 @@
+# Copyright (C) 2025 Intel Corporation
+# SPDX-License-Identifier: Apache-2.0
+
+import pytest
+import sys
+
+from conftest import SAMPLES_PY_DIR, SAMPLES_CPP_DIR
+from test_utils import run_sample
+
+class TestStableDiffusionExportImport:
+    @pytest.mark.samples
+    @pytest.mark.LCM_Dreamshaper_v7_int8_ov
+    @pytest.mark.parametrize("executable", [
+        [SAMPLES_CPP_DIR / "stable_diffusion_export_import"],
+        [sys.executable, SAMPLES_PY_DIR / "image_generation/stable_diffusion_export_import.py"],
+    ])
+    @pytest.mark.parametrize(
+        "download_model, prompt",
+        [
+            pytest.param("LCM_Dreamshaper_v7-int8-ov", "cyberpunk cityscape like Tokyo New York  with tall buildings at dusk golden hour cinematic lighting"),
+        ],
+        indirect=["download_model"],
+    )
+    def test_sample_stable_diffusion_export_import(self, executable, download_model, prompt):
+        run_sample(executable + [download_model, '"' + prompt + '"'])
