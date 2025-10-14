openvinotoolkit/openvino.genai

commit e8403813ecb6b10659091796bfff612fc0193dff

[llm_bench] Refactor class mapping and add qwen3 to text_embeds/rerank supported list (#2782)
## Description

1. qwen models can be rerananking/embedding/text generation models,
class mapping is refactored and --task option added to handle such
cases.
2. special cases added for QwenEmbedding/Reranking models.

examples:
Qwen/Qwen3-Reranker-0.6B
`python ./tools/llm_bench/benchmark.py -m ./models/Qwen3-Reranker-0.6B/
--task text_rerank`
Qwen3-Embedding-0.6B
`python ./tools/llm_bench/benchmark.py -m
./models/Qwen3-Embedding-0.6B_new/ --task text_embed --embedding_pooling
last_token --embedding_padding_side left --embedding_normalize`

<!--- Jira ticket number (e.g., 123). Delete if there's no ticket. Don't
include full link or project name. -->
Ticket: connected with CVS-171040

## Checklist:
- [x] Tests have been updated or added to cover the new code <!--- If
the change isn't maintenance related, update the tests at
https://github.com/openvinotoolkit/openvino.genai/tree/master/tests or
explain in the description why the tests don't need an update. -->
- [ ] This patch fully addresses the ticket. <!--- If follow-up pull
requests are needed, specify in description. -->
- [ ] I have made corresponding changes to the documentation

tools/llm_bench/benchmark.py
@@ -188,25 +188,30 @@ def get_argprser():
     parser.add_argument('-mi', '--mask_image', default=None,
                         help='Mask image for Inpainting pipelines. Can be directory or path to single image. Applicable for Image Generation.')
     parser.add_argument('-t', '--task', default=None,
-                        help='The task to setup the pipeline/config for. Applicable for Text to Image/Image to imaage/Inpainting pipelines.')
+                        choices=['text_gen', 'image_gen', "visual_text_gen", 'speech_to_text', 'image_cls', 'code_gen',
+                                 'ldm_super_resolution', 'text_embed', 'text_rerank', 'text_to_speech', "text-to-image", "image-to-image", "inpainting"],
+                        help='The task to setup the pipeline type')
     parser.add_argument(
         '--strength', type=float, default=None,
         help='Applicable for Image to imaage/Inpainting pipelines. Indicates extent to transform the reference `image`. Must be between 0 and 1.')
     parser.add_argument("--disable_prompt_permutation", action="store_true", help="Disable modification prompt from run to run for avoid prefix caching")
-    parser.add_argument("--embedding_pooling", choices=["cls", "mean"], default=None, help="Pooling type CLS or MEAN. Applicable only for text embeddings")
+    parser.add_argument("--embedding_pooling", choices=["cls", "mean", "last_token"], default=None,
+                        help="Pooling type CLS or MEAN for encoders, LAST_TOKEN for decoders. "
+                             "Different post-processing is applied depending on the padding side. Applicable only for text embeddings")
     parser.add_argument("--embedding_normalize", action="store_true", help="Normalize embeddings. Applicable only for text embeddings")
     parser.add_argument("--embedding_max_length", type=int, default=None,
                         help="Max length for text embeddings. Input text will be padded or truncated to specified value")
+    parser.add_argument("--embedding_padding_side", choices=["left", "right"], default=None,
+                        help="Side to use for padding 'left' or 'right'. Applicable only for text embeddings")
     parser.add_argument("--reranking_max_length", type=int, default=None,
                         help="Max length for text reranking. Input text will be padded or truncated to specified value")
-    parser.add_argument("--reranking_top_n", type=int, default=None,
+    parser.add_argument("--reranking_top_n", type=int, default=3,
                         help="Number of top results to return for text reranking")
     parser.add_argument("--texts", nargs='+', default=None,
                         help="List of candidates for reranking based on their relevance to a prompt(query). Applicable for Text Rerank pipeline.")
     parser.add_argument('--texts_file', nargs='+', default=None,
                         help='Texts file(s) in jsonl format with candidates for reranking based on relevance to a prompt(query). '
                         'Multiple files should be separated with space(s). Applicable for Text Rerank pipeline.')
-    parser.add_argument('--rerank', action='store_true', help='Benchmark reranking pipeline.')
     parser.add_argument("--apply_chat_template", action="store_true",
                         help="Apply chat template for LLM. By default chat template is not applied. It's better to use with --disable_prompt_permutation,"
                              " otherwise the prompt will be modified after applying the chat template, so the structure of chat template will not be kept.")
@@ -222,10 +227,10 @@ CASE_TO_BENCH = {
     'image_gen': bench_image.run_image_generation_benchmark,
     'code_gen': bench_text.run_text_generation_benchmark,
     'ldm_super_resolution': bench_ldm_sr.run_ldm_super_resolution_benchmark,
-    'speech2text': bench_speech.run_speech_2_txt_benchmark,
-    "vlm": bench_vlm.run_visual_language_generation_benchmark,
+    'speech_to_text': bench_speech.run_speech_2_txt_benchmark,
+    "visual_text_gen": bench_vlm.run_visual_language_generation_benchmark,
     "text_embed": bench_text_embed.run_text_embddings_benchmark,
-    "text2speech": bench_text_to_speech.run_text_2_speech_benchmark,
+    "text_to_speech": bench_text_to_speech.run_text_2_speech_benchmark,
     "text_rerank": bench_text_rerank.run_text_reranker_benchmark
 }
 
@@ -294,12 +299,12 @@ def main():
             memory_monitor.set_dir(args.memory_consumption_dir)
         memory_data_collector = MemoryDataSummarizer(memory_monitor)
     try:
-        if model_args['use_case'] in ['text_gen', 'code_gen']:
-            iter_data_list, pretrain_time, iter_timestamp = CASE_TO_BENCH[model_args['use_case']](
+        if model_args['use_case'].task in ['text_gen', 'code_gen']:
+            iter_data_list, pretrain_time, iter_timestamp = CASE_TO_BENCH[model_args['use_case'].task](
                 model_path, framework, args.device, args.tokens_len, args.streaming, model_args,
                 args.num_iters, memory_data_collector)
         else:
-            iter_data_list, pretrain_time, iter_timestamp = CASE_TO_BENCH[model_args['use_case']](
+            iter_data_list, pretrain_time, iter_timestamp = CASE_TO_BENCH[model_args['use_case'].task](
                 model_path, framework, args.device, model_args, args.num_iters, memory_data_collector)
         if args.report is not None or args.report_json is not None:
             model_precision = ''
@@ -308,7 +313,7 @@ def main():
                 if ir_conversion_frontend != '':
                     framework = framework + '(' + ir_conversion_frontend + ')'
                 model_precision = llm_bench_utils.model_utils.get_model_precision(model_path.parts)
-            case, model_name = llm_bench_utils.model_utils.get_model_name(args.model)
+            case, model_name = llm_bench_utils.model_utils.get_model_name(args.model, args.task)
             if model_name is None:
                 model_name = llm_bench_utils.model_utils.get_model_name_with_path_part(args.model)
             if args.report is not None:

commit 431e86c97b70ffebe47c2885618b2cd5899a9f7c

Fix output wrong model name for CSV and json (#2821)
Test model qwen2-7b, the model name in CSV and JSON file is qwen not
qwen2-7b.
The issue was caused by
https://github.com/openvinotoolkit/openvino.genai/pull/2782

Refactoring get_use_case and move get_use_case to get_use_case.py

tools/llm_bench/benchmark.py
@@ -5,7 +5,6 @@ import os
 import sys
 import argparse
 import logging as log
-import llm_bench_utils.model_utils
 from openvino import get_version
 import torch
 import traceback
@@ -20,6 +19,11 @@ import task.speech_to_text_generation as bench_speech
 import task.text_embeddings as bench_text_embed
 import task.text_to_speech_generation as bench_text_to_speech
 import task.text_reranker as bench_text_rerank
+from llm_bench_utils.model_utils import (
+    analyze_args,
+    get_ir_conversion_frontend,
+    get_model_precision
+)
 
 DEFAULT_TORCH_THREAD_NUMS = 16
 
@@ -251,8 +255,8 @@ def main():
     if args.streaming and args.tokens_len is None:
         log.error("--streaming requires --tokens_len to be set.")
         exit(1)
-    model_path, framework, model_args, model_name_or_id = (
-        llm_bench_utils.model_utils.analyze_args(args)
+    model_path, framework, model_args = (
+        analyze_args(args)
     )
     # Set the device for running OpenVINO backend for torch.compile()
     if model_args['torch_compile_backend']:
@@ -309,17 +313,14 @@ def main():
         if args.report is not None or args.report_json is not None:
             model_precision = ''
             if framework == 'ov':
-                ir_conversion_frontend = llm_bench_utils.model_utils.get_ir_conversion_frontend(model_name_or_id, model_path.parts)
+                ir_conversion_frontend = get_ir_conversion_frontend(model_args['model_name'], model_path.parts)
                 if ir_conversion_frontend != '':
                     framework = framework + '(' + ir_conversion_frontend + ')'
-                model_precision = llm_bench_utils.model_utils.get_model_precision(model_path.parts)
-            case, model_name = llm_bench_utils.model_utils.get_model_name(args.model, args.task)
-            if model_name is None:
-                model_name = llm_bench_utils.model_utils.get_model_name_with_path_part(args.model)
+                model_precision = get_model_precision(model_path.parts)
             if args.report is not None:
                 llm_bench_utils.output_csv.write_result(
                     args.report,
-                    model_name,
+                    model_args['model_name'],
                     framework,
                     args.device,
                     model_args,
@@ -332,7 +333,7 @@ def main():
             if args.report_json is not None:
                 llm_bench_utils.output_json.write_result(
                     args.report_json,
-                    model_name,
+                    model_args['model_name'],
                     framework,
                     args.device,
                     model_args,

tools/llm_bench/llm_bench_utils/config_class.py
@@ -29,27 +29,6 @@ from llm_bench_utils.ov_model_classes import OVMPTModel, OVLDMSuperResolutionPip
 from dataclasses import dataclass, field
 
 
-def normalize_model_ids(model_ids_list):
-    return [m_id[:-1] if m_id.endswith('_') else m_id for m_id in model_ids_list]
-
-
-def get_use_case_by_model_id(model_type, task=None):
-    possible_use_cases = sum(list(USE_CASES.values()), [])
-    if task:
-        if task in list(UseCaseImageGen.TASK.keys()):
-            possible_use_cases = USE_CASES["image_gen"]
-        else:
-            possible_use_cases = USE_CASES[task]
-    for use_case in possible_use_cases:
-        for m_type in normalize_model_ids(use_case.model_types):
-            # TODO go to equality and raise error if use_cases is already found, as it will mean that
-            # model with that task can be applicable to execute with different pipelines and user doesn't specify one
-            if model_type.startswith(m_type):
-                return use_case, m_type
-
-    return None, None
-
-
 @dataclass
 class UseCase:
     task = ''

tools/llm_bench/llm_bench_utils/get_use_case.py
@@ -1,0 +1,178 @@
+import os
+import json
+import logging as log
+from pathlib import Path
+from typing import Optional, Tuple
+import llm_bench_utils.model_utils as model_utils
+from llm_bench_utils.config_class import (
+    USE_CASES,
+    UseCaseImageGen
+)
+
+KNOWN_FRAMEWORKS = ['pytorch', 'ov', 'dldt']
+
+OTHER_IGNORE_MODEL_PATH_PARTS = ['compressed_weights']
+
+IGNORE_MODEL_PATH_PARTS_SET = {
+    x.lower() for x in (KNOWN_FRAMEWORKS + model_utils.KNOWN_PRECISIONS + OTHER_IGNORE_MODEL_PATH_PARTS)
+}
+
+# --- 1. Constants for maintainability ---
+# Use a set for efficient 'in' checks.
+DIFFUSERS_PIPELINE_TYPES = {
+    "StableDiffusionPipeline",
+    "StableDiffusionXLPipeline",
+    "StableDiffusion3Pipeline",
+    "StableDiffusionInpaintPipeline",
+    "StableDiffusionXLInpaintPipeline",
+    "FluxPipeline",
+    "LatentConsistencyModelPipeline",
+}
+
+
+# --- 2. Helper function to reduce repetition (DRY Principle) ---
+def log_and_return(case, model_type: str, model_name: str):
+    """A helper to standardize success logging and returning."""
+    log.info(f'==SUCCESS FOUND==: use_case: {case.task}, model_type: {model_type}, model_Name: {model_name}')
+    return case, model_type, model_name
+
+
+def safe_json_load(file_path: Path) -> Optional[dict]:
+    """Safely loads a JSON file, returning None on failure."""
+    try:
+        if file_path.is_file():
+            return json.loads(file_path.read_text(encoding="utf-8"))
+    except (json.JSONDecodeError, OSError) as e:
+        log.warning(f"Could not read or parse JSON file at {file_path}: {e}")
+    return None
+
+
+def resolve_complex_model_types(config):
+    model_type = config.get("model_type").lower().replace('_', '-')
+    if model_type == "gemma3":
+        return "visual_text_gen", model_type
+    if model_type == "gemma3-text":
+        return "text_gen", model_type
+    if model_type in ["phi4mm", "phi4-multimodal"]:
+        return "visual_text_gen", model_type
+    if model_type == "llama4":
+        return "visual_text_gen", model_type
+    return None, None
+
+
+def get_model_name(model_path: Path, task: Optional[str] = None) -> Tuple[Optional[object], Optional[str], Optional[str]]:
+    """
+    Attempts to extract the model name and its use case/type from the given path.
+    Falls back to extracting a name from the path if no match is found.
+    """
+    path = os.path.abspath(model_path)
+    model_parts = path.split(os.sep)
+
+    # Determine possible use cases based on task
+    possible_use_cases = sum(USE_CASES.values(), [])
+    if task:
+        if task in UseCaseImageGen.TASK:
+            possible_use_cases = USE_CASES.get("image_gen", [])
+        else:
+            possible_use_cases = USE_CASES.get(task, [])
+
+    # Search for matching model type in path parts
+    for part in reversed(model_parts):
+        for use_case in possible_use_cases:
+            for model_type in use_case.model_types:
+                if part.lower().startswith(model_type):
+                    return use_case, model_type, part
+
+    # Fallback to extracting model name from path
+    model_name = get_model_name_with_path_part(model_path)
+    return None, None, model_name
+
+
+def get_model_name_with_path_part(model_name_or_path: Path) -> Optional[str]:
+    """
+    Extracts a model name from the path, ignoring known framework/precision keywords.
+    """
+    model_path = Path(model_name_or_path)
+    for part in reversed(model_path.parts):
+        if part.lower() not in IGNORE_MODEL_PATH_PARTS_SET:
+            return part
+    return None
+
+
+def normalize_model_ids(model_ids_list):
+    return [m_id[:-1] if m_id.endswith('_') else m_id for m_id in model_ids_list]
+
+
+def get_use_case_by_model_id(model_id, task=None):
+    possible_use_cases = sum(list(USE_CASES.values()), [])
+    if task:
+        if task in list(UseCaseImageGen.TASK.keys()):
+            possible_use_cases = USE_CASES["image_gen"]
+        else:
+            possible_use_cases = USE_CASES[task]
+    for use_case in possible_use_cases:
+        for m_type in normalize_model_ids(use_case.model_types):
+            # TODO go to equality and raise error if use_cases is already found, as it will mean that
+            # model with that task can be applicable to execute with different pipelines and user doesn't specify one
+            if model_id.startswith(m_type):
+                return use_case, m_type
+
+    return None, None
+
+
+def get_use_case(model_path: Path, task: Optional[str] = None):
+    """
+    Determines the use case, model type, and name for a given model.
+
+    It tries several strategies in order:
+    1. Diffusers: Checks for a 'model_index.json'.
+    2. Transformers: Checks for a 'config.json'.
+    3. GGUF: Inspects metadata if the file has a '.gguf' extension.
+    4. Fallback: Uses an initial guess based on the model name.
+    """
+    # --- 3. Normalize input to a Path object once ---
+    cur_case, cur_model_type, cur_model_name = get_model_name(model_path)
+
+    # Strategy 1: Check for a Diffusers model via 'model_index.json'
+    if (diffusers_config := safe_json_load(model_path / "model_index.json")):
+        if (pipe_type := diffusers_config.get("_class_name")) in DIFFUSERS_PIPELINE_TYPES:
+            model_type = pipe_type.replace("Pipeline", "")
+            return log_and_return(USE_CASES["image_gen"][0], model_type, cur_model_name)
+
+    # Strategy 2 & 3: Determine a 'model_id' from config or GGUF metadata
+    model_id = None
+    if (config := safe_json_load(model_path / "config.json")):
+        # First, attempt resolution with more complex logic
+        case, model_type = resolve_complex_model_types(config)
+        if case and model_type:
+            return log_and_return(case, model_type, cur_model_name)
+        # Fallback to simple 'model_type' key
+        if model_type_val := config.get("model_type"):
+            model_id = str(model_type_val).lower().replace('_', '-')
+
+    elif model_path.suffix == '.gguf' and model_path.is_file():
+        # --- 4. Robustness: Handle missing dependency and parsing errors ---
+        try:
+            import gguf_parser
+            parser = gguf_parser.GGUFParser(str(model_path))
+            parser.parse()
+            if arch := parser.metadata.get('general.architecture'):
+                model_id = arch.lower()
+        except ImportError:
+            log.warning("Module 'gguf_parser' not found. Cannot inspect .gguf file.")
+        except Exception as e:
+            log.error(f"Failed to parse GGUF file {model_path}: {e}")
+
+    # Use the derived model_id to find the use case
+    if model_id:
+        case, model_type = get_use_case_by_model_id(model_id, task)
+        if case:
+            # The original code used case.task, adjust if `case` is an object
+            return log_and_return(case, model_type, cur_model_name)
+
+    # Fallback Strategy: Use the initial guess if it was valid
+    if cur_case:
+        return log_and_return(cur_case, cur_model_type, cur_model_name)
+
+    # --- 5. Simplified final error ---
+    raise RuntimeError('==Failure FOUND==: no use_case found after checking all strategies.')

tools/llm_bench/llm_bench_utils/model_utils.py
@@ -8,11 +8,8 @@ import numpy as np
 import logging as log
 from pathlib import Path
 from llm_bench_utils.config_class import (
-    USE_CASES,
     PA_ATTENTION_BACKEND,
     SDPA_ATTENTION_BACKEND,
-    get_use_case_by_model_id,
-    UseCaseImageGen
 )
 import librosa
 
@@ -25,12 +22,6 @@ KNOWN_PRECISIONS = [
     'GPTQ_INT4-FP32', 'GPTQ_INT4-FP16', 'INT4',
     'OV_FP16-INT4_SYM', 'OV_FP16-INT4_ASYM', 'OV_FP32-INT4_SYM', 'OV_FP32-INT4_ASYM',
     'OV_FP32-4BIT_DEFAULT', 'OV_FP16-4BIT_DEFAULT', 'OV_FP32-4BIT_MAXIMUM', 'OV_FP16-4BIT_MAXIMUM']
-
-
-KNOWN_FRAMEWORKS = ['pytorch', 'ov', 'dldt']
-
-
-OTHER_IGNORE_MODEL_PATH_PARTS = ['compressed_weights']
 
 
 def get_param_from_file(args, input_key):
@@ -184,7 +175,8 @@ def analyze_args(args):
     use_case = None
     model_name = None
     if model_framework in ('ov', 'pt'):
-        use_case, model_name = get_use_case(args.model, args.task)
+        from llm_bench_utils.get_use_case import get_use_case
+        use_case, model_type, model_name = get_use_case(Path(args.model), args.task)
     model_args['use_case'] = use_case
     if use_case.task == 'code_gen' and not model_args['prompt'] and not model_args['prompt_file']:
         model_args['prompt'] = 'def print_hello_world():'
@@ -228,90 +220,7 @@ def analyze_args(args):
     model_args['vocoder_path'] = args.vocoder_path
     if model_args['vocoder_path'] and not Path(model_args['vocoder_path']).exists():
         raise RuntimeError(f'==Failure FOUND==: Incorrect vocoder path:{model_args["vocoder_path"]}')
-
-    return model_path, model_framework, model_args, model_name
-
-
-def get_use_case(model_name_or_path: str | Path, task: str | None = None):
-    if (Path(model_name_or_path) / "model_index.json").exists():
-        diffusers_config = json.loads((Path(model_name_or_path) / "model_index.json").read_text())
-        pipe_type = diffusers_config.get("_class_name")
-        if pipe_type in ["StableDiffusionPipeline", "StableDiffusionXLPipeline", "StableDiffusion3Pipeline", "StableDiffusionInpaintPipeline",
-                         "StableDiffusionXLInpaintPipeline", "FluxPipeline", "LatentConsistencyModelPipeline"]:
-            return USE_CASES["image_gen"][0], pipe_type.replace("Pipeline", "")
-
-    model_id = None
-    config_file = Path(model_name_or_path) / "config.json"
-    if config_file.exists():
-        config = json.loads(config_file.read_text())
-        if config is not None:
-            case, model_name = resolve_complex_model_types(config)
-            if case is not None:
-                log.info(f'==SUCCESS FOUND==: use_case: {case}, model_type: {model_name}')
-                return case, model_name
-            model_id = config.get("model_type").lower().replace('_', '-')
-    elif Path(model_name_or_path).suffix in '.gguf':
-        import gguf_parser
-        parser = gguf_parser.GGUFParser(model_name_or_path)
-        parser.parse()
-        if parser.metadata and parser.metadata.get('general.architecture'):
-            model_id = parser.metadata.get('general.architecture').lower()
-
-    if model_id is not None:
-        case, model_id = get_use_case_by_model_id(model_id, task)
-        if case:
-            log.info(f'==SUCCESS FOUND==: use_case: {case.task}, model_name: {model_id}')
-            return case, model_id
-
-    case, model_name = get_model_name(model_name_or_path)
-    if case is None:
-        raise RuntimeError('==Failure FOUND==: no use_case found')
-    else:
-        log.info(f'==SUCCESS FOUND==: use_case: {case}, model_Name: {model_name}')
-    return case, model_name
-
-
-def resolve_complex_model_types(config):
-    model_type = config.get("model_type").lower().replace('_', '-')
-    if model_type == "gemma3":
-        return "visual_text_gen", model_type
-    if model_type == "gemma3-text":
-        return "text_gen", model_type
-    if model_type in ["phi4mm", "phi4-multimodal"]:
-        return "visual_text_gen", model_type
-    if model_type == "llama4":
-        return "visual_text_gen", model_type
-    return None, None
-
-
-def get_model_name(model_name_or_path, task=None):
-    # try to get use_case from model name
-    path = os.path.abspath(model_name_or_path)
-    model_names = path.split(os.sep)
-    possible_use_cases = sum(list(USE_CASES.values()), [])
-    if task:
-        if task in list(UseCaseImageGen.TASK.keys()):
-            possible_use_cases = USE_CASES["image_gen"]
-        else:
-            possible_use_cases = USE_CASES[task]
-    for model_name in reversed(model_names):
-        for use_case in possible_use_cases:
-            for m_type in use_case.model_types:
-                if model_name.lower().startswith(m_type):
-                    return use_case, m_type
-
-    return None, None
-
-
-def get_model_name_with_path_part(model_name_or_path):
-    IGNORE_MODEL_PATH_PARTS = [x.lower() for x in (KNOWN_FRAMEWORKS + KNOWN_PRECISIONS + OTHER_IGNORE_MODEL_PATH_PARTS)]
-    model_path = Path(model_name_or_path)
-    model_name = None
-    for path_part in reversed(model_path.parts):
-        if not path_part.lower() in IGNORE_MODEL_PATH_PARTS:
-            model_name = path_part
-            break
-    return model_name
+    return model_path, model_framework, model_args
 
 
 def get_config(config):