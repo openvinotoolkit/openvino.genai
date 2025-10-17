e17bee3a4eb2587ea52450ab7fc22f3367bd1e46
likholat <anna.likholat@intel.com>
2025-10-17 21:51:05 +0200

    bad transformer output

---
+++ src/cpp/include/openvino/genai/video_generation/autoencoder_kl_ltx_video.hpp
@@ -0,0 +1,74 @@
+// Copyright (C) 2023-2025 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
+
+#pragma once
+
+#include <filesystem>
+#include <vector>
+#include <string>
+
+#include "openvino/core/any.hpp"
+#include "openvino/runtime/tensor.hpp"
+#include "openvino/runtime/infer_request.hpp"
+#include "openvino/runtime/properties.hpp"
+
+#include "openvino/genai/visibility.hpp"
+#include "openvino/genai/image_generation/generation_config.hpp"
+
+namespace ov {
+namespace genai {
+
+class OPENVINO_GENAI_EXPORTS AutoencoderKLLTXVideo {
+public:
+    struct OPENVINO_GENAI_EXPORTS Config {
+        size_t in_channels = 3;
+        size_t latent_channels = 4;
+        size_t out_channels = 3;
+        float shift_factor = 0.0f;
+        float scaling_factor = 1.0f;
+        std::vector<size_t> block_out_channels = { 64 };
+        
+        size_t patch_size = 4;  // TODO: read from vae_decoder/config.json
+        std::vector<bool> spatio_temporal_scaling{true, true, true, false};  // TODO: read from vae_decoder/config.json. I use it only to compute sum over it so far, so it may be removed
+        size_t patch_size_t = 1;  // TODO: read from vae_decoder/config.json
+
+        // latents_mean = torch.zeros((latent_channels,), requires_grad=False)
+        // latents_std = torch.ones((latent_channels,), requires_grad=False)
+        std::vector<float> latents_mean_data; // TODO: set default value
+        std::vector<float> latents_std_data;  // TODO: set default value
+
+        explicit Config(const std::filesystem::path& config_path);
+    };
+
+    explicit AutoencoderKLLTXVideo(const std::filesystem::path& vae_decoder_path);
+
+    AutoencoderKLLTXVideo(const std::filesystem::path& vae_encoder_path,
+                  const std::filesystem::path& vae_decoder_path);
+
+    AutoencoderKLLTXVideo(const std::filesystem::path& vae_decoder_path,
+                  const std::string& device,
+                  const ov::AnyMap& properties = {});
+
+    AutoencoderKLLTXVideo(const std::filesystem::path& vae_encoder_path,
+                  const std::filesystem::path& vae_decoder_path,
+                  const std::string& device,
+                  const ov::AnyMap& properties = {});
+
+    AutoencoderKLLTXVideo& compile(const std::string& device, const ov::AnyMap& properties = {});
+
+    ov::Tensor decode(ov::Tensor latent);
+
+    const Config& get_config() const;
+
+    size_t get_vae_scale_factor() const;
+
+private:
+    void merge_vae_image_post_processing() const;
+
+    Config m_config;
+    ov::InferRequest m_encoder_request, m_decoder_request;
+    std::shared_ptr<ov::Model> m_encoder_model = nullptr, m_decoder_model = nullptr;
+};
+
+} // namespace genai
+} // namespace ov
--- src/cpp/include/openvino/genai/video_generation/text2video_pipeline.hpp
+++ src/cpp/include/openvino/genai/video_generation/text2video_pipeline.hpp
@@ -7,6 +7,7 @@
 #include "openvino/genai/image_generation/image_generation_perf_metrics.hpp"
 #include "openvino/genai/image_generation/scheduler.hpp"
 #include "openvino/genai/image_generation/t5_encoder_model.hpp"
+#include "openvino/genai/video_generation/autoencoder_kl_ltx_video.hpp"
 #include "utils.hpp"
 
 namespace ov::genai {
@@ -16,79 +17,6 @@ struct VideoGenerationConfig : public ImageGenerationConfig {
     double guidance_rescale = 0.0;
     size_t num_frames = 161;
     size_t frame_rate = 25;
-};
-
-// struct LTXVideoTransformer3DModel {
-//     struct OPENVINO_GENAI_EXPORTS Config {  // TODO: video fields instead
-//         size_t in_channels = 128;  // Comes from transformer/config.json  // TODO: Could I just use model shape?
-//         bool guidance_embeds = false;
-//         size_t m_default_sample_size = 128;
-//         size_t patch_size = 1;  // TODO: read from transformer/config.json
-//         size_t patch_size_t = 1;  // TODO: read from transformer/config.json
-//     };
-//     ov::InferRequest m_ireq;
-//     LTXVideoTransformer3DModel(const std::filesystem::path& dir, const std::string& device, const ov::AnyMap& properties)
-//     : m_ireq{utils::singleton_core().compile_model(dir / "openvino_model.xml", device, properties).create_infer_request()} {}
-// inputs[
-// <ConstOutput: names[hidden_states] shape[?,?,128] type: f32>,
-// <ConstOutput: names[encoder_hidden_states] shape[?,?,4096] type: f32>,
-// <ConstOutput: names[timestep] shape[?] type: f32>,
-// <ConstOutput: names[encoder_attention_mask] shape[?,?] type: i64>,
-// <ConstOutput: names[num_frames] shape[] type: i64>,
-// <ConstOutput: names[height] shape[] type: i64>,
-// <ConstOutput: names[width] shape[] type: i64>,
-// <ConstOutput: names[rope_interpolation_scale] shape[3] type: f32>
-// ]
-// outputs[
-// <ConstOutput: names[out_sample] shape[?,?,128] type: f32>
-// ]>
-
-// Flux:
-// inputs[
-// <ConstOutput: names[hidden_states] shape[?,?,64] type: f32>,
-// <ConstOutput: names[encoder_hidden_states] shape[?,?,4096] type: f32>,
-// <ConstOutput: names[pooled_projections] shape[?,768] type: f32>,
-// <ConstOutput: names[timestep] shape[?] type: f32>,
-// <ConstOutput: names[img_ids] shape[?,3] type: f32>,
-// <ConstOutput: names[txt_ids] shape[?,3] type: f32>,
-// <ConstOutput: names[guidance] shape[?] type: f32>
-// ]
-// outputs[
-// <ConstOutput: names[out_sample] shape[?,?,64] type: f32>
-// ]>
-//     Config get_config() const {return Config{};}
-//     void set_hidden_states(const std::string& tensor_name, const ov::Tensor& encoder_hidden_states) {
-//         OPENVINO_ASSERT(m_ireq, "Transformer model must be compiled first");
-//         m_ireq.set_tensor(tensor_name, encoder_hidden_states);
-//     }
-//     ov::Tensor infer(const ov::Tensor& latent_model_input, const ov::Tensor& timestep) {
-//         OPENVINO_ASSERT(m_ireq, "Transformer model must be compiled first. Cannot infer non-compiled model");
-
-//         m_ireq.set_tensor("hidden_states", latent_model_input);
-//         m_ireq.set_tensor("timestep", timestep);
-//         m_ireq.infer();
-
-//         return m_ireq.get_output_tensor();
-//     }
-// };
-struct AutoencoderKLLTXVideo {
-    struct OPENVINO_GENAI_EXPORTS Config {
-        size_t in_channels = 3;
-        size_t latent_channels = 4;
-        size_t out_channels = 3;
-        float scaling_factor = 1.0f;
-        float shift_factor = 0.0f;
-        std::vector<size_t> block_out_channels = { 64 };
-        size_t patch_size = 4;  // TODO: read from vae_decoder/config.json
-        std::vector<bool> spatio_temporal_scaling{true, true, true, false};  // TODO: read from vae_decoder/config.json. I use it only to compute sum over it so far, so it may be removed
-        size_t patch_size_t = 1;  // TODO: read from vae_decoder/config.json
-    };
-    Config m_config;
-    AutoencoderKLLTXVideo(const std::filesystem::path& dir, const std::string& device, const ov::AnyMap& properties) {}
-    size_t get_vae_scale_factor() const {  // TODO: compare with reference. Drop?
-        return std::pow(2, m_config.block_out_channels.size() - 1);
-    }
-    const Config& get_config() const {return m_config;}  // TODO: are getters needed?
 };
 
 class OPENVINO_GENAI_EXPORTS Text2VideoPipeline {
