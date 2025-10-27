7bbfa4fbc8998457c7d2efcbf59b10ad4166a368
Vladimir Zlobin <vladimir.zlobin@intel.com>
2025-10-27 14:07:36 +0400

    Download stb_image_write.h

--- samples/cpp/video_generation/text2video.cpp
+++ samples/cpp/video_generation/text2video.cpp
@@ -56,9 +56,8 @@ int main(int32_t argc, char* argv[]) {
     config.num_frames = 9;
     pipe.set_generation_config(config);
     ov::Tensor video = pipe.generate(
-        "Will Smith eating spaghetti",
+        prompt,
         "worst quality, inconsistent motion, blurry, jittery, distorted",
-        // "",
         ov::genai::height(512),  // OVLTXPipeline's default
         ov::genai::width(704),  // OVLTXPipeline's default
         ov::genai::num_inference_steps(10),
