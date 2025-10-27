5e81041208206f0b7789c58ee91062c2015b6591
GuoliangShiIntel <guoliang.shi@intel.com>
2025-10-28 02:27:36 +0800

    eagle3 cb impl with top-1 proposal PR#2740

--- src/cpp/src/speculative_decoding/continuous_batching_for_speculative_decoding_impl.hpp
+++ src/cpp/src/speculative_decoding/continuous_batching_for_speculative_decoding_impl.hpp
@@ -40,5 +40,56 @@ public:
 protected:
     void finish_request(SequenceGroup::Ptr request);
     void _pull_awaiting_requests() override {};
+    bool eagle_mode_enabled = false;
+};
+
+class ContinuousBatchingPipeline::ContinuousBatchingForEagle3DecodingImpl
+    : public ContinuousBatchingPipeline::ContinuousBatchingForSpeculativeDecodingImpl {
+public:
+    ContinuousBatchingForEagle3DecodingImpl() = default;
+
+    ContinuousBatchingForEagle3DecodingImpl(const std::shared_ptr<ov::Model>& model,
+                                                 const Tokenizer& tokenizer,
+                                                 const GenerationConfig& generation_config,
+                                                 const SchedulerConfig& scheduler_config,
+                                                 const std::string& device,
+                                                 const ov::AnyMap& plugin_config,
+                                                 bool is_validation_mode_enabled) : ContinuousBatchingForSpeculativeDecodingImpl(
+                                                     model, tokenizer, generation_config,
+                                                     scheduler_config, device, plugin_config,
+                                                     is_validation_mode_enabled) {
+                                                        eagle_mode_enabled = true;
+                                                     };
+
+    bool is_requests_empty();
+
+    void set_d2t_for_draft_decoding(std::shared_ptr<ov::op::v0::Constant>& d2t) {
+        if (m_sampler) {
+            m_sampler->set_d2t_for_decoding(d2t);
+        }
+    }
+    void set_hidden_state_export_needed(bool is_needed) {
+        if (m_model_runner) {
+            m_model_runner->enable_hidden_state_export(is_needed);
+        }
+    }
+
+    void set_hidden_state_import_needed(bool is_needed) {
+        if (m_model_runner) {
+            m_model_runner->enable_hidden_state_import(is_needed);
+        }
+    }
+
+    void set_hidden_state_internal_needed(bool is_needed) {
+        if (m_model_runner) {
+            m_model_runner->enable_hidden_state_internal(is_needed);
+        }
+    }
+
+    void set_adjust_factor(size_t adjust_factor) {
+        if (m_model_runner) {
+            m_model_runner->set_adjust_factor(adjust_factor);
+        }
+    }
 };
 }
