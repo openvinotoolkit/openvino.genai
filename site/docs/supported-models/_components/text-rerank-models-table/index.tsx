1ce44958af21a998e71d6859e380f7c659c61595
Alexander Suvorov <alexander.suvorov@intel.com>
2025-10-29 17:21:14 +0100

    Add text rerank models task

--- site/docs/supported-models/_components/text-rerank-models-table/index.tsx
+++ site/docs/supported-models/_components/text-rerank-models-table/index.tsx
@@ -3,13 +3,16 @@ import { TEXT_RERANK_MODELS } from './models';
 import { BaseModelsTable, LinksCell } from '../base-models-table';
 
 export default function TextRerankModelsTable(): React.JSX.Element {
-  const headers = ['Architecture', 'Example HuggingFace Models'];
+  const headers = ['Architecture', 'optimum-cli task', 'Example HuggingFace Models'];
 
-  const rows = TEXT_RERANK_MODELS.map(({ architecture, models }) => (
+  const rows = TEXT_RERANK_MODELS.map(({ architecture, optimumIntelTask, models }) => (
     <>
       <tr key={architecture}>
         <td rowSpan={models.length}>
           <code>{architecture}</code>
+        </td>
+        <td rowSpan={models.length}>
+          <code>{optimumIntelTask}</code>
         </td>
         <LinksCell links={models[0].links} />
       </tr>
