387aecc2f9146635268b5b48dc330ab9c97eeadf
Chen, Peter <peter.chen@intel.com>
2025-10-29 19:51:28 +0800

    Revert "[llm_bench] Don't print the Input Text if output to dir"
    This reverts commit eef4366b3c634eb8dd7e3220327ea10c0ab68392.

--- tools/llm_bench/task/text_to_speech_generation.py
+++ tools/llm_bench/task/text_to_speech_generation.py
@@ -218,8 +218,7 @@ def run_text_2_speech_benchmark(model_path, framework, device, args, num_iters, 
         for num in range(num_iters + 1):
             for idx, input_text in enumerate(text_list):
                 p_idx = prompt_idx_list[idx]
-                if args["output_dir"] is None and num == 0:
-                    # args["output_dir"] is not None, then the Input text is saved to output_dir
+                if num == 0:
                     metrics_print.print_unicode(f'[warm-up][P{p_idx}] Input text: {input_text}', f'[warm-up][P{p_idx}] Unable print input text')
                 iter_timestamp[num][p_idx]['start'] = datetime.datetime.now().isoformat()
                 gen_fn(input_text, num, model, processor, vocoder, args, iter_data_list, md5_list,
@@ -231,8 +230,7 @@ def run_text_2_speech_benchmark(model_path, framework, device, args, num_iters, 
         for idx, input_text in enumerate(text_list):
             p_idx = prompt_idx_list[idx]
             for num in range(num_iters + 1):
-                if args["output_dir"] is None and num == 0:
-                    # args["output_dir"] is not None, then the Input text is saved to output_dir
+                if num == 0:
                     metrics_print.print_unicode(f'[warm-up][P{p_idx}] Input text: {input_text}', f'[warm-up][P{p_idx}] Unable print input text')
                 iter_timestamp[num][p_idx]['start'] = datetime.datetime.now().isoformat()
                 gen_fn(input_text, num, model, processor, vocoder, args, iter_data_list, md5_list,
