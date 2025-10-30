0a5f271b0c58fb914f89a3320a8e27fa24f51d59
Vladimir Zlobin <vladimir.zlobin@intel.com>
2025-10-30 15:17:04 +0400

    Add validate() and update_generation_config() for VideoGenerationConfig

--- src/cpp/include/openvino/genai/video_generation/text2video_pipeline.hpp
+++ src/cpp/include/openvino/genai/video_generation/text2video_pipeline.hpp
@@ -13,10 +13,44 @@
 namespace ov::genai {
 using VideoGenerationPerfMetrics = ImageGenerationPerfMetrics;
 
+/**
+ * Generation config used for Text2VideoPipeline.
+ * Note, that not all values are applicable for all pipelines and models - please, refer
+ * to corresponding properties for ImageGenerationConfig to understand a meaning and applicability for specific models.
+ */
 struct VideoGenerationConfig : public ImageGenerationConfig {
+    /// guidance_rescale (`float`, *optional*, defaults to 0.0):
+    /// Guidance rescale factor proposed by [Common Diffusion Noise Schedules and Sample Steps are
+    /// Flawed](https://arxiv.org/pdf/2305.08891.pdf) `guidance_scale` is defined as `φ` in equation 16. of
+    /// [Common Diffusion Noise Schedules and Sample Steps are Flawed](https://arxiv.org/pdf/2305.08891.pdf).
+    /// Guidance rescale factor should fix overexposure when using zero terminal SNR.
+    /// Mixes with the original results from guidance by factor guidance_rescale to avoid "plain looking" images.
+    /// 0.0 disables rescaling.
     double guidance_rescale = 0.0;
+    /// The number of video frames to generate.
     size_t num_frames = 161;
-    size_t frame_rate = 25;
+    /// Video frame rate.
+    float frame_rate = 25.0f;
+
+    /**
+     * Checks whether video generation config is valid, otherwise throws an exception.
+     */
+    void validate() const;
+
+    /**
+     * Updates generation config from a map of properties.
+     * @param properties A map of properties
+     */
+    void update_generation_config(const ov::AnyMap& properties);
+
+    /**
+     * Updates generation config from properties. Calls AnyMap version of update_generation_config().
+     * @param properties
+     */
+    template <typename... Properties>
+    ov::util::EnableIfAllStringAny<void, Properties...> update_generation_config(Properties&&... properties) {
+        return update_generation_config(ov::AnyMap{std::forward<Properties>(properties)...});
+    }
 };
 
 class OPENVINO_GENAI_EXPORTS Text2VideoPipeline {
@@ -58,4 +92,17 @@ private:
     class LTXPipeline;
     std::unique_ptr<LTXPipeline> m_impl;
 };
+
+/// guidance_rescale (`float`, *optional*, defaults to 0.0):
+/// Guidance rescale factor proposed by [Common Diffusion Noise Schedules and Sample Steps are
+/// Flawed](https://arxiv.org/pdf/2305.08891.pdf) `guidance_scale` is defined as `φ` in equation 16. of
+/// [Common Diffusion Noise Schedules and Sample Steps are Flawed](https://arxiv.org/pdf/2305.08891.pdf).
+/// Guidance rescale factor should fix overexposure when using zero terminal SNR.
+/// Mixes with the original results from guidance by factor guidance_rescale to avoid "plain looking" images.
+/// 0.0 disables rescaling.
+static constexpr ov::Property<double> guidance_rescale{"guidance_rescale"};
+/// The number of video frames to generate.
+static constexpr ov::Property<size_t> num_frames{"num_frames"};
+/// Video frame rate.
+static constexpr ov::Property<float> frame_rate{"guidance_rescale"};
 }  // namespace ov::genai
