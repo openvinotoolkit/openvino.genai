3f0109cabd1c1460ba809eb81acdc3e4205d139c
Krzysztof Czarnecki <krzysztof.czarnecki@intel.com>
2025-11-06 11:33:31 +0100

    kcz/support-for-video-in-benchmark

--- tools/llm_bench/benchmark.py
+++ tools/llm_bench/benchmark.py
@@ -229,6 +229,8 @@ def get_argprser():
                         help="Path to .bin or .pt file with speaker embeddings for text to speech scenarios")
     parser.add_argument("--vocoder_path", type=str, default=None,
                         help="Path to vocoder  for text to speech scenarios")
+    parser.add_argument("-vf", "--video_frames", type=int, default=None,
+                        help="controler of video frames to process")
     return parser.parse_args()
 
 
@@ -315,7 +317,8 @@ def main():
                 args.num_iters, memory_data_collector)
         else:
             iter_data_list, pretrain_time, iter_timestamp = CASE_TO_BENCH[model_args['use_case'].task](
-                model_path, framework, args.device, model_args, args.num_iters, memory_data_collector)
+                model_path, framework, args.device, model_args, args.num_iters,
+                memory_data_collector, args.video_frames)
         if args.report is not None or args.report_json is not None:
             model_precision = ''
             if framework == 'ov':
