689b08c9b9599ef9ce22e8ba648088ec103719ab
xipingya <xiping.yan@intel.com>
2025-11-10 15:57:25 +0800

    Get tokenizer so path.
    Signed-off-by: xipingya <xiping.yan@intel.com>

--- tests/python_tests/test_add_extension.py
+++ tests/python_tests/test_add_extension.py
@@ -4,10 +4,23 @@
 
 import pytest
 import openvino_genai as ov_genai
+import platform
+import os
+import openvino_tokenizers;
 
 @pytest.mark.precommit
 def test_add_extension():
     # Path to the OpenVINO extension shared library (update as needed).
-    extension_path = ""
-    with pytest.raises(RuntimeError):
-        ov_genai.add_extension(extension_path)
+    os_name = platform.system()
+    if os_name == "Windows":
+        ov_tokenizer_path = os.path.dirname(openvino_tokenizers.__file__) + "\\lib\\openvino_tokenizers.dll"
+    elif os_name == "Linux":
+        ov_tokenizer_path = os.path.dirname(openvino_tokenizers.__file__) + "/lib/libopenvino_tokenizers.so"
+    else:
+        print(f"Skipped. Current test only support Windows and Linux")
+        return
+
+    try:
+        ov_genai.add_extension(ov_tokenizer_path)
+    except RuntimeError as e:
+        raise RuntimeError(f"Add extension fail, maybe tokenizers' version mismatch. Original error: {e}")
