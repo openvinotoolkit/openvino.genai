527520e0992e3ffe881c02f357ec9b824fd84045
yatarkan <yaroslav.tarkan@intel.com>
2025-11-20 20:28:12 +0400

    Move encoded images check to inputs embedder

--- src/cpp/src/visual_language/inputs_embedder.cpp
+++ src/cpp/src/visual_language/inputs_embedder.cpp
@@ -172,12 +172,13 @@ std::vector<ov::Tensor> InputsEmbedder::IInputsEmbedder::to_single_image_tensors
 }
 
 std::vector<ov::genai::EncodedImage> InputsEmbedder::IInputsEmbedder::encode_images(const std::vector<ov::Tensor>& images) {
-    std::vector<EncodedImage> embeds;
+    std::vector<EncodedImage> encoded_images;
     std::vector<ov::Tensor> single_images = to_single_image_tensors(images);
     for (const ov::Tensor& image : single_images) {
-        embeds.emplace_back(m_vision_encoder->encode(image));
+        encoded_images.emplace_back(m_vision_encoder->encode(image));
     }
-    return embeds;
+    OPENVINO_ASSERT(images.size() == encoded_images.size(), "Input images size and encoded images size mismatch!");
+    return encoded_images;
 }
 
 ov::Tensor InputsEmbedder::IInputsEmbedder::get_inputs_embeds(
