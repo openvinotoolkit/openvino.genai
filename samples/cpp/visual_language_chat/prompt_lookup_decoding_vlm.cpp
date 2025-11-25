6820fb8610db8e1b4ee34d9ab756a2a70918efc2
xipingya <xiping.yan@intel.com>
2025-11-25 14:45:13 +0800

    Add prompt lookup decoding sample for VLM
    Signed-off-by: xipingya <xiping.yan@intel.com>

---
+++ samples/cpp/visual_language_chat/prompt_lookup_decoding_vlm.cpp
@@ -0,0 +1,51 @@
+// Copyright (C) 2023-2025 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
+
+#include <openvino/openvino.hpp>
+
+#include "load_image.hpp"
+#include "openvino/genai/visual_language/pipeline.hpp"
+
+int main(int argc, char* argv[]) try {
+    if (4 != argc) {
+        throw std::runtime_error(std::string{"Usage: "} + argv[0] + " <MODEL_DIR> <IMAGE_FILE OR DIR_WITH_IMAGES> <PROMPT>");
+    }
+
+    ov::genai::GenerationConfig config;
+    config.max_new_tokens = 100;
+    // Define candidates number for candidate generation
+    config.num_assistant_tokens = 5;
+    // Define max_ngram_size
+    config.max_ngram_size = 3;
+
+    std::string model_path = argv[1];
+    std::vector<ov::Tensor> rgbs = utils::load_images(argv[2]);
+    std::string prompt = argv[3];
+
+    std::string device = "CPU";
+
+    ov::genai::VLMPipeline pipe(
+        model_path,
+        device,
+        ov::genai::prompt_lookup(true));
+
+    auto streamer = [](std::string subword) {
+        std::cout << subword << std::flush;
+        return ov::genai::StreamingStatus::RUNNING;
+    };
+
+    // Since the streamer is set, the results will
+    // be printed each time a new token is generated.
+    pipe.generate(prompt, ov::genai::images(rgbs), ov::genai::generation_config(config), ov::genai::streamer(streamer));
+    std::cout << std::endl;
+} catch (const std::exception& error) {
+    try {
+        std::cerr << error.what() << '\n';
+    } catch (const std::ios_base::failure&) {}
+    return EXIT_FAILURE;
+} catch (...) {
+    try {
+        std::cerr << "Non-exception object thrown\n";
+    } catch (const std::ios_base::failure&) {}
+    return EXIT_FAILURE;
+}
