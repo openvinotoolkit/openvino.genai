5b6a9cc3e4c82d3ecb086b254c629c0d4ecf319f
Anastasiia Pnevskaia <anastasiia.pnevskaia@intel.com>
2025-11-27 11:09:11 +0100

    Removed the not needed code from apply_paged_attention_transformations().

--- src/cpp/src/continuous_batching/paged_attention_transformations.cpp
+++ src/cpp/src/continuous_batching/paged_attention_transformations.cpp
@@ -30,36 +30,6 @@ void apply_paged_attention_transformations(std::shared_ptr<ov::Model> model, boo
 
     OPENVINO_ASSERT(key_cache_params.size() == value_cache_params.size() && key_cache_params.size() > 0);
 
-    size_t num_decoder_layers = key_cache_params.size();
-    for (size_t idx = 0; idx < num_decoder_layers; idx++) {
-        auto k = key_cache_params[std::string("key_cache.") + std::to_string(idx)];
-        auto key_shape = k->get_partial_shape();
-        size_t num_k_heads = key_shape[1].get_length();
-        size_t k_head_size = key_shape[2].get_length();
-
-        auto v = value_cache_params[std::string("value_cache.") + std::to_string(idx)];
-        auto value_shape = v->get_partial_shape();
-        size_t num_v_heads = value_shape[1].get_length();
-        size_t v_head_size = value_shape[2].get_length();
-
-        // reset information in KV cache parameters and set PagedAttention's rt_info
-        // allow a plugin to automatically set KV cache precisions
-        k->set_element_type(ov::element::dynamic);
-        v->set_element_type(ov::element::dynamic);
-
-        // order of dimensions within shapes are not required for plugin during compilation
-        k->set_partial_shape(ov::PartialShape::dynamic(4));
-        v->set_partial_shape(ov::PartialShape::dynamic(4));
-
-        // set KV cache parameters as rt_info for PagedAttention op, so plugins can apply
-        // model compile-time optimizations based on them
-        auto pa_op = k->get_output_target_inputs(0).begin()->get_node();
-        pa_op->get_rt_info()["num_k_heads"] = num_k_heads;
-        pa_op->get_rt_info()["k_head_size"] = k_head_size;
-        pa_op->get_rt_info()["num_v_heads"] = num_v_heads;
-        pa_op->get_rt_info()["v_head_size"] = v_head_size;
-    }
-
     model->validate_nodes_and_infer_types();
 }
 
