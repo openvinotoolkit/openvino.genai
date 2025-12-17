e0348e39c5245622d76138a83facb8dc5ca0220d
xipingya <xiping.yan@intel.com>
2025-12-17 09:27:22 +0800

    fix py test lint error

--- samples/python/visual_language_chat/prompt_lookup_decoding_vlm.py
+++ samples/python/visual_language_chat/prompt_lookup_decoding_vlm.py
@@ -10,20 +10,21 @@ from pathlib import Path
 import openvino_genai
 from openvino import Tensor
 
+
 def streamer(subword):
-    print(subword, end='', flush=True)
+    print(subword, end="", flush=True)
     # Return flag corresponds whether generation should be stopped.
     return openvino_genai.StreamingStatus.RUNNING
 
 def read_image(path: str) -> Tensor:
-    '''
+    """
 
     Args:
         path: The path to the image.
 
     Returns: the ov.Tensor containing the image.
 
-    '''
+    """
     pic = Image.open(path).convert("RGB")
     image_data = np.array(pic)
     return Tensor(image_data)
@@ -35,24 +36,27 @@ def read_images(path: str) -> list[Tensor]:
         return [read_image(str(file)) for file in sorted(entry.iterdir())]
     return [read_image(path)]
 
+
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('model_dir')
-    parser.add_argument('image_dir', help="Image file or dir with images")
-    parser.add_argument('prompt')
-    parser.add_argument('--disable_lookup',
-                        action='store_false',
-                        dest='enable_lookup',
-                        default=True,
-                        help="Disable lookup decoding (i.e., set enable_lookup to False).")
+    parser.add_argument("model_dir")
+    parser.add_argument("image_dir", help="Image file or dir with images")
+    parser.add_argument("prompt")
+    parser.add_argument(
+        "--disable_lookup",
+        action="store_false",
+        dest="enable_lookup",
+        default=True,
+        help="Disable lookup decoding (i.e., set enable_lookup to False).",
+    )
 
     args = parser.parse_args()
 
-    device = 'CPU'
+    device = "CPU"
 
     # Currently only for ATTENTION_BACKEND="PA", PLD is enabled.
     pipe = openvino_genai.VLMPipeline(args.model_dir, device, prompt_lookup=args.enable_lookup, ATTENTION_BACKEND="PA")
-    
+
     config = openvino_genai.GenerationConfig()
     config.max_new_tokens = 100
     if args.enable_lookup:
@@ -63,10 +67,11 @@ def main():
 
     rgbs = read_images(args.image_dir)
 
-    # Since the streamer is set, the results are printed 
+    # Since the streamer is set, the results are printed
     # every time a new token is generated and put into the streamer queue.
     pipe.generate(args.prompt, images=rgbs, generation_config=config, streamer=streamer)
     print()
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
--- tests/python_tests/samples/test_prompt_lookup_decoding_vlm.py
+++ tests/python_tests/samples/test_prompt_lookup_decoding_vlm.py
@@ -8,13 +8,18 @@ import sys
 from conftest import SAMPLES_PY_DIR, SAMPLES_CPP_DIR
 from test_utils import run_sample
 
+
 class TestPromptLookupDecodingVLM:
     @pytest.mark.vlm
     @pytest.mark.samples
     @pytest.mark.parametrize(
         "convert_model, download_test_content, question",
         [
-            pytest.param("Qwen2-VL-2B-Instruct", "monalisa.jpg", "Who drew this painting? Please answer in JSON format: {\"Painter\":\"Van Gogh\"}."),
+            pytest.param(
+                "Qwen2-VL-2B-Instruct",
+                "monalisa.jpg",
+                'Who drew this painting? Please answer in JSON format: {"Painter":"Van Gogh"}.',
+            ),
         ],
         indirect=["convert_model", "download_test_content"],
     )
@@ -32,4 +37,6 @@ class TestPromptLookupDecodingVLM:
         py_result_ref = run_sample(py_command_ref, env=env)
 
         # Compare results
-        assert py_result.stdout == py_result_ref.stdout, "Results should be identical when running without lookup and lookup speculative decoding enabled."
+        assert py_result.stdout == py_result_ref.stdout, (
+            "Results should be identical when running without lookup and lookup speculative decoding enabled."
+        )
