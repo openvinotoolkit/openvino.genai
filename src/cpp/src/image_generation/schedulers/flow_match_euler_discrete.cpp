081786e8225e504831f337b2e23853f9a34a4296
likholat <anna.likholat@intel.com>
2026-01-08 20:24:20 +0100

    FlowMatchEulerDiscreteScheduler config parse fix

--- src/cpp/src/image_generation/schedulers/flow_match_euler_discrete.cpp
+++ src/cpp/src/image_generation/schedulers/flow_match_euler_discrete.cpp
@@ -54,8 +54,12 @@ FlowMatchEulerDiscreteScheduler::Config::Config(const std::filesystem::path& sch
     read_json_param(data, "max_image_seq_len", max_image_seq_len);
     nlohmann::json::iterator iter = data.find("shift_terminal");
     // TODO: verify other pipelines don't set it because they don't use it
-    if (iter != data.end()) {
-        shift_terminal = *iter;
+    if (iter == data.end() || iter->is_null()) {
+        shift_terminal = 0;
+    } else if (iter->is_number()) {
+        shift_terminal =  *iter;
+    } else {
+        OPENVINO_THROW("'shift_terminal' must be a number or null, but got: ", iter->type_name());
     }
 }
 
