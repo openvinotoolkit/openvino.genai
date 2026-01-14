c3bc5ba73a22172a620373909f49495b17bfe223
Chen Peter <peter.chen@intel.com>
2026-01-14 20:11:49 +0800

    Update description for pruning_ratio and relavance_weight

--- samples/python/visual_language_chat/benchmark_vlm.py
+++ samples/python/visual_language_chat/benchmark_vlm.py
@@ -43,7 +43,7 @@ def main():
     parser.add_argument("-mt", "--max_new_tokens", type=int, default=20, help="Maximal number of new tokens")
     parser.add_argument("-d", "--device", type=str, default="CPU", help="Device")
     parser.add_argument("--pruning_ratio", type=int, help="Percentage of visual tokens to prune (0 to disable)")
-    parser.add_argument("--relevance_weight", type=float, help="Relevance weight for the model")
+    parser.add_argument("--relevance_weight", type=float, help="Float value from 0 to 1, control the trade-off between diversity and relevance for visual tokens pruning, a value of 0 disables relevance weighting, while higher values (up to 1.0) emphasize relevance, making pruning more conservative on borderline tokens.")
 
     args = parser.parse_args()
 
