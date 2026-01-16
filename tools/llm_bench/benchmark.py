51c4be3f3ac8788cc4b6bedf1f012f4783b574df
wgzintel <guozhong.wang@intel.com>
2026-01-16 13:59:35 +0800

    format code

--- tools/llm_bench/benchmark.py
+++ tools/llm_bench/benchmark.py
@@ -127,8 +127,8 @@ def get_argprser():
     parser.add_argument(
         "--relevance_weight",
         type=float,
-        help="Float value from 0 to 1, control the trade-off between diversity and relevance for visual tokens pruning, "
-        "a value of 0 disables relevance weighting, while higher values (up to 1.0) emphasize relevance, making pruning more conservative on borderline tokens.",
+        help="Float value from 0 to 1, control the trade-off between diversity and relevance for visual tokens pruning, a value of 0 disables "
+        "relevance weighting, while higher values (up to 1.0) emphasize relevance, making pruning more conservative on borderline tokens.",
     )
     parser.add_argument(
         '--torch_compile_backend',
