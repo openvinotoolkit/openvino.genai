f10f4c721fc02818bb4bd524c007eb26d89e7cac
mweiguo <weiguo.meng@intel.com>
2026-01-22 00:49:39 +0800

    Move property description to public header

--- src/cpp/src/rag/npu/text_embedding_pipeline.hpp
+++ src/cpp/src/rag/npu/text_embedding_pipeline.hpp
@@ -9,20 +9,6 @@
 namespace ov {
 namespace genai {
 
-//
-// Enable NPU dynamic prompt input support, which allows text-embedding models to handle long-context inputs more
-// effectively.
-//
-// NPUW_LLM_PREFILL_CHUNK_SIZE:
-//     Controls the chunk size for prompt prefill, which determines the granularity of input dynamism.
-//     Default value: 1024.
-//
-// NPUW_F16IC:
-//     Forces subgraph interconnect tensors to use FP16 precision when they would otherwise be FP32, provided that the
-//     partitioning pipeline is enabled. Setting this property to False may improve accuracy.
-//     Default value: True
-//
-
 InferRequest create_text_embedding_npu_request(std::shared_ptr<ov::Model>& model,
                                                const TextEmbeddingPipeline::Config& config,
                                                const ov::AnyMap& properties,
