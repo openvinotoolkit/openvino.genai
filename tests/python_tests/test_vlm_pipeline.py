67dcdeeb40617c5a7967e944c780a4a460f9c860
xipingya <xiping.yan@intel.com>
2026-01-22 09:42:31 +0800

    update code format

--- tests/python_tests/test_vlm_pipeline.py
+++ tests/python_tests/test_vlm_pipeline.py
@@ -1926,6 +1926,7 @@ def test_cdpruner_continuous_batching(
 
     assert results[0].texts[0].strip() != "", "Result should not be empty"
 
+
 def test_vlm_prompt_lookup_functionality(cat_tensor):
     """Test prompt_lookup functionality for Qwen2VL model."""
     model_id = "optimum-intel-internal-testing/tiny-random-qwen2vl"
@@ -1933,21 +1934,15 @@ def test_vlm_prompt_lookup_functionality(cat_tensor):
 
     ov_pipe = VLMPipeline(model_path, "CPU")
     generation_config = _setup_generation_config(ov_pipe, max_new_tokens=20, do_sample=False)
-    results = ov_pipe.generate(
-        PROMPTS[0],
-        images=[cat_tensor],
-        generation_config=generation_config
-    )
+    results = ov_pipe.generate(PROMPTS[0], images=[cat_tensor], generation_config=generation_config)
 
     ov_pipe_pld = VLMPipeline(model_path, "CPU", prompt_lookup=True)
     # add parameter to enable prompt lookup decoding to generate `num_assistant_tokens` candidates per iteration
     generation_config.num_assistant_tokens = 5
     # Define max_ngram_size
     generation_config.max_ngram_size = 3
-    results_pld = ov_pipe_pld.generate(
-        PROMPTS[0],
-        images=[cat_tensor],
-        generation_config=generation_config
-    )
+    results_pld = ov_pipe_pld.generate(PROMPTS[0], images=[cat_tensor], generation_config=generation_config)
 
-    assert results.texts[0].strip() == results_pld.texts[0].strip(), "Result should be same for enable prompt_lookup and disable prompt_lookup."
+    assert results.texts[0].strip() == results_pld.texts[0].strip(), (
+        "Result should be same for enable prompt_lookup and disable prompt_lookup."
+    )
