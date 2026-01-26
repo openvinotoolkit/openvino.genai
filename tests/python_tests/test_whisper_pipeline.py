fb8b5a58a462bbb6c950e737b438085d62da0ac4
as-suvorov <alexander.suvorov@intel.com>
2026-01-26 13:57:54 +0100

    Add ticket

--- tests/python_tests/test_whisper_pipeline.py
+++ tests/python_tests/test_whisper_pipeline.py
@@ -6,6 +6,7 @@ import sys
 # win32 fails on ffmpeg DLLs load
 # import transformers.pipeline imports VideoClassificationPipeline which requires PyAV (ffmpeg bindings)
 # wa is to create mock 'av' module to prevent DLLs loading errors
+# ticket: 179943
 if sys.platform == "win32":
     from types import ModuleType
 
