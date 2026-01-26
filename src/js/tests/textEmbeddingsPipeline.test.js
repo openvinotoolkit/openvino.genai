7f6fd6ffdb2add1beb0ed1e0d641f072d95ef314
as-suvorov <alexander.suvorov@intel.com>
2026-01-26 14:25:22 +0100

    Skip js tests

--- src/js/tests/textEmbeddingsPipeline.test.js
+++ src/js/tests/textEmbeddingsPipeline.test.js
@@ -12,48 +12,52 @@ if (!EMBEDDING_MODEL_PATH)
     "Set the path to the model directory in the " + "EMBEDDING_MODEL_PATH environment variable.",
   );
 
-describe("TextEmbeddingPipeline", () => {
-  let pipeline = null;
+describe(
+  "TextEmbeddingPipeline",
+  // Ticket: 179949
+  { skip: os.platform() === "darwin" },
+  () => {
+    let pipeline = null;
 
-  before(async () => {
-    pipeline = await TextEmbeddingPipeline(EMBEDDING_MODEL_PATH, "CPU");
-  });
-
-  it("async embed query", async () => {
-    const result = pipeline.embedQuery("test");
-    assert.ok(result instanceof Promise, "result should be Promise");
-
-    const embedResult = await result;
-    assert.ok(isFloat32Array(embedResult));
-  });
-
-  it("async embed documents", async () => {
-    const result = pipeline.embedDocuments(["Hello", "World"]);
-    assert.ok(result instanceof Promise, "result should be Promise");
-
-    const embedResult = await result;
-    assert.ok(embedResult instanceof Array);
-    assert.strictEqual(embedResult.length, 2);
-    assert.ok(isFloat32Array(embedResult[0]));
-  });
-
-  it("sync embed query", () => {
-    const embedResult = pipeline.embedQuerySync("test");
-    assert.ok(isFloat32Array(embedResult));
-  });
-
-  it("sync embed documents", () => {
-    const embedResult = pipeline.embedDocumentsSync(["Hello", "World"]);
-    assert.ok(embedResult instanceof Array);
-    assert.strictEqual(embedResult.length, 2);
-    assert.ok(isFloat32Array(embedResult[0]));
-  });
-
-  it("test TextEmbeddingPipeline config param", async () => {
-    const pipelineWithConfig = await TextEmbeddingPipeline(EMBEDDING_MODEL_PATH, "CPU", {
-      pooling_type: PoolingType.MEAN,
-      normalize: false,
+    before(async () => {
+      pipeline = await TextEmbeddingPipeline(EMBEDDING_MODEL_PATH, "CPU");
     });
-    assert.ok(pipelineWithConfig instanceof Object);
+
+    it("async embed query", async () => {
+      const result = pipeline.embedQuery("test");
+      assert.ok(result instanceof Promise, "result should be Promise");
+
+      const embedResult = await result;
+      assert.ok(isFloat32Array(embedResult));
+    });
+
+    it("async embed documents", async () => {
+      const result = pipeline.embedDocuments(["Hello", "World"]);
+      assert.ok(result instanceof Promise, "result should be Promise");
+
+      const embedResult = await result;
+      assert.ok(embedResult instanceof Array);
+      assert.strictEqual(embedResult.length, 2);
+      assert.ok(isFloat32Array(embedResult[0]));
+    });
+
+    it("sync embed query", () => {
+      const embedResult = pipeline.embedQuerySync("test");
+      assert.ok(isFloat32Array(embedResult));
+    });
+
+    it("sync embed documents", () => {
+      const embedResult = pipeline.embedDocumentsSync(["Hello", "World"]);
+      assert.ok(embedResult instanceof Array);
+      assert.strictEqual(embedResult.length, 2);
+      assert.ok(isFloat32Array(embedResult[0]));
+    });
+
+    it("test TextEmbeddingPipeline config param", async () => {
+      const pipelineWithConfig = await TextEmbeddingPipeline(EMBEDDING_MODEL_PATH, "CPU", {
+        pooling_type: PoolingType.MEAN,
+        normalize: false,
+      });
+      assert.ok(pipelineWithConfig instanceof Object);
+    });
   });
-});
