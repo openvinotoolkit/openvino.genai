32d15d4fd7eb38741573eec7eb545c5a577d7601
John Feng <john.feng@intel.com>
2026-01-31 18:58:41 +0800

    fix lint

--- samples/python/image_generation/encrypted_stable_diffusion.py
+++ samples/python/image_generation/encrypted_stable_diffusion.py
@@ -11,6 +11,7 @@ from openvino import Tensor
 from pathlib import Path
 from PIL import Image
 
+
 def decrypt_model(model_dir, model_file_name, weights_file_name):
     with open(model_dir / model_file_name, "r") as file:
         model = file.read()
@@ -23,13 +24,14 @@ def decrypt_model(model_dir, model_file_name, weights_file_name):
 
     return model, Tensor(weights)
 
+
 def read_tokenizer(model_dir):
-    tokenizer_model_name = 'openvino_tokenizer.xml'
-    tokenizer_weights_name = 'openvino_tokenizer.bin'
+    tokenizer_model_name = "openvino_tokenizer.xml"
+    tokenizer_weights_name = "openvino_tokenizer.bin"
     tokenizer_model, tokenizer_weights = decrypt_model(model_dir, tokenizer_model_name, tokenizer_weights_name)
 
-    detokenizer_model_name = 'openvino_detokenizer.xml'
-    detokenizer_weights_name = 'openvino_detokenizer.bin'
+    detokenizer_model_name = "openvino_detokenizer.xml"
+    detokenizer_weights_name = "openvino_detokenizer.bin"
     detokenizer_model, detokenizer_weights = decrypt_model(model_dir, detokenizer_model_name, detokenizer_weights_name)
 
     return openvino_genai.Tokenizer(tokenizer_model, tokenizer_weights, detokenizer_model, detokenizer_weights)
@@ -38,11 +40,14 @@ def read_tokenizer(model_dir):
 # here is example how to make cache de-encryption based on base64
 import base64
 
+
 def encrypt_base64(src: bytes):
     return base64.b64encode(src)
 
+
 def decrypt_base64(src: bytes):
     return base64.b64decode(src)
+
 
 def get_config_for_cache_encryption():
     config_cache = dict()
@@ -51,12 +56,13 @@ def get_config_for_cache_encryption():
     config_cache["CACHE_MODE"] = "OPTIMIZE_SIZE"
     return config_cache
 
+
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('model_dir')
-    parser.add_argument('prompt')
+    parser.add_argument("model_dir")
+    parser.add_argument("prompt")
 
-    parser.add_argument('device', nargs='?', default='CPU')
+    parser.add_argument("device", nargs="?", default="CPU")
 
     args = parser.parse_args()
 
@@ -69,39 +75,54 @@ def main():
         # next run. It's not beneficial for CPU.
         config = get_config_for_cache_encryption()
 
-    text_encoder_model, text_encoder_weights = decrypt_model(model_dir / "text_encoder", 'openvino_model.xml', 'openvino_model.bin')
+    text_encoder_model, text_encoder_weights = decrypt_model(
+        model_dir / "text_encoder", "openvino_model.xml", "openvino_model.bin"
+    )
     text_encoder_tokenizer = read_tokenizer(model_dir / "tokenizer")
 
-    text_encoder_2_model, text_encoder_2_weights = decrypt_model(model_dir / "text_encoder_2", 'openvino_model.xml', 'openvino_model.bin')
+    text_encoder_2_model, text_encoder_2_weights = decrypt_model(
+        model_dir / "text_encoder_2", "openvino_model.xml", "openvino_model.bin"
+    )
     text_encoder_2_tokenizer = read_tokenizer(model_dir / "tokenizer_2")
 
-    unet_model, unet_weights = decrypt_model(model_dir / "unet", 'openvino_model.xml', 'openvino_model.bin')
-    vae_decoder_model, vae_decoder_weights = decrypt_model(model_dir / "vae_decoder", 'openvino_model.xml', 'openvino_model.bin')
+    unet_model, unet_weights = decrypt_model(model_dir / "unet", "openvino_model.xml", "openvino_model.bin")
+    vae_decoder_model, vae_decoder_weights = decrypt_model(
+        model_dir / "vae_decoder", "openvino_model.xml", "openvino_model.bin"
+    )
 
     text_encoder = openvino_genai.CLIPTextModel(
         text_encoder_model,
         text_encoder_weights,
-        openvino_genai.CLIPTextModel.Config(model_dir / "text_encoder" / "config.json"), 
-        text_encoder_tokenizer, device, **config)
+        openvino_genai.CLIPTextModel.Config(model_dir / "text_encoder" / "config.json"),
+        text_encoder_tokenizer,
+        device,
+        **config,
+    )
     text_encoder_2 = openvino_genai.CLIPTextModelWithProjection(
         text_encoder_2_model,
         text_encoder_2_weights,
-        openvino_genai.CLIPTextModelWithProjection.Config(model_dir / "text_encoder_2" / "config.json"), 
-        text_encoder_2_tokenizer, device, **config)
-    
+        openvino_genai.CLIPTextModelWithProjection.Config(model_dir / "text_encoder_2" / "config.json"),
+        text_encoder_2_tokenizer,
+        device,
+        **config,
+    )
+
     vae = openvino_genai.AutoencoderKL(
         vae_decoder_model,
         vae_decoder_weights,
         openvino_genai.AutoencoderKL.Config(model_dir / "vae_decoder" / "config.json"),
-        device, **config)
+        device,
+        **config,
+    )
 
     unet = openvino_genai.UNet2DConditionModel(
         unet_model,
         unet_weights,
         openvino_genai.UNet2DConditionModel.Config(model_dir / "unet" / "config.json"),
         vae.get_vae_scale_factor(),
-        device, **config)
-
+        device,
+        **config,
+    )
 
     pipe = openvino_genai.Text2ImagePipeline.stable_diffusion_xl(
         scheduler=openvino_genai.Scheduler.from_config(model_dir / "scheduler" / "scheduler_config.json"),
@@ -116,15 +137,12 @@ def main():
         return False
 
     image_tensor = pipe.generate(
-        args.prompt,
-        width=512,
-        height=512,
-        num_inference_steps=20,
-        num_images_per_prompt=1,
-        callback=callback)
+        args.prompt, width=512, height=512, num_inference_steps=20, num_images_per_prompt=1, callback=callback
+    )
 
     image = Image.fromarray(image_tensor.data[0])
     image.save("image.bmp")
 
-if '__main__' == __name__:
+
+if "__main__" == __name__:
     main()
