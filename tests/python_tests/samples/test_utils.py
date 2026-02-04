e9fbaa848718fe044fef906a35b4f8806fc4f620
Kirill Suvorov <kirill.suvorov@intel.com>
2026-02-04 20:57:32 +0100

    [JS] Add VLMPipeline samples
    Signed-off-by: Kirill Suvorov <kirill.suvorov@intel.com>

--- tests/python_tests/samples/test_utils.py
+++ tests/python_tests/samples/test_utils.py
@@ -19,3 +19,45 @@ def run_sample(
         raise
     logger.info(f"Sample output: {result.stdout}")
     return result
+
+
+def run_js_chat(
+    command: list[str],
+    input_data: str,
+    env: dict[str, str] = os.environ,
+):
+    print(f"Running JS sample command: {' '.join(map(str, command))}")
+    inputs = input_data.split("\n")
+    print(f"Input data: {input_data}")
+    try:
+        proc = subprocess.Popen(
+            command,
+            text=True,
+            encoding="utf-8",
+            env=env,
+            stdin=subprocess.PIPE,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.STDOUT,
+            bufsize=1,
+        )
+
+        output_lines: list[str] = []
+        i = 0
+        for line in proc.stdout:
+            output_lines.append(line)
+            if "question:" in line:
+                if i < len(inputs):
+                    proc.stdin.write(inputs[i])
+                    proc.stdin.write("\n")
+                    proc.stdin.flush()
+                    i += 1
+                else:
+                    break
+
+        proc.stdin.close()
+    except subprocess.CalledProcessError as error:
+        print(f"Sample returned {error.returncode}. Output:\n{error.output}")
+        raise
+    stdout = "".join(output_lines)
+    print(f"Sample output: {stdout}")
+    return stdout
