a1e9c0f4f46760c63bc231a56d10f3c5f0bd2f62
Damian Kalinowski <damian.kalinowski@intel.com>
2026-02-04 14:58:41 +0100

    save

--- src/cpp/src/logger.hpp
+++ src/cpp/src/logger.hpp
@@ -79,11 +79,6 @@ inline void log_message(ov::log::Level level, const char* file, int line, const 
 
 }  // namespace detail
 
-#define GENAI_CHECK_LOG_LEVEL(LOG_LEVEL, ...)                                         \
-    if (::ov::genai::Logger::get_instance().should_log(LOG_LEVEL)) {                  \
-        ::ov::genai::detail::log_message(LOG_LEVEL, __FILE__, __LINE__, __VA_ARGS__); \
-    }
-
 #define GENAI_DEBUG(...) ::ov::genai::detail::log_message(ov::log::Level::DEBUG, __FILE__, __LINE__, __VA_ARGS__)
 #define GENAI_INFO(...)  ::ov::genai::detail::log_message(ov::log::Level::INFO, __FILE__, __LINE__, __VA_ARGS__)
 #define GENAI_WARN(...)  ::ov::genai::detail::log_message(ov::log::Level::WARNING, __FILE__, __LINE__, __VA_ARGS__)
