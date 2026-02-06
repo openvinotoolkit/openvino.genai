f251fbbf95ee0f8645aedd6f60c4ea97b34bd1b5
as-suvorov <alexander.suvorov@intel.com>
2026-02-06 08:59:14 +0100

    Update comment

--- tests/python_tests/test_whisper_pipeline.py
+++ tests/python_tests/test_whisper_pipeline.py
@@ -760,7 +760,7 @@ def test_perf_metrics(model_descr, sample_from_dataset):
     assert np.allclose(mean_dur, np.mean(raw_dur))
     assert np.allclose(std_dur, np.std(raw_dur))
 
-    # processing duration is a single value per inference
+    # processing duration is a single value per generate
     assert len(whisper_raw_metrics.word_level_timestamps_processing_durations) == 1
 
     word_ts_raw_dur = np.array(whisper_raw_metrics.word_level_timestamps_processing_durations) / 1000
