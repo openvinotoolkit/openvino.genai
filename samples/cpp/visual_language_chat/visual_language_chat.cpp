30b6aa1a97ac8546743ed0e6d620da5161e83560
sunxiaoxia2022 <xiaoxia.sun@intel.com>
2026-02-11 15:45:51 +0800

    remove ENABLE_LOOKUP option in sample

--- samples/cpp/visual_language_chat/visual_language_chat.cpp
+++ samples/cpp/visual_language_chat/visual_language_chat.cpp
@@ -11,38 +11,31 @@ ov::genai::StreamingStatus print_subword(std::string&& subword) {
 }
 
 int main(int argc, char* argv[]) try {
-    if (argc < 3 || argc > 5) {
-        throw std::runtime_error(std::string{"Usage "} + argv[0] +
-                                 " <MODEL_DIR> <IMAGE_FILE OR DIR_WITH_IMAGES> <ENABLE_LOOKUP> <DEVICE>");
+    if (argc < 3 || argc > 4) {
+        throw std::runtime_error(std::string{"Usage "} + argv[0] + " <MODEL_DIR> <IMAGE_FILE OR DIR_WITH_IMAGES> <DEVICE>");
     }
 
     std::vector<ov::Tensor> rgbs = utils::load_images(argv[2]);
 
     // GPU and NPU can be used as well.
     // Note: If NPU is selected, only language model will be run on NPU
-    std::string enable_lookup = (argc >= 4) ? argv[3] : "False";
-    bool enable_lookup_bool = (enable_lookup == "True" || enable_lookup == "true");
-    std::string device = (argc == 5) ? argv[4] : "CPU";
+    std::string device = (argc == 4) ? argv[3] : "CPU";
     ov::AnyMap properties;
     if (device == "GPU") {
         // Cache compiled models on disk for GPU to save time on the
         // next run. It's not beneficial for CPU.
         properties.insert({ov::cache_dir("vlm_cache")});
     }
-    if (enable_lookup_bool) {
-        std::cout << "Prompt lookup decoding is enabled\n";
-        properties.insert({ov::genai::prompt_lookup(enable_lookup_bool)});
-    }
+    properties.insert({ov::genai::prompt_lookup(true)});
+
     ov::genai::VLMPipeline pipe(argv[1], device, properties);
 
     ov::genai::GenerationConfig generation_config;
     generation_config.max_new_tokens = 100;
-    if (enable_lookup_bool) {
-        // Define candidates number for candidate generation
-        generation_config.num_assistant_tokens = 5;
-        // Define max_ngram_size
-        generation_config.max_ngram_size = 3;
-    }
+    // Define candidates number for candidate generation
+    generation_config.num_assistant_tokens = 5;
+    // Define max_ngram_size
+    generation_config.max_ngram_size = 3;
 
     ov::genai::ChatHistory history;
     std::string prompt;
