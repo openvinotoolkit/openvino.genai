d8d6477a67bf93f2543fa9bdda8b8d88624ef244
Liubov Talamanova <liubov.talamanova@intel.com>
2026-02-19 21:41:06 +0000

    Update LongBench tests (#3351)
    <!-- Keep your pull requests (PRs) as atomic as possible. That increases
    the likelihood that an individual PR won't be stuck because of adjacent
    problems, merge conflicts, or code review.
    Your merged PR is going to appear in the automatically generated release
    notes on GitHub. So the clearer the title the better. -->
    ## Description
    Update LongBench source, because the old one is no longer supported.
    
    <!-- Jira ticket number (e.g., 123). Delete if there's no ticket. -->
    CVS-181303
    
    <!-- Remove if not applicable -->
    Fixes #(issue)
    
    ## Checklist:
    - [x] This PR follows [GenAI Contributing
    guidelines](https://github.com/openvinotoolkit/openvino.genai?tab=contributing-ov-file#contributing).
    <!-- Always follow them. If there are deviations, explain what and why.
    -->
    - [x] Tests have been updated or added to cover the new code. <!--
    Specify exactly which tests were added or updated. If the change isn't
    maintenance related, update the tests at
    https://github.com/openvinotoolkit/openvino.genai/tree/master/tests or
    explain in the description why the tests don't need an update. -->
    - [x] This PR fully addresses the ticket. <!--- If not, explain clearly
    what is covered and what is not. If follow-up pull requests are needed,
    specify in the description. -->
    - [NA] I have made corresponding changes to the documentation. <!-- Run
    github.com/\<username>/openvino.genai/actions/workflows/deploy_gh_pages.yml
    on your fork with your branch as a parameter to deploy a test version
    with the updated content. Replace this comment with the link to the
    built docs. If the documentation is updated in a separate PR, clearly
    specify it. -->

--- tests/python_tests/test_kv_cache_eviction/test_kv_cache_eviction_1.py
+++ tests/python_tests/test_kv_cache_eviction/test_kv_cache_eviction_1.py
@@ -188,7 +188,6 @@ class LongBenchTestData:
     LongBenchTestData("samsum", 4, 1.6, 2.5),
     LongBenchTestData("trec", 3.2, 2.0, 3.3),
 ], ids=["samsum", "trec"])
-@pytest.mark.xfail(reason="load_dataset fail because of remote code. Ticket: 181303")
 def test_optimized_generation_longbench(test_struct):
     seqs_per_request = 32
     device = "CPU"
@@ -214,7 +213,7 @@ def test_optimized_generation_longbench(test_struct):
     generation_config.num_return_sequences = 1
     generation_config.max_new_tokens = max_new_tokens
 
-    data = datasets.load_dataset('THUDM/LongBench', subset, split='test[:32]', trust_remote_code=True)
+    data = datasets.load_dataset("zai-org/LongBench", subset, split="test[:32]", revision="8cbd1")
     with tqdm(total=len(data)) as progress_bar:
         batch = []
         answers = []
