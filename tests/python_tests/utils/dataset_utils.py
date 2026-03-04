dd1e3296e2a487def74c19b77ccfbeb17ac53c47
as-suvorov <alexander.suvorov@intel.com>
2026-03-04 15:56:20 +0100

    Remove logs

--- tests/python_tests/utils/dataset_utils.py
+++ tests/python_tests/utils/dataset_utils.py
@@ -9,9 +9,6 @@ from huggingface_hub import snapshot_download
 
 def load_dataset_via_snapshot(
     repo_id: str, *args: Any, **kwargs: Any
-) -> "datasets.Dataset | datasets.DatasetDict | datasets.IterableDataset | datasets.IterableDatasetDict":
+) -> datasets.Dataset | datasets.DatasetDict | datasets.IterableDataset | datasets.IterableDatasetDict:
     local_path = retry_request(lambda: snapshot_download(repo_id, repo_type="dataset"))
-    print("snapshot_download downloaded to:", local_path)
-    print(f"datasets.config.HF_DATASETS_CACHE: {datasets.config.HF_DATASETS_CACHE}")
-    result = datasets.load_dataset(local_path, *args, **kwargs)
-    return result
+    return datasets.load_dataset(local_path, *args, **kwargs)
