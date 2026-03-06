b5144f601379db70a421a4639a54bf70a59d8c9f
Vladimir Zlobin <vladimir.zlobin@intel.com>
2026-03-06 14:58:40 +0400

    import

--- tests/python_tests/test_whisper_pipeline_static.py
+++ tests/python_tests/test_whisper_pipeline_static.py
@@ -1,6 +1,7 @@
 # Copyright (C) 2024 Intel Corporation
 # SPDX-License-Identifier: Apache-2.0
 
+from utils.constants import NPUW_CPU_PROPERTIES
 from utils.network import retry_request
 from utils.atomic_download import AtomicDownloadManager
 from test_whisper_pipeline import get_whisper_models_list, sample_from_dataset, get_fixture_params_for_n_whisper_dataset_samples
