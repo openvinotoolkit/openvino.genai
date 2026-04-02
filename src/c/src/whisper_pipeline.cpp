9a3e9794c92e44242a38f4efcd1ff3537df403b8
Harsh Dinodia <23f3001449@ds.study.iitm.ac.in>
2026-04-02 21:23:45 +0530

    Update whisper_pipeline.cpp

--- src/c/src/whisper_pipeline.cpp
+++ src/c/src/whisper_pipeline.cpp
@@ -514,7 +514,7 @@ ov_status_e ov_genai_whisper_pipeline_set_generation_config(ov_genai_whisper_pip
     return ov_status_e::OK;
 }
 
-ov_status_e ov_genai_whisper_generation_config_set_word_timestamps(ov_genai_whisper_generation_config* config, bool return_word_timestamps) {
+ov_status_e ov_genai_whisper_generation_config_set_word_timestamps(ov_genai_whisper_generation_config* config, bool word_timestamps) {
     if (!config || !config->object) return ov_status_e::INVALID_C_PARAM;
     try {
         config->object->word_timestamps = word_timestamps;
