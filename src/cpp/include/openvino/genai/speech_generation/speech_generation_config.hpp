591109ac690b86365e2306fdf32562b40461c38d
Ryan Metcalfe <ryan.metcalfe@intel.com>
2026-04-09 05:10:01 -0700

    fix namespace collision for language property

--- src/cpp/include/openvino/genai/speech_generation/speech_generation_config.hpp
+++ src/cpp/include/openvino/genai/speech_generation/speech_generation_config.hpp
@@ -78,7 +78,7 @@ static constexpr ov::Property<float> threshold{"threshold"};
 
 static constexpr ov::Property<float> speed{"speed"};
 
-static constexpr ov::Property<std::string> language{"language"};
+static constexpr ov::Property<std::string> speech_language{"language"};
 static constexpr ov::Property<uint32_t> max_phoneme_length{"max_phoneme_length"};
 static constexpr ov::Property<std::filesystem::path> phonemize_fallback_model_dir{"phonemize_fallback_model_dir"};
 
