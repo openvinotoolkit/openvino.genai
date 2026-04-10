bb23bfd971ada2c6ecc68c80482a2d287c8c4d8a
as-suvorov <alexander.suvorov@intel.com>
2026-04-10 11:46:46 +0200

    Enable HunYuan (tencent/HY-MT1.5-1.8B) model support

--- tools/llm_bench/llm_bench_utils/config_class.py
+++ tools/llm_bench/llm_bench_utils/config_class.py
@@ -215,6 +215,7 @@ USE_CASES = {
                 "granite",
                 "granitemoe",
                 "gptj",
+                "hunyuan",
                 "yi-",
                 "lfm2-moe",
                 "afmoe",
