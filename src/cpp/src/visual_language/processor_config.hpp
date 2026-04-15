e3d1fe48022b431d99c1c819e19292f36f0c9a20
yatarkan <yaroslav.tarkan@intel.com>
2026-04-16 01:49:55 +0400

    Fix imports

--- src/cpp/src/visual_language/processor_config.hpp
+++ src/cpp/src/visual_language/processor_config.hpp
@@ -3,11 +3,13 @@
 
 #pragma once
 
-#include "openvino/genai/visibility.hpp"
-#include <openvino/runtime/properties.hpp>
 #include <array>
 #include <filesystem>
+
 #include <nlohmann/json.hpp>
+
+#include <openvino/runtime/properties.hpp>
+#include "openvino/genai/visibility.hpp"
 
 namespace ov::genai {
 /// @brief A Configuration class passed to VisionEncoder and used to
