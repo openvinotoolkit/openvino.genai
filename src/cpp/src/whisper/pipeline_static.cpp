57557a11e56f9c2dbb27f173f4cc937ac4743184
Alexander Suvorov <alexander.suvorov@intel.com>
2026-04-24 08:50:12 +0200

    Remove decoding from static pipeline

--- src/cpp/src/whisper/pipeline_static.cpp
+++ src/cpp/src/whisper/pipeline_static.cpp
@@ -263,8 +263,7 @@ std::string to_unescaped_language(const std::string& language) {
 ov::genai::SotTokensResult prepare_sot_tokens(ov::Tensor& encoder_hidden_state,
                                       ov::InferRequest& decoder,
                                       const ov::genai::WhisperGenerationConfig& config,
-                                      ov::genai::RawPerfMetrics& raw_metrics,
-                                      ov::genai::Tokenizer& tokenizer) {
+                                      ov::genai::RawPerfMetrics& raw_metrics) {
     if (!config.is_multilingual) {
         // non-multilingual whisper models are english-only
         return {std::vector<int64_t>{config.decoder_start_token_id}, "en"};
@@ -279,14 +278,7 @@ ov::genai::SotTokensResult prepare_sot_tokens(ov::Tensor& encoder_hidden_state,
         }
     } else {
         language_token_id = detect_language(encoder_hidden_state, decoder, config, raw_metrics);
-        if (!config.lang_to_id.empty()) {
-            // first try to find in config map
-            language = find_language_by_token_id(config.lang_to_id, language_token_id);
-        } else {
-            // if no map provided, decode with tokenizer
-            language =
-                tokenizer.decode(std::vector<int64_t>{language_token_id}, {ov::genai::skip_special_tokens(false)});
-        }
+        language = find_language_by_token_id(config.lang_to_id, language_token_id);
     }
 
     int64_t task_token_id = config.transcribe_token_id;
@@ -1198,7 +1190,7 @@ WhisperDecodedResults WhisperPipeline::StaticWhisperPipeline::generate(
 
         // prepare sot_tokens just once for whole input
         if (sot_tokens.empty()) {
-            auto sot_result = prepare_sot_tokens(hidden_state_tensor, m_models.decoder, config, raw_metrics, m_tokenizer);
+            auto sot_result = prepare_sot_tokens(hidden_state_tensor, m_models.decoder, config, raw_metrics);
             sot_tokens = std::move(sot_result.tokens);
             result.language = sot_result.language;
         }
