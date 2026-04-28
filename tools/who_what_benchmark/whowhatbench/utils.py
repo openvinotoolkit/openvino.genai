fa4aee747215561506aea1ff7534b51195f53a0c
Sofya Balandina <sofya.balandina@intel.com>
2026-04-28 16:37:26 +0100

    Update utils.py

--- tools/who_what_benchmark/whowhatbench/utils.py
+++ tools/who_what_benchmark/whowhatbench/utils.py
@@ -235,13 +235,13 @@ def prepare_default_data_video(num_samples=None, num_frames=10):
                                       filename=f"{SUBSET}/{SUBSET}_videos_10.tar.gz",
                                       repo_type="dataset")
 
+    max_video_size_mb = 5.4 * 1024 * 1024
     video_samples = []
     extract_dir = "./videos"
     os.makedirs(extract_dir, exist_ok=True)
     with tarfile.open(videos_arc_path, "r:gz") as tar:
         all_videos = []
         for member in tar.getmembers():
-            max_video_size_mb = 5.4 * 1024 * 1024
             if member.size < max_video_size_mb:
                 all_videos.append(member.name)
 
