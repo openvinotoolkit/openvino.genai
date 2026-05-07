c11b68b57695ded5c955cbf5c9744f4250e65973
Pavel Esir <pavel.esir@intel.com>
2026-05-07 23:41:20 +0200

    fix test

--- tests/python_tests/test_llm_pipeline.py
+++ tests/python_tests/test_llm_pipeline.py
@@ -1086,7 +1086,7 @@ def test_perf_metrics_with_apply_chat_template(ov_pipe: ov_genai.LLMPipeline) ->
 
     metrics = [None] * len(chat_history)
     for i, msg in enumerate(chat_history):
-        metrics[i] = ov_pipe.generate(msg, max_new_tokens=20, apply_chat_template=True).perf_metrics
+        metrics[i] = ov_pipe.generate([msg], max_new_tokens=20, apply_chat_template=True).perf_metrics
     perf_metrics = sum(metrics[1:], start=metrics[0])
 
     raw_metrics = perf_metrics.raw_metrics
@@ -1104,7 +1104,8 @@ def test_perf_metrics_without_apply_chat_template(ov_pipe: ov_genai.LLMPipeline)
     result = ov_pipe.generate(["What is the capital of France?"], max_new_tokens=20, apply_chat_template=False)
 
     assert result.perf_metrics.raw_metrics.chat_template_durations == []
-    assert result.perf_metrics.get_chat_template_duration() is None
+    assert result.perf_metrics.get_chat_template_duration().mean == -1
+    assert result.perf_metrics.get_chat_template_duration().std == -1
 
 
 @pytest.mark.parametrize("llm_model", ["facebook/opt-125m"], indirect=True)
