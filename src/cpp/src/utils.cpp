195871b091bef9446e0e36661304daf514e6b3a0
John Feng <john.feng@intel.com>
2026-05-14 21:25:04 -0800

    make import model support ov::Tensor

--- src/cpp/src/utils.cpp
+++ src/cpp/src/utils.cpp
@@ -1005,6 +1005,13 @@ ov::CompiledModel import_model(const std::filesystem::path& blob_path,
     return ov::genai::utils::singleton_core().import_model(blob_tensor, device, properties);
 }
 
+ov::CompiledModel import_model(const ov::Tensor& blob_tensor,
+                               const std::string& device,
+                               const ov::AnyMap& properties) {
+    OPENVINO_ASSERT(blob_tensor.get_element_type() == ov::element::u8, "Blob tensor should have uint8 element type");
+    return ov::genai::utils::singleton_core().import_model(blob_tensor, device, properties);
+}
+
 void export_model(ov::CompiledModel& compiled_model, const std::filesystem::path& blob_path) {
     OPENVINO_ASSERT(!blob_path.empty(), "blob path is empty");
 
