9c697c50f12b6f74b1b5f97b3b3d038fc7ef29bf
likholat <anna.likholat@intel.com>
2026-07-07 12:13:30 +0200

    unpack_latents fix

--- src/cpp/src/image_generation/flux_latent_utils.hpp
+++ src/cpp/src/image_generation/flux_latent_utils.hpp
@@ -45,14 +45,14 @@ inline ov::Tensor unpack_latents(const ov::Tensor& latents, const size_t height,
     ov::Shape latents_shape = latents.get_shape();
     size_t batch_size = latents_shape[0], channels = latents_shape[2];
 
-    height /= vae_scale_factor;
-    width /= vae_scale_factor;
+    size_t out_height = height / vae_scale_factor;
+    size_t out_width = width / vae_scale_factor;
 
-    size_t h_half = height / 2;
-    size_t w_half = width / 2;
+    size_t h_half = out_height / 2;
+    size_t w_half = out_width / 2;
     size_t c_quarter = channels / 4;
 
-    ov::Shape final_shape = {batch_size, c_quarter, height, width};
+    ov::Shape final_shape = {batch_size, c_quarter, out_height, out_width};
     ov::Tensor permuted_latents(latents.get_element_type(), final_shape);
 
     OPENVINO_ASSERT(latents.get_size() == permuted_latents.get_size(), "Incorrect target shape, tensors must have the same sizes");
@@ -65,12 +65,12 @@ inline ov::Tensor unpack_latents(const ov::Tensor& latents, const size_t height,
             for (size_t h2 = 0; h2 < h_half; ++h2) {
                 for (size_t w2 = 0; w2 < w_half; ++w2) {
                     size_t base_reshaped_index = (((b * h_half + h2) * w_half + w2) * c_quarter + c4) * 4;
-                    size_t base_final_index = (b * c_quarter * height * width) + (c4 * height * width) + (h2 * 2 * width + w2 * 2);
+                    size_t base_final_index = (b * c_quarter * out_height * out_width) + (c4 * out_height * out_width) + (h2 * 2 * out_width + w2 * 2);
 
                     dst_data[base_final_index] = src_data[base_reshaped_index];
                     dst_data[base_final_index + 1] = src_data[base_reshaped_index + 1];
-                    dst_data[base_final_index + width] = src_data[base_reshaped_index + 2];
-                    dst_data[base_final_index + width + 1] = src_data[base_reshaped_index + 3];
+                    dst_data[base_final_index + out_width] = src_data[base_reshaped_index + 2];
+                    dst_data[base_final_index + out_width + 1] = src_data[base_reshaped_index + 3];
                 }
             }
         }
