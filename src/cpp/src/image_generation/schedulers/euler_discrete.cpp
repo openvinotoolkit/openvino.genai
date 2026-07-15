47dd0cc3fd93e03507163ba29ae6804431efa88f
likholat <anna.likholat@intel.com>
2026-07-15 14:41:04 +0200

    Fix TRAILING timestep loop

--- src/cpp/src/image_generation/schedulers/euler_discrete.cpp
+++ src/cpp/src/image_generation/schedulers/euler_discrete.cpp
@@ -139,7 +139,8 @@ void EulerDiscreteScheduler::set_timesteps(size_t num_inference_steps, float str
     }
     case TimestepSpacing::TRAILING: {
         float step_ratio = static_cast<float>(m_config.num_train_timesteps) / static_cast<float>(m_num_inference_steps);
-        for (float i = m_config.num_train_timesteps; i > 0; i -= step_ratio) {
+        for (size_t s = 0; s < num_inference_steps; ++s) {
+            float i = m_config.num_train_timesteps - s * step_ratio;
             m_timesteps.push_back(static_cast<int64_t>(std::round(i)) - 1);
         }
         break;
