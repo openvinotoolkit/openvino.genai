a21274e8e30ec1eda547443d16061a849232aff1
Artem Vasenin <artem.vasenin@intel.com>
2026-08-20 13:17:50 +0200

    Drop LLM generated tests

--- tools/llm_bench/tests/conftest.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-# Copyright (C) 2023-2026 Intel Corporation
-# SPDX-License-Identifier: Apache-2.0
-import sys
-from pathlib import Path
-
-LLM_BENCH_ROOT = Path(__file__).resolve().parents[1]
-if str(LLM_BENCH_ROOT) not in sys.path:
-    sys.path.insert(0, str(LLM_BENCH_ROOT))
