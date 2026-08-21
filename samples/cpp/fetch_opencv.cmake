b2fc1b8ade90b9eddcae6bb3f1d2e64e4df414a2
nikolaygorb <nikolay.gorb@intel.com>
2026-08-21 10:00:02 +0000

    add opencv-src to support opencv 4.14

--- samples/cpp/fetch_opencv.cmake
+++ samples/cpp/fetch_opencv.cmake
@@ -58,6 +58,10 @@ function(ov_genai_link_opencv target_name)
         set(BUILD_opencv_stitching OFF CACHE BOOL "" FORCE)
         set(BUILD_opencv_ts OFF CACHE BOOL "" FORCE)
 
+        include_directories(
+            "${FETCHCONTENT_BASE_DIR}/opencv-src/modules/core/include"
+            "${FETCHCONTENT_BASE_DIR}/opencv-src/modules/imgproc/include")
+
         FetchContent_Declare(opencv
             GIT_REPOSITORY https://github.com/opencv/opencv.git
             GIT_TAG 4.14.0
