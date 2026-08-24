4e89434deebdf7c2d6a37765c292767456a08aba
Artem Vasenin <artem.vasenin@intel.com>
2026-08-24 18:15:04 +0200

    Remove unused imports

--- tools/who_what_benchmark/tests/test_unit_speech_recognition.py
+++ tools/who_what_benchmark/tests/test_unit_speech_recognition.py
@@ -9,8 +9,6 @@ import pandas as pd
 import pytest
 import torch
 
-import whowhatbench.speech_recognition_evaluator as asr
-from whowhatbench.model_loaders import load_speech_recognition_model
 from whowhatbench.whowhat_metrics import TranscriptSimilarity
 from whowhatbench.utils import to_mono_16k
 from whowhatbench.speech_recognition_evaluator import (
