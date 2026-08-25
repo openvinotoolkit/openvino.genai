b7de0e8eac7406f21fa12f7f1ec4d2726aefe179
Artem Vasenin <artem.vasenin@intel.com>
2026-08-25 08:06:38 +0200

    Export tiny-random in fp32

--- tools/who_what_benchmark/tests/conftest.py
+++ tools/who_what_benchmark/tests/conftest.py
@@ -48,7 +48,7 @@ MODELS: Dict[str, Dict[str, Any]] = {
     },
     "tiny-random-fun-asr": {
         "name": "optimum-intel-internal-testing/tiny-random-fun-asr",
-        "convert_args": [],
+        "convert_args": ["--weight-format", "fp32"],
     },
     "tiny-random-llava-next-video": {
         "name": "optimum-intel-internal-testing/tiny-random-llava-next-video",
